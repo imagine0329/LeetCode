4679e24a0638948fdb4eeb1f684c1af6fac35d6b
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-07-25 12:02:01 +0900

    Time: 89 ms (5.83%), Space: 34.7 MB (92.44%) - LeetHub

--- search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -12,11 +12,17 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(root == NULL) return NULL;
-        if(root->val == val) return root;
-        if(root->val > val)
-            return searchBST(root->left, val);
-        else
-            return searchBST(root->right, val);
+        while(root != NULL)
+        {
+            if(root->val == val)
+                return root;
+            
+            if(root->val > val)
+                root = root->left;
+            else
+                root = root->right;
+        }
+        
+        return root;
     }
 };
