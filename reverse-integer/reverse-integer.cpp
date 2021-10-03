40d8c993e7098c2b8bbb9d044680d0b5d9c0eb4b
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-10-04 08:26:38 +0900

    Time: 4 ms (40.62%), Space: 5.8 MB (96.28%) - LeetHub

--- reverse-integer/reverse-integer.cpp
+++ reverse-integer/reverse-integer.cpp
@@ -3,22 +3,22 @@ public:
     //  INT_MAX = 2147483647
     //  INT_MIN = -2147483648
     int reverse(int x) {
-        int rev = 0;
+        int reversed = 0;
         
         while(x != 0)
         {
             int pop = x % 10;
             x /= 10;
             
-            //  overflow check
-            if(rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7))
+            // overflow check
+            if(reversed > INT_MAX / 10 || (reversed == INT_MAX / 10 && reversed % 10 > 7))
                 return 0;
-            if(rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8))
+            if(reversed < INT_MIN / 10 || (reversed == INT_MIN / 10 && reversed % 10 < -8))
                 return 0;
             
-            rev = (rev * 10) + pop;
+            reversed = (reversed * 10) + pop;
         }
         
-        return rev;
+        return reversed;
     }
 };
