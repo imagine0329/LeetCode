25a4005c74c77777470054fdf3039d5a5f85f418
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-10-04 09:52:03 +0900

    Time: 4 ms (92.44%), Space: 6 MB (67.51%) - LeetHub

--- roman-to-integer/roman-to-integer.cpp
+++ roman-to-integer/roman-to-integer.cpp
@@ -1,59 +1,47 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-        char ch = 0;
-        char prev = 0;
-        int decimal = 0;
-        
-        prev = s.back();
-        while(!s.empty())
-        {
-            ch = s.back();
-            s.pop_back();
-            decimal += getAddition(ch, prev);
-            prev = ch;
-        }
-        
-        return decimal;
-    }
-    
-    int getAddition(char ch, char prev)
+    int getDecimal(char c)
     {
-        switch(ch)
+        switch(c)
         {
             case 'I':
-                if(prev == ch)
-                    return 1;
-                else
-                    return -1;
-                
+                return 1;
             case 'V':
                 return 5;
-                
             case 'X':
-                if(prev == ch || prev == 'I' || prev == 'V')
-                    return 10;
-                else
-                    return -10;
-                
+                return 10;
             case 'L':
                 return 50;
-                
             case 'C':
-                if(prev == ch || prev == 'I' || prev == 'V'
-                    || prev == 'X' || prev == 'L')
-                    return 100;
-                else
-                    return -100;
-                
+                return 100;
             case 'D':
                 return 500;
-                
             case 'M':
                 return 1000;
-                
             default:
-                return 0;
+                return -1;
         }
+    }
+    
+    int romanToInt(string s) {
+        int ans = 0;
+        int prev = 0;
+        
+        for(int i = s.length()-1; i>=0; i--)
+        {
+            int curr = getDecimal(s[i]);
+            
+            if(curr == -1)
+                return -1;
+            
+            if(prev > curr)
+                ans -= curr;
+            else
+                ans += curr;
+            
+            prev = curr;
+        }
+        
+        return ans;
     }
 };
