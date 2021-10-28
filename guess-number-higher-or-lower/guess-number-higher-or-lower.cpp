03a91bf7e253df83fb8ee4addbef792180134ca7
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-10-29 06:11:48 +0900

    Time: 0 ms (100.00%), Space: 5.7 MB (99.50%) - LeetHub

--- guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
+++ guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -10,6 +10,7 @@
 class Solution {
 public:
     int guessNumber(int n) {
+        /*  binary search
         int left = 1, right = n, pivot;
         while(left <= right)
         {
@@ -21,6 +22,30 @@ public:
                 left = pivot + 1;
             else
                 right = pivot - 1;
+        }*/
+        
+        /*  ternary search*/
+        int low = 1, high = n;
+        while(low <= high)
+        {
+            int mid1 = low + (high - low)/3;
+            int mid2 = high - (high - low)/3;
+            int res1 = guess(mid1);
+            int res2 = guess(mid2);
+            if(res1 == 0)
+                return mid1;
+            if(res2 == 0)
+                return mid2;
+            
+            if(res1 < 0)
+                high = mid1 - 1;
+            else if(res2 > 0)
+                low = mid2 + 1;
+            else
+            {
+                low = mid1 + 1;
+                high = mid2 + 1;
+            }
         }
         
         return -1;
