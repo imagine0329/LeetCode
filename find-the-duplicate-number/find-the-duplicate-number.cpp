d566975c2cca1bb2a50abfb4d758728b3303dae3
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-11-18 11:28:48 +0900

    Time: 136 ms (42.28%), Space: 61.2 MB (59.09%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.cpp
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        /*  Binary Search*/
+        auto small_or_equal = [&](int cur)
+        {
+            int count = 0;
+            for(auto &num : nums)
+            {
+                if(num <= cur)
+                    count++;
+            }
+            
+            return count;
+        };
+        
+        int low = 1, high = nums.size();
+        int duplicate;
+        
+        while(low <= high)
+        {
+            int cur = low + (high - low)/2;
+            if(small_or_equal(cur) > cur)
+            {
+                duplicate = cur;
+                high = cur - 1;
+            }
+            else
+                low = cur + 1;
+        }
+        
+        return duplicate;
+        
+        
+        /*  Floyd's Cylcle Detection
         int tortoise = nums[0];
         int hare = nums[0];
         
@@ -16,6 +48,6 @@ public:
             hare = nums[hare];
         }
         
-        return tortoise;
+        return tortoise;*/
     }
 };
