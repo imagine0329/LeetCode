d18e3dbb39a8d3e879621e8c7dd94eda9b4c32c9
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-11-21 10:04:57 +0900

    Time: 4 ms (49.74%), Space: 7.2 MB (80.05%) - LeetHub

---
+++ design-hit-counter/design-hit-counter.cpp
@@ -0,0 +1,31 @@
+class HitCounter {
+private:
+    queue<int> hits;
+    
+public:
+    HitCounter() {
+        
+    }
+    
+    void hit(int timestamp) {
+        this->hits.push(timestamp);
+    }
+    
+    int getHits(int timestamp) {
+        while(!this->hits.empty())
+        {
+            int diff = timestamp - this->hits.front();
+            if(diff >= 300) this->hits.pop();
+            else break;
+        }
+        
+        return this->hits.size();
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
