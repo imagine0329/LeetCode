d6be6f5ebd5d385043336e57de2b02a349e56ab4
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-11-29 12:43:05 +0900

    Time: 16 ms (83.11%), Space: 11.5 MB (93.41%) - LeetHub

---
+++ meeting-rooms/meeting-rooms.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b){
+            return a[0] < b[0];
+        });
+        
+        for(int i = 1; i<intervals.size(); i++)
+        {
+            if(intervals[i-1][1] > intervals[i][0])
+                return false;
+        }
+        
+        return true;
+    }
+};
