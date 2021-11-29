3e31d4db4556cc5411656f95e19cca974b7e2585
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-11-29 10:05:05 +0900

    Time: 8 ms (99.83%), Space: 11.9 MB (95.10%) - LeetHub

--- number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
@@ -6,6 +6,94 @@ public:
         
         int root = parent[vertex];
         while(parent[root] != root)
+            root = parent[root];
+        
+        while(parent[vertex] != root)
+        {
+            int next = parent[vertex];
+            parent[vertex] = root;
+            vertex = next;
+        }
+        
+        return root;
+    }
+    
+    int combine(vector<int>& parent, int v1, int v2){
+        v1 = find(parent, v1);
+        v2 = find(parent, v2);
+        
+        if(v1 == v2)
+            return 0;
+        
+        if(v1 < v2)
+            parent[v2] = v1;
+        else
+            parent[v1] = v2;
+        
+        return -1;
+    }
+    
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<int> parent(n);
+        for(int i=0; i<n; i++)
+            parent[i] = i;
+        
+        int ans = n;
+        for(int i=0; i<edges.size(); i++)
+            ans += combine(parent, edges[i][0], edges[i][1]);
+        
+        return ans;
+    }
+};
+
+
+
+/*  DFS
+class Solution {
+public:
+    void dfs(vector<int> adj[], vector<int>& visited, int src){
+        visited[src] = 1;
+        
+        for(int i=0; i<adj[src].size(); i++)
+        {
+            if(visited[adj[src][i]] == 0)
+                dfs(adj, visited, adj[src][i]);
+        }
+    }
+    
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<int> visited(n, 0);
+        vector<int> adj[n];
+        
+        for(auto edge : edges)
+        {
+            adj[edge.front()].push_back(edge.back());
+            adj[edge.back()].push_back(edge.front());
+        }
+        
+        int ans = 0;
+        for(int i=0; i<n; i++)
+        {
+            if(visited[i] == 0)
+            {
+                ans++;
+                dfs(adj, visited, i);
+            }
+        }
+        
+        return ans;
+    }
+};*/
+
+/*  Union Find
+class Solution {
+public:
+    int find(vector<int>& parent, int vertex){
+        if(parent[vertex] == vertex)
+            return vertex;
+        
+        int root = parent[vertex];
+        while(parent[root] != root)
         {
             root = parent[root];
             parent[vertex] = root;
@@ -43,3 +131,4 @@ public:
         return ans;
     }
 };
+*/
