d85b43f957697f7a67a4ed1ee770a1a603096aa6
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-12-11 10:10:17 +0900

    Time: 532 ms (36.61%), Space: 7.9 MB (23.15%) - LeetHub

---
+++ word-search/word-search.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        
+        for(int row=0; row<board.size(); row++)
+        {
+            for(int col=0; col<board.front().size(); col++)
+            {
+                if(backtracking(board, word, 0, row, col))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    bool backtracking(vector<vector<char>>& board, string word, int idx, int row, int col)
+    {
+        if(idx == word.length())
+            return true;
+        
+        if(row < 0 || row >= board.size() || col < 0 || col >= board.front().size())
+            return false;
+        
+        if(word[idx] != board[row][col])
+            return false;
+        
+        bool ret = false;
+        board[row][col] = '#';
+        
+        int rowOffsets[] = {0, 1, 0, -1};
+        int colOffsets[] = {1, 0, -1, 0};
+        
+        for(int i=0; i<4; i++)
+        {
+            ret = backtracking(board, word, idx+1, row+rowOffsets[i], col+colOffsets[i]);
+            if(ret)
+                break;
+        }
+        
+        board[row][col] = word[idx];
+        
+        return ret;
+    }
+};
