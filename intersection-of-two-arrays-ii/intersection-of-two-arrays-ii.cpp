3b10e548d59b5bfa476d8bb2ac1542e8f337931e
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-12-12 15:49:43 +0900

    Time: 0 ms (100.00%), Space: 10.1 MB (74.37%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
+        /*  Sort*/
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        int i=0, j=0, k=0;
+        while(i<nums1.size() && j<nums2.size())
+        {
+            if(nums1[i] == nums2[j])
+            {
+                nums1[k++] = nums1[i++];
+                j++;
+            }
+            else if(nums1[i] < nums2[j])
+                i++;
+            else
+                j++;
+        }
+        
+        return vector<int>(nums1.begin(), nums1.begin() + k);
+        
+        /*  First approach
         multiset<int> m(nums1.begin(), nums1.end());
         vector<int> ans;
         for(auto a : nums2)
@@ -13,6 +35,25 @@ public:
             }
         }
         
-        return ans;
+        return ans;*/
+        
+        
+        /*  Second approach - Hash Map
+        if(nums1.size() > nums2.size())
+            return intersect(nums2, nums1);
+        
+        unordered_map<int, int> m;
+        for(auto n : nums1)
+            ++m[n];
+        
+        int k = 0;
+        for(auto n : nums2)
+        {
+            auto it = m.find(n);
+            if(it != m.end() && --(it->second) >= 0)
+                nums1[k++] = n;
+        }
+        
+        return vector(nums1.begin(), nums1.begin() + k);*/
     }
 };
