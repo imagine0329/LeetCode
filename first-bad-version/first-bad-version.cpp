3436afaeb51bfdd20c1ef451ccd394ac7612d30f
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-12-15 16:41:14 +0900

    Time: 4 ms (10.29%), Space: 5.8 MB (65.66%) - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -5,11 +5,12 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int left = 1, right = n;
-        while(left < right)
+        
+        while(left <= right)
         {
             int mid = left + (right - left)/2;
             if(isBadVersion(mid))
-                right = mid;
+                right = mid - 1;
             else
                 left = mid + 1;
         }
