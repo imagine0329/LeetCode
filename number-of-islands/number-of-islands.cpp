4cc0a5ddea1607f6c533def2810828282b0a6505
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-12-22 08:44:47 +0900

    Time: 40 ms (36.88%), Space: 15.8 MB (23.36%) - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,9 +1,14 @@
-class Solution {
+class UnionFind{
+private:
+    vector<int> parent;
+    vector<int> rank;
+    int components;
+    
 public:
-    //  BFS
-    int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = m ? grid[0].size() : 0,
-            islands = 0, offsets[] = {0, 1, 0, -1, 0};
+    UnionFind(vector<vector<char>>& grid){
+        components = 0;
+        int m = grid.size();
+        int n = grid[0].size();
         
         for(int i=0; i<m; i++)
         {
@@ -11,28 +16,74 @@ public:
             {
                 if(grid[i][j] == '1')
                 {
-                    islands++;
-                    queue<pair<int, int>> todo;
-                    todo.push({i, j});
-                    while(!todo.empty())
-                    {
-                        pair<int, int> p = todo.front();
-                        todo.pop();
-                        for(int k=0; k<4; k++)
-                        {
-                            int r = p.first + offsets[k],
-                                c = p.second + offsets[k + 1];
-                            if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1')
-                            {
-                                grid[r][c] = '0';
-                                todo.push({r, c});
-                            }
-                        }
-                    }
+                    parent.push_back((i * n) + j);
+                    components++;
+                }
+                else
+                    parent.push_back(-1);
+                
+                rank.push_back(0);
+            }
+        }
+    }
+    
+    int find(int i)
+    {
+        if(parent[i] != i)
+            parent[i] = find(parent[i]);
+        
+        return parent[i];
+    }
+    
+    void Union(int i, int j)
+    {
+        int x = find(i);
+        int y = find(j);
+        
+        if(x == y)
+            return;
+        
+        if(rank[x] > rank[y])
+            parent[y] = x;
+        else if(rank[x] < rank[y])
+            parent[x] = y;
+        else
+        {
+            parent[y] = x;
+            rank[x]++;
+        }
+        
+        --components;
+    }
+    
+    int getNumOfComponents()
+    {
+        return components;
+    }
+};
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        UnionFind uf(grid);
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        for(int row=0; row<m; row++)
+        {
+            for(int col=0; col<n; col++)
+            {
+                if(grid[row][col] == '1')
+                {
+                    grid[row][col] = '0';
+                    if(row-1 >= 0 && grid[row-1][col] == '1')   uf.Union(row*n+col, (row-1)*n+col);
+                    if(row+1 < m && grid[row+1][col] == '1')    uf.Union(row*n+col, (row+1)*n+col);
+                    if(col-1 >= 0 && grid[row][col-1] == '1')    uf.Union(row*n+col, row*n+(col-1));
+                    if(col+1 < n && grid[row][col+1] == '1')    uf.Union(row*n+col, row*n+(col+1));
                 }
             }
         }
         
-        return islands;
+        return uf.getNumOfComponents();
     }
 };
