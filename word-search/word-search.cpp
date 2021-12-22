77e1eace9e05a59dbca499886d82e77687207167
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-12-22 14:29:12 +0900

    Time: 192 ms (90.40%), Space: 7.9 MB (32.04%) - LeetHub

--- word-search/word-search.cpp
+++ word-search/word-search.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        
-        for(int row=0; row<board.size(); row++)
+        for(int row = 0; row < board.size(); row++)
         {
-            for(int col=0; col<board.front().size(); col++)
+            for(int col = 0; col < board[0].size(); col++)
             {
-                if(backtracking(board, word, 0, row, col))
+                if(backtracking(board, word, row, col, 0))
                     return true;
             }
         }
+        
         return false;
     }
     
-    bool backtracking(vector<vector<char>>& board, string word, int idx, int row, int col)
+    bool backtracking(vector<vector<char>>& board, string& word, int row, int col, int idx)
     {
         if(idx == word.length())
             return true;
         
-        if(row < 0 || row >= board.size() || col < 0 || col >= board.front().size())
+        if(row < 0 || col < 0 || row >= board.size() || col >= board[0].size())
             return false;
         
         if(word[idx] != board[row][col])
             return false;
         
-        bool ret = false;
         board[row][col] = '#';
         
-        int rowOffsets[] = {0, 1, 0, -1};
-        int colOffsets[] = {1, 0, -1, 0};
+        int rowOffset[4] = {-1, 0, 1, 0};
+        int colOffset[4] = {0, -1, 0, 1};
         
         for(int i=0; i<4; i++)
         {
-            ret = backtracking(board, word, idx+1, row+rowOffsets[i], col+colOffsets[i]);
-            if(ret)
-                break;
+            if(backtracking(board, word, row+rowOffset[i], col+colOffset[i], idx+1))
+                return true;
         }
         
         board[row][col] = word[idx];
         
-        return ret;
+        return false;
     }
 };
