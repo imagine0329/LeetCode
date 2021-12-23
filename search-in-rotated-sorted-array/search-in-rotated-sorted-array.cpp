e2ec7f2270eb53998542f157cf04b0b09015688e
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-12-23 15:24:32 +0900

    Time: 4 ms (70.90%), Space: 10.9 MB (95.99%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -13,31 +13,54 @@ public:
     */
     
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        int mid;
+        int pivot = findPivot(nums);
         
+        if(nums[pivot] == target)
+            return pivot;
+        
+        if(pivot == 0)
+            return findTarget(nums, target, 0, nums.size() - 1);
+        
+        if(target < nums[0])
+            return findTarget(nums, target, pivot, nums.size() - 1);
+        else
+            return findTarget(nums, target, 0, pivot - 1);
+    }
+    
+    int findPivot(vector<int>& nums)
+    {
+        if(nums.front() < nums.back())
+            return 0;
+        
+        int left = 0, right = nums.size() - 1;
+        
+        while(left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= nums[0])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        
+        return left;
+    }
+    
+    int findTarget(vector<int>& nums, int target, int left, int right)
+    {
         while(left <= right)
         {
-            mid = left + (right - left)/2;
+            int mid = left + (right - left) / 2;
             if(nums[mid] == target)
                 return mid;
             
-            if(nums[left] <= nums[mid])
-            {
-                if(nums[left] <= target && target < nums[mid])
-                    right = mid - 1;
-                else
-                    left = mid + 1;
-            }
+            if(nums[mid] < target)
+                left = mid + 1;
             else
-            {
-                if(nums[mid] < target && target <= nums[right])
-                    left = mid + 1;
-                else
-                    right = mid - 1;
-            }
+                right = mid - 1;
         }
         
         return -1;
     }
+    
 };
