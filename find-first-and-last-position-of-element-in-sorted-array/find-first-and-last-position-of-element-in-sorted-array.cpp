413797f6990a898a77f949ad34a952bf58680201
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-12-23 11:49:29 +0900

    Time: 4 ms (95.55%), Space: 13.8 MB (16.31%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,62 +1,12 @@
 class Solution {
 public:
-    int binarySearch_start(vector<int>& nums, int target, int left, int right)
-    {
-        int start = right;
-        while(left <= right)
-        {
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target)
-            {   
-                start = mid;
-                right = mid - 1;
-            }
-            else
-                left = mid + 1;
-        }
-        
-        return start;
-    }
-    
-    int binarySearch_end(vector<int>& nums, int target, int left, int right)
-    {
-        int end = left;
-        while(left <= right)
-        {
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target)
-            {
-                end = mid;
-                left = mid + 1;
-            }
-            else
-                right = mid - 1;
-        }
-        
-        return end;
-    }
-    
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans(2, -1);
-        int left = 0, right = nums.size() - 1, mid;
+        int start = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        if(start == nums.size() || nums[start] != target)
+            return {-1, -1};
         
-        while(left <= right)
-        {
-            mid = left + (right - left) / 2;
-            if(nums[mid] == target)
-            {
-                ans[0] = binarySearch_start(nums, target, left, mid);
-                ans[1] = binarySearch_end(nums, target, mid, right);
-                break;
-            }
-            
-            if(nums[mid] > target)
-                right = mid - 1;
-            else
-                left = mid + 1;
-        }
+        int end = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
         
-        return ans;
+        return {start, end};
     }
 };
