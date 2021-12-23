97c67a43bfe7377b296d3cc5a8b33b5825a1d943
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-12-23 10:26:47 +0900

    Time: 8 ms (75.42%), Space: 10.2 MB (17.75%) - LeetHub

---
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,49 @@
+class Solution {
+private:
+    vector<int> nums;
+    
+public:
+    int partition(int left, int right, int pivot)
+    {
+        int store = left;
+        
+        swap(nums[pivot], nums[right]);
+        
+        for(int i=left; i<=right; i++)
+        {
+            if(nums[i] < nums[right])
+            {
+                swap(nums[i], nums[store]);
+                store++;
+            }
+        }
+        
+        swap(nums[store], nums[right]);
+        return store;
+    }
+    
+    void quickSelect(int left, int right, int k)
+    {
+        if(left == right)
+            return;
+        
+        
+        int pivot = left + rand() % (right - left + 1);
+        pivot = partition(left, right, pivot);
+        
+        if(pivot == k)
+            return;
+        
+        if(k < pivot)
+            quickSelect(left, pivot-1, k);
+        else
+            quickSelect(pivot+1, right, k);
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        this->nums = nums;
+        quickSelect(0, nums.size()-1, nums.size()-k);
+        
+        return this->nums[nums.size()-k];
+    }
+};
