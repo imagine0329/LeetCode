1f31aa3043defe912c6073c01687448f7cbb6bef
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-12-24 15:34:33 +0900

    Time: 304 ms (19.40%), Space: 10.5 MB (36.40%) - LeetHub

---
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        int max_len = 1;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    dp[i] = max(dp[i], dp[j]+1);
+                    max_len = max(dp[i], max_len);
+                }
+            }
+        }
+        
+        return max_len;
+    }
+};
