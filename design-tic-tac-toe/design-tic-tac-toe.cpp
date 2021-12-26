c4728c3ba54860debdf8682df60ed05d69ebfab0
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2021-12-26 10:24:23 +0900

    Time: 24 ms (73.03%), Space: 19.3 MB (41.52%) - LeetHub

--- design-tic-tac-toe/design-tic-tac-toe.cpp
+++ design-tic-tac-toe/design-tic-tac-toe.cpp
@@ -1,71 +1,39 @@
+/*  Optimized Approach*/
 class TicTacToe {
 private:
-    int boardSize;
-    vector<vector<int>> board;
+    vector<int> rows, cols;
+    int l_diagonal, r_diagonal;
+    int n;
     
 public:
     TicTacToe(int n) {
-        this->boardSize = n;
-        board.assign(n, vector<int>(n, 0));
+        this->n = n;
+        rows = vector<int>(n, 0);
+        cols = vector<int>(n, 0);
+        l_diagonal = 0;
+        r_diagonal = 0;
     }
     
     int move(int row, int col, int player) {
-        board[row][col] = player;
+        int x = player == 1 ? 1 : -1;
         
-        if(checkCol(row, player) ||
-          checkRow(col, player) ||
-          (row == col && checkDiagonal(player)) ||
-          (row == boardSize-col-1 && checkAntiDiagonal(player))){
+        rows[row] += x;
+        cols[col] += x;
+        
+        if(row == col)
+            l_diagonal += x;
+        
+        if(row == n - col - 1)
+            r_diagonal += x;
+        
+        int res = n*x;
+        if(rows[row] == res || cols[col] == res || l_diagonal == res || r_diagonal == res)
             return player;
-        }
         
         return 0;
     }
-    
-    bool checkCol(int row, int player)
-    {
-        for(int i=0; i<boardSize; i++)
-        {
-            if(board[row][i] != player)
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool checkRow(int col, int player)
-    {
-        for(int i=0; i<boardSize; i++)
-        {
-            if(board[i][col] != player)
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool checkDiagonal(int player)
-    {
-        for(int i=0; i<boardSize; i++)
-        {
-            if(board[i][i] != player)
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool checkAntiDiagonal(int player)
-    {
-        for(int i=0; i<boardSize; i++)
-        {
-            if(board[i][boardSize-i-1] != player)
-                return false;
-        }
-        
-        return true;
-    }
 };
+
 
 /**
  * Your TicTacToe object will be instantiated and called as such:
