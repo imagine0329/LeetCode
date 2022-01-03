173f0990bfc9bcdb8a31ef2a8968fa97d11feaa6
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-03 15:11:34 +0900

    Time: 45 ms (12.38%), Space: 9.7 MB (92.23%) - LeetHub

--- 3sum-closest/3sum-closest.cpp
+++ 3sum-closest/3sum-closest.cpp
@@ -3,43 +3,24 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
         
-        int closest = INT_MAX;
-        int minSum = 0;
-        for(int i=0; i<nums.size()-2; i++)
+        int diff = INT_MAX;
+        int n = nums.size();
+        for(int i=0; i<n-2; i++)
         {
-            int sum = nums[i] + twoSumClosest(nums, target-nums[i], i+1);
-            if(closest > abs(target - sum))
+            int left = i+1, right = n-1;
+            while(left < right)
             {
-                closest = abs(target - sum);
-                minSum = sum;
+                int sum = nums[i] + nums[left] + nums[right];
+                if(abs(target-sum) < abs(diff))
+                    diff = target - sum;
+                
+                if(sum <= target)
+                    left++;
+                else
+                    right--;
             }
         }
         
-        return minSum;
-    }
-    
-    int twoSumClosest(vector<int>& nums, int target, int start)
-    {
-        int left = start, right = nums.size() - 1;
-        int closest = INT_MAX;
-        int minSum;
-        
-        while(left < right)
-        {
-            int sum = nums[left] + nums[right];
-            
-            if(closest > abs(target - sum))
-            {
-                closest = abs(target - sum);
-                minSum = sum;
-            }
-            
-            if(sum < target)
-                left++;
-            else
-                right--;
-        }
-        
-        return minSum;
+        return target - diff;
     }
 };
