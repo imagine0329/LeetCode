d06158791a4292e7192954f69e253086f6cae7b0
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-04 16:05:43 +0900

    Time: 11 ms (47.85%), Space: 10.7 MB (96.95%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int> nums;
         
-        backtracking(candidates, target, 0, ans, nums);
+        backtracking(candidates, target, 0, 0, ans, nums);
         
-        return vector<vector<int>>(ans.begin(), ans.end());
+        return ans;
     }
     
-    void backtracking(vector<int>& candidates, int target, int sum, set<vector<int>>& ans, vector<int>& nums) {
+    void backtracking(vector<int>& candidates, int target, int sum, int start, vector<vector<int>>& ans, vector<int>& nums) {
         if(sum == target)
         {
-            vector<int> temp = nums;
-            sort(temp.begin(), temp.end());
-            ans.insert(temp);
+            ans.push_back(nums);
             return;
         }
         
-        for(int i=0; i<candidates.size(); i++)
+        for(int i=start; i<candidates.size(); i++)
         {
             if(sum + candidates[i] > target)
                 continue;
             
             sum += candidates[i];
             nums.push_back(candidates[i]);
-            backtracking(candidates, target, sum, ans, nums);
+            backtracking(candidates, target, sum, i, ans, nums);
             nums.pop_back();
             sum -= candidates[i];
         }
