271a1f6ff8d7559023b347e18ac1d4a515ae2629
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-05 12:03:32 +0900

    Time: 23 ms (30.09%), Space: 11 MB (22.61%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        
+        backtracking(nums, 0, ans);
+        
+        return ans;
+    }
+    
+    void backtracking(vector<int>& nums, int start, vector<vector<int>>& ans)
+    {
+        if(start == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        
+        unordered_set<int> s;
+        for(int i=start; i<nums.size(); i++)
+        {
+            if(s.find(nums[i]) != s.end())
+                continue;
+            
+            s.insert(nums[i]);
+            swap(nums[i], nums[start]);
+            backtracking(nums, start+1, ans);
+            swap(nums[i], nums[start]);
+        }
+    }
+};
