b651e23ac49c0d2110dc771b9fa13525df907e8e
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-07 15:26:24 +0900

    Time: 508 ms (41.52%), Space: 7.9 MB (45.76%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        for(int i=0; i<board.size(); i++)
+        
+        for(int row=0; row<board.size(); row++)
         {
-            for(int j=0; j<board[0].size(); j++)
+            for(int col=0; col<board.front().size(); col++)
             {
-                if(backtracking(board, word, i, j, 0))
+                if(backtracking(board, word, 0, row, col))
                     return true;
             }
         }
-        
         return false;
     }
     
-    bool backtracking(vector<vector<char>>& board, string word, int row, int col, int idx)
+    bool backtracking(vector<vector<char>>& board, string word, int idx, int row, int col)
     {
         if(idx == word.length())
             return true;
         
-        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size())
+        if(row < 0 || row >= board.size() || col < 0 || col >= board.front().size())
             return false;
         
-        if(board[row][col] != word[idx])
+        if(word[idx] != board[row][col])
             return false;
         
         board[row][col] = '#';
         
-        vector<int> offset = {-1, 0, 1, 0, -1};
-        bool ret;
+        int rowOffsets[] = {0, 1, 0, -1};
+        int colOffsets[] = {1, 0, -1, 0};
+        
         for(int i=0; i<4; i++)
         {
-            ret = backtracking(board, word, row+offset[i], col+offset[i+1], idx+1);
-            if(ret)
-                break;
+            if(backtracking(board, word, idx+1, row+rowOffsets[i], col+colOffsets[i]))
+                return true;
         }
         
         board[row][col] = word[idx];
-        return ret;
+        
+        return false;
     }
 };
