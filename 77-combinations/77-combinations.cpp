30864455e2b45a77c8cff828a5740798ec587822
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-07 12:54:24 +0900

    Time: 39 ms (35.08%), Space: 9.2 MB (64.12%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> comb;
+        
+        backtracking(ans, comb, n, k, 1);
+        
+        return ans;
+    }
+    
+    void backtracking(vector<vector<int>>& ans, vector<int>& comb, int n, int k, int start)
+    {
+        if(comb.size() == k)
+        {
+            ans.push_back(comb);
+            return;
+        }
+        
+        for(int i=start; i<=n; i++)
+        {
+            comb.push_back(i);
+            backtracking(ans, comb, n, k, i+1);
+            comb.pop_back();
+        }
+    }
+};
