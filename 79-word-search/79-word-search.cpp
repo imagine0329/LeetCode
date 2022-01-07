dd2ffedd4f8c28804fa0542633bc770427d91c55
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-07 15:27:57 +0900

    Time: 875 ms (10.37%), Space: 8 MB (18.23%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        
-        for(int row=0; row<board.size(); row++)
+        for(int i=0; i<board.size(); i++)
         {
-            for(int col=0; col<board.front().size(); col++)
+            for(int j=0; j<board[0].size(); j++)
             {
-                if(backtracking(board, word, 0, row, col))
+                if(backtracking(board, word, i, j, 0))
                     return true;
             }
         }
+        
         return false;
     }
     
-    bool backtracking(vector<vector<char>>& board, string word, int idx, int row, int col)
+    bool backtracking(vector<vector<char>>& board, string word, int row, int col, int idx)
     {
         if(idx == word.length())
             return true;
         
-        if(row < 0 || row >= board.size() || col < 0 || col >= board.front().size())
+        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size())
             return false;
         
-        if(word[idx] != board[row][col])
+        if(board[row][col] != word[idx])
             return false;
         
         board[row][col] = '#';
@@ -31,12 +31,11 @@ public:
         
         for(int i=0; i<4; i++)
         {
-            if(backtracking(board, word, idx+1, row+rowOffsets[i], col+colOffsets[i]))
+            if(backtracking(board, word, row+rowOffsets[i], col+colOffsets[i], idx+1))
                 return true;
         }
         
         board[row][col] = word[idx];
-        
         return false;
     }
 };
