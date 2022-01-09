94feb75c1ce9ba4c4fcecb2d9ecb97adbd66a743
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-09 11:26:46 +0900

    Time: 137 ms (34.38%), Space: 93.3 MB (52.51%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if(prices.size() == 0)
-            return 0;
-        
         int profit = 0;
         int min = INT_MAX;
         for(auto n : prices)
