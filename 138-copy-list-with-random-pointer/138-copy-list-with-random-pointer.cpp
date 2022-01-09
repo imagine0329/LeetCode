31444d86e056e44e9eb15ce128c792dbdd9cec5f
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-10 08:30:15 +0900

    Time: 12 ms (32.17%), Space: 11.6 MB (9.05%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -18,36 +18,20 @@ class Solution {
 private:
     unordered_map<Node*, Node*> m;
     
-    Node* getClonedNode(Node* node)
-    {
-        if(node == nullptr)
-            return nullptr;
-        
-        if(m.find(node) != m.end())
-            return m[node];
-        
-        m[node] = new Node(node->val);
-        
-        return m[node];
-    }
-    
 public:
     Node* copyRandomList(Node* head) {
         if(head == nullptr)
             return nullptr;
         
-        Node* newHead = new Node(head->val);
-        Node* node = newHead;
-        m[head] = newHead;
+        if(m.find(head) != m.end())
+            return m[head];
         
-        while(head)
-        {
-            node->next = getClonedNode(head->next);
-            node->random = getClonedNode(head->random);
-            node = node->next;
-            head = head->next;
-        }
+        Node* node = new Node(head->val);
+        m[head] = node;
         
-        return newHead;
+        node->next = copyRandomList(head->next);
+        node->random = copyRandomList(head->random);
+        
+        return node;
     }
 };
