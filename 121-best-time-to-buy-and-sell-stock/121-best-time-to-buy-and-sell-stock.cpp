52257d9a96f0730a79137b1763cd1bdb6af4684b
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-09 11:25:41 +0900

    Time: 203 ms (15.99%), Space: 93.3 MB (90.11%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,20 +4,15 @@ public:
         if(prices.size() == 0)
             return 0;
         
-        int n = prices.size();
-        vector<int> left_min(n), right_max(n);
-        left_min[0] = prices[0];
-        right_max[n-1] = prices[n-1];
-        
-        for(int i=1; i<n; i++)
-        {
-            left_min[i] = min(left_min[i-1], prices[i]);
-            right_max[n-i-1] = max(right_max[n-i], prices[n-i-1]);
-        }
-        
         int profit = 0;
-        for(int i=0; i<n; i++)
-            profit = max(profit, right_max[i] - left_min[i]);
+        int min = INT_MAX;
+        for(auto n : prices)
+        {
+            if(n > min)
+                profit = max(profit, n - min);
+            else
+                min = n;
+        }
         
         return profit;
     }
