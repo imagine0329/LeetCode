3d5dc3a836cd5a63748e7d06066372a457032b40
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-11 21:59:41 +0900

    Time: 40 ms (44.99%), Space: 15.7 MB (24.54%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,37 +1,78 @@
+class UnionFind {
+public:
+    UnionFind(vector<vector<char>>& grid) {
+        count = 0;
+        int m = grid.size(), n = grid[0].size();
+        
+        for(int row=0; row<m; row++) {
+            for(int col=0; col<n; col++) {
+                if(grid[row][col] == '1') {
+                    parent.push_back(row * n + col);
+                    ++count;
+                }
+                else
+                    parent.push_back(-1);
+                
+                rank.push_back(0);
+            }
+        }
+    }
+    
+    int find(int i) {
+        if(parent[i] != i)
+            parent[i] = find(parent[i]);
+        
+        return parent[i];
+    }
+    
+    void Union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        
+        if(rootX == rootY)
+            return;
+        
+        if(rank[rootX] > rank[rootY])
+            parent[rootY] = rootX;
+        else if(rank[rootX] < rank[rootY])
+            parent[rootX] = rootY;
+        else
+        {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+        
+        --count;
+    }
+    
+    int getCount() {
+        return count;
+    }
+    
+private:
+    vector<int> parent;
+    vector<int> rank;
+    int count;
+};
+
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        queue<pair<int, int>> q;
-        int islands = 0;
+        UnionFind uf(grid);
         
         for(int row=0; row<m; row++) {
             for(int col=0; col<n; col++) {
-                if(grid[row][col] == '1'){
-                    islands++;
-                    q.push({row, col});
-                    while(!q.empty())
-                    {
-                        int r = q.front().first, c = q.front().second;
-                        q.pop();
-                        
-                        if(r-1 >= 0 && grid[r-1][c] == '1') {
-                            q.push({r-1, c}); grid[r-1][c] = '0';
-                        }
-                        if(r+1 < m && grid[r+1][c] == '1') {
-                            q.push({r+1, c}); grid[r+1][c] = '0';
-                        }
-                        if(c-1 >= 0 && grid[r][c-1] == '1') {
-                            q.push({r, c-1}); grid[r][c-1] = '0';
-                        }
-                        if(c+1 < n && grid[r][c+1] == '1') {
-                            q.push({r, c+1}); grid[r][c+1] = '0';
-                        }
-                    }
+                if(grid[row][col] == '1') {
+                    grid[row][col] = '0';
+                    if(row-1 >= 0 && grid[row-1][col] == '1')   uf.Union(row*n+col, (row-1)*n+col);
+                    if(row+1 < m && grid[row+1][col] == '1')   uf.Union(row*n+col, (row+1)*n+col);
+                    if(col-1 >= 0 && grid[row][col-1] == '1')   uf.Union(row*n+col, row*n+col-1);
+                    if(col+1 < n && grid[row][col+1] == '1')   uf.Union(row*n+col, row*n+col+1);
                 }
             }
         }
         
-        return islands;
+        return uf.getCount();
     }
 };
