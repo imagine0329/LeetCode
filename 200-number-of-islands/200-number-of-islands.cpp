2c99b37fdcf957cb16bca08c3c1158e89b877497
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-11 21:11:55 +0900

    Time: 60 ms (22.58%), Space: 17.2 MB (23.06%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
         int islands = 0;
         
         for(int row=0; row<m; row++)
         {
             for(int col=0; col<n; col++)
             {
-                if(!visited[row][col] && grid[row][col] == '1')
+                if(grid[row][col] == '1')
                 {
                     islands++;
-                    dfs(grid, visited, row, col);
+                    dfs(grid, row, col);
                 }
             }
         }
@@ -20,21 +19,17 @@ public:
         return islands;
     }
     
-    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col)
+    void dfs(vector<vector<char>>& grid, int row, int col)
     {
         if(row == grid.size() || row < 0 || col == grid[0].size() || col < 0)
             return;
         
-        if(visited[row][col])
-            return;
-        
-        visited[row][col] = true;
-        
         if(grid[row][col] == '0')
             return;
         
+        grid[row][col] = '0';
         vector<int> offset = {-1, 0, 1, 0, -1};
         for(int i=0; i<4; i++)
-            dfs(grid, visited, row+offset[i], col+offset[i+1]);
+            dfs(grid, row+offset[i], col+offset[i+1]);
     }
 };
