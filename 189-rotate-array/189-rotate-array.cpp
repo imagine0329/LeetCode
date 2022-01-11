dd34af3a3bada157c4b14022e60abc15e8f7addb
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-11 13:26:57 +0900

    Time: 49 ms (17.16%), Space: 24.9 MB (92.74%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,10 +3,19 @@ public:
     void rotate(vector<int>& nums, int k) {
         k %= nums.size();
         
-        vector<int> extra(nums.size());
-        for(int i=0; i<nums.size(); i++)
-            extra[(i+k)%nums.size()] = nums[i];
-        
-        nums = extra;
+        int count = 0;
+        for(int start=0; count<nums.size(); start++)
+        {
+            int current_idx = start;
+            int prev_val = nums[start];
+            do{
+                int next_idx = (current_idx + k) % nums.size();
+                int next_val = nums[next_idx];
+                nums[next_idx] = prev_val;
+                prev_val = next_val;
+                current_idx = next_idx;
+                count++;
+            }while(current_idx != start);
+        }
     }
 };
