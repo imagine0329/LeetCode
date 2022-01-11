3846b80a646e3ab1237541decaca9a2c81e1c2d3
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-11 10:33:08 +0900

    Time: 10 ms (7.02%), Space: 5.9 MB (28.05%) - LeetHub

---
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count = 0;
+        
+        for(int i=5; i<=n; i+=5)
+        {
+            int curr = i;
+            while(curr % 5 == 0)
+            {
+                count++;
+                curr /= 5;
+            }
+        }
+        
+        return count;
+    }
+};
