37b461045bf92bae056104ff87b6eaaadbc38458
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-12 13:25:55 +0900

    Time: 35 ms (25.17%), Space: 14.1 MB (51.28%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> m;
-        vector<int> visited(numCourses, -1);
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+        queue<int> q;
         
-        for(auto p : prerequisites)
-            m[p[1]].push_back(p[0]);
-        
-        for(int curr=0; curr<numCourses; curr++) {
-            if(isCyclic(curr, m, visited))
-                return false;
+        for(auto p : prerequisites) {
+            graph[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
         }
         
-        return true;
-    }
-    
-    bool isCyclic(int curr, unordered_map<int, vector<int>>& m, vector<int>& visited)
-    {
-        if(visited[curr] == 1)
-            return false;
-        else if(visited[curr] == 0)
-            return true;
-        
-        if(m.find(curr) == m.end())
-            return false;
-        
-        visited[curr] = 0;
-        for(int i=0; i<m[curr].size(); i++)
-        {
-            if(isCyclic(m[curr][i], m, visited))
-                return true;
+        for(int i=0; i<numCourses; i++) {
+            if(indegree[i] == 0)
+                q.push(i);
         }
         
-        visited[curr] = 1;
-        return false;
+        int count = 0;
+        while(!q.empty()) {
+            int u = q.front();
+            q.pop();
+            count++;
+            
+            for(auto v : graph[u]) {
+                if(--indegree[v] == 0)
+                    q.push(v);
+            }
+        }
+        
+        return count == numCourses;
     }
 };
