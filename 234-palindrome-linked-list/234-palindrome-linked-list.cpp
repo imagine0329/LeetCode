1cedd4d4557707098fe0e0b306a39e974336375e
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-13 13:29:32 +0900

    Time: 487 ms (5.24%), Space: 128.3 MB (11.72%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -13,49 +13,19 @@ private:
     ListNode* front;
 public:
     bool isPalindrome(ListNode* head) {
-        if(head == nullptr)
-            return true;
-        ListNode* node = head;
-        int len = 0;
+        vector<int> v;
         
-        while(node) {
-            len++;
-            node = node->next;
+        while(head) {
+            v.push_back(head->val);
+            head = head->next;
         }
         
-        if(len == 1)
-            return true;
-        
-        int n;
-        if(len & 1) n = len/2 + 1;
-        else n = len/2;
-        
-        node = head;
-        while(n--)
-            node = node->next;
-        
-        ListNode* reversed = reverseNode(head, len/2);
-        
-        while(node && reversed) {
-            if(node->val != reversed->val)
+        int left = 0, right = v.size() - 1;
+        while(left < right) {
+            if(v[left++] != v[right--])
                 return false;
-            
-            node = node->next;
-            reversed = reversed->next;
         }
         
-        return node == nullptr && reversed == nullptr;
-    }
-    
-    ListNode* reverseNode(ListNode* head, int length) {
-        ListNode* prev = nullptr;
-        while(length--) {
-            ListNode* next = head->next;
-            head->next = prev;
-            prev = head;
-            head = next;
-        }
-        
-        return prev;
+        return true;
     }
 };
