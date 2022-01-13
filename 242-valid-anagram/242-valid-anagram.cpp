5f3c0a5c311af62e8c75de2938c12491552e051b
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-13 16:40:37 +0900

    Time: 7 ms (86.13%), Space: 7.3 MB (45.11%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        if(s.length() != t.length())
+            return false;
+        
+        unordered_map<char, int> m;
+        
+        for(auto c : s)
+            m[c - 'a']++;
+        
+        for(auto c : t)
+        {
+            if(m[c - 'a']-- == 0)
+                return false;
+        }
+        
+        return true;
     }
 };
