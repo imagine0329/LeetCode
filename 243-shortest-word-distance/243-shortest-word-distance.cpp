3f0807449775ad25f271d0c499214ef86a0467c8
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-13 16:27:35 +0900

    Time: 19 ms (19.04%), Space: 11.7 MB (8.46%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.cpp
+++ 243-shortest-word-distance/243-shortest-word-distance.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+        int l1 = -1, l2 = -1;
         int shortest = INT_MAX;
         
         for(int i=0; i<wordsDict.size(); i++) {
-            int j = -1;
             if(wordsDict[i] == word1)
-                j = getWordIdx(wordsDict, word2, i+1);
+                l1 = i;
             else if(wordsDict[i] == word2)
-                j = getWordIdx(wordsDict, word1, i+1);
+                l2 = i;
             
-            if(j != -1)
-                shortest = min(shortest, j - i);
+            if(l1 != -1 && l2 != -1)
+                shortest = min(shortest, abs(l1 - l2));
         }
-        
+            
         return shortest;
-    }
-    
-    int getWordIdx(vector<string>& wordsDict, string word, int start)
-    {
-        for(int i=start; i<wordsDict.size(); i++) {
-            if(wordsDict[i] == word)
-                return i;
-        }
-        
-        return -1;
     }
 };
