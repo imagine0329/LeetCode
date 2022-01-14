cf7429cae0142b2b4e456d9ef0eb15fe50461c7f
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-14 21:37:22 +0900

    Time: 50 ms (6.53%), Space: 17.9 MB (93.72%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int missing = nums.size();
-        for(int i=0; i<nums.size(); i++)
-            missing ^= i ^ nums[i];
+        int n = nums.size();
+        int expected = (n * (n + 1)) / 2;
+        int actual = 0;
+        for(auto i : nums)
+            actual += i;
         
-        return missing;
+        return expected - actual;
     }
 };
