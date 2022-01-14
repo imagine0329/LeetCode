0b711763ee88d8b8c00604b090eab675e90aa94e
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-14 15:57:02 +0900

    Time: 34 ms (19.64%), Space: 12.4 MB (48.03%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.cpp
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -1,35 +1,62 @@
-class Solution {
+class UnionFind {
+private:
+    vector<int> parent;
+    vector<int> rank;
+    int count;
+    
 public:
-    bool validTree(int n, vector<vector<int>>& edges) {
-        unordered_map<int, vector<int>> m;
-        vector<bool> visited(n, false);
+    UnionFind(int n){
+        parent = vector<int>(n);
+        rank = vector<int>(n);
+        count = n;
         
-        for(auto e : edges) {
-            m[e[0]].push_back(e[1]);
-            m[e[1]].push_back(e[0]);
+        for(int i=0; i<n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
         }
+    }
+    
+    int find(int n) {
+        if(parent[n] != n)
+            return parent[n] = find(parent[n]);
         
-        if(dfs(0, -1, m, visited) == false)
+        return n;
+    }
+    
+    bool Union(int x, int y) {
+        int xp = find(x);
+        int yp = find(y);
+        
+        if(xp == yp)
             return false;
         
-        for(auto v : visited) {
-            if(v != 1)
-                return false;
+        if(rank[xp] >= rank[yp]) {
+            parent[yp] = xp;
+            rank[xp]++;
         }
-        
+        else {
+            parent[xp] = yp;
+            rank[yp]++;
+        }
+            
+        count--;
         return true;
     }
     
-    bool dfs(int n, int parent, unordered_map<int, vector<int>>& m, vector<bool>& visited) {
-        if(visited[n])
-            return false;
-        
-        visited[n] = true;
-        for(auto adj : m[n]) {
-            if(parent != adj && dfs(adj, n, m, visited) == false)
+    int getCount() {
+        return count;
+    }
+};
+
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        UnionFind uf(n);
+        for(auto e : edges) {
+            if(uf.Union(e[0], e[1]) == false)
                 return false;
         }
         
-        return true;
+        return uf.getCount() == 1;
     }
 };
