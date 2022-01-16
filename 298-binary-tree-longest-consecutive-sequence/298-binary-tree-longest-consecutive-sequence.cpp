a7e24623d6a81c423158186c4c71a58f139e2a95
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-17 08:11:39 +0900

    Time: 24 ms (98.40%), Space: 32.7 MB (26.78%) - LeetHub

--- 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.cpp
+++ 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.cpp
@@ -13,35 +13,29 @@
 class Solution {
 public:
     int longestConsecutive(TreeNode* root) {
-        queue<pair<TreeNode*, int>> q;
         int longest = 0;
-        
-        q.push({root, 1});
-        while(!q.empty() && root) {
-            int n = q.size();
-            
-            while(n--) {
-                root = q.front().first;
-                int len = q.front().second;
-                q.pop();
-                longest = max(longest, len);
-                
-                if(root->left) {
-                    if(root->left->val == root->val + 1)
-                        q.push({root->left, len + 1});
-                    else
-                        q.push({root->left, 1});
-                }
-                
-                if(root->right) {
-                    if(root->right->val == root->val + 1)
-                        q.push({root->right, len + 1});
-                    else
-                        q.push({root->right, 1});
-                }
-            }
-        }
+        dfs(root, 1, longest);
         
         return longest;
+    }
+    
+    void dfs(TreeNode* root, int len, int& longest) {
+        if(root == nullptr)
+            return;
+        
+        longest = max(longest, len);
+        int left_l, right_l;
+        if(root->left && root->left->val == root->val + 1)
+            left_l = len + 1;
+        else
+            left_l = 1;
+        
+        if(root->right && root->right->val == root->val + 1)
+            right_l = len + 1;
+        else
+            right_l = 1;
+            
+        dfs(root->left, left_l, longest);
+        dfs(root->right, right_l, longest);
     }
 };
