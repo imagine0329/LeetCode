74af81f8a5c10cee63313d69394ac16d2d4849d1
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-16 13:16:07 +0900

    Time: 212 ms (21.49%), Space: 61.3 MB (59.14%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,37 +1,19 @@
 class Solution {
-private:
-    int calcMaxBit(int n) {
-        int bits = 0;
-        while(n > 0) {
-            n /= 2;
-            bits++;
-        }
-        
-        return bits;
-    }
-    
 public:
     int findDuplicate(vector<int>& nums) {
-        int duplicate = 0;
-        int n = nums.size() - 1;
-        int max_num = *max_element(nums.begin(), nums.end());
-        int max_bit = calcMaxBit(max_num);
+        int slow = nums[0], fast = nums[0];
         
-        for(int bit = 0; bit < max_bit; bit++) {
-            int mask = 1 << bit;
-            int base_count = 0, nums_count = 0;
-            for(int i = 0; i <= n; i++) {
-                if(i & mask)
-                    base_count++;
-                if(nums[i] & mask)
-                    nums_count++;
-            }
-            
-            if(base_count < nums_count)
-                duplicate |= mask;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
         
-        return duplicate;
-        
+        return slow;
     }
 };
