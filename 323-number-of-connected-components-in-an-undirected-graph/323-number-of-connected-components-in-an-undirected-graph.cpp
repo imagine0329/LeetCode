62718d7c4d235f6931041210cf2a14d3a3edb5c4
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-17 13:27:04 +0900

    Time: 38 ms (20.55%), Space: 14.4 MB (28.15%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,59 +1,35 @@
-class UnionFind {
-private:
-    vector<int> parent;
-    vector<int> rank;
-    int count;
-    
-public:
-    UnionFind(int n) {
-        parent = vector<int>(n);
-        rank = vector<int>(n, 0);
-        count = n;
-        
-        for(int i = 0; i < n; i++)
-            parent[i] = i;
-    }
-    
-    int find(int n) {
-        if(parent[n] != n)
-            return parent[n] = find(parent[n]);
-        
-        return n;
-    }
-    
-    void Union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        
-        if(x == y)
-            return;
-        
-        if(rank[x] > rank[y]){
-            parent[y] = x;
-            rank[x]++;
-        }
-        else{
-            parent[x] = y;
-            rank[y]++;
-        }
-        
-        count--;
-    }
-    
-    int getCount() {
-        return count;
-    }
-};
-
 class Solution {
 public:
     int countComponents(int n, vector<vector<int>>& edges) {
-        UnionFind uf(n);
+        vector<vector<int>> graph(n);
+        vector<bool> visited(n, false);
         
-        for(auto e : edges)
-            uf.Union(e[0], e[1]);
+        for(auto e : edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
         
-        return uf.getCount();
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(visited[i] == false) {
+                count++;
+                dfs(graph, visited, i);
+            }
+        }
+        
+        return count;
+    }
+    
+    void dfs(vector<vector<int>>& graph, vector<bool>& visited, int i) {
+        if(visited[i])
+            return;
+        
+        visited[i] = true;
+        
+        for(int j = 0; j < graph[i].size(); j++) {
+            if(visited[graph[i][j]] == false)
+                dfs(graph, visited, graph[i][j]);
+        }
     }
 };
 
