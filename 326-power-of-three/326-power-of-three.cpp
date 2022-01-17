08886127492fa850f4b041534a7d524ad651cb61
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-17 16:08:33 +0900

    Time: 136 ms (5.56%), Space: 7 MB (8.02%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n == 0)
-            return false;
+        string converted = convertBase(n);
+        
+        if(converted.front() == '1') {
+            string temp = converted.substr(1, converted.length()-1);
+            if(temp.size() == 0 || stoll(temp) == 0)
+                return true;
+        }
+        
+        return false;
+    }
+    
+    string convertBase(int n) {
+        string str;
         
         while(n) {
-            if(n != 1 && n % 3 != 0)
-                return false;
+            str = to_string(n % 3) + str;
             n /= 3;
         }
         
-        return true;
+        return str;
     }
 };
