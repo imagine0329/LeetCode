51bc8d6ea0e10156132fd12cbcd9b933802b4a3d
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-18 06:58:41 +0900

    Time: 13 ms (12.95%), Space: 7.9 MB (73.05%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> ret(n + 1, 0);
+        int x = 0, b = 1;
         
-        for(int i = 0; i <= n; i++)
-            ret[i] = popCount(i);
-        
-        return ret;
-    }
-    
-    int popCount(int n) {
-        int count = 0;
-        while(n) {
-            n &= n - 1;
-            count++;
+        while(b <= n) {
+            while(x < b && x + b <= n) {
+                ret[x + b] = ret[x] + 1;
+                x++;
+            }
+            x = 0;
+            b <<= 1;
         }
         
-        return count;
+        return ret;
     }
 };
