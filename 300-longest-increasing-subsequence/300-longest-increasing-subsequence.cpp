73c1bd782dd105dc727bd9117b33b04910a72913
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-17 09:35:49 +0900

    Time: 8 ms (90.72%), Space: 10.5 MB (63.10%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -8,15 +8,25 @@ public:
             if(sub.back() < nums[i])
                 sub.push_back(nums[i]);
             else {
-                for(auto& s : sub) {
-                    if(s >= nums[i]) {
-                        s = nums[i];
-                        break;
-                    }
-                }
+                int idx = binarySearch(sub, nums[i]);
+                sub[idx] = nums[i];
             }
         }
         
         return sub.size();
+    }
+    
+    int binarySearch(vector<int>& sub, int target) {
+        int left = 0, right = sub.size() - 1;
+        
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(sub[mid] >= target)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        
+        return left;
     }
 };
