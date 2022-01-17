e39ce28697de22735d0fbf15845cd24cbe9b4ebf
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-17 21:03:30 +0900

    Time: 60 ms (79.63%), Space: 61.6 MB (70.08%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        vector<int> v;
+        int first = INT_MAX, second = INT_MAX;
         
-        v.push_back(nums[0]);
-        for(int i = 1; i < nums.size(); i++) {
-            if(v.back() >= nums[i]) {
-                int idx = binarySearch(v, nums[i]);
-                v[idx] = nums[i];
-            }
+        for(auto n : nums) {
+            if(n <= first)
+                first = n;
+            else if(n <= second)
+                second = n;
             else
-                v.push_back(nums[i]);
+                return true;
         }
         
-        return v.size() >= 3;
-    }
-    
-    int binarySearch(vector<int>& v, int target) {
-        int left = 0, right = v.size() - 1;
-        
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-            if(v[mid] < target)
-                left = mid + 1;
-            else
-                right = mid;
-        }
-        
-        return left;
+        return false;
     }
 };
