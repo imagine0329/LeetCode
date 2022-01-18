080f8f67d61d4c67245cffcd825da953834ab7af
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-18 10:41:31 +0900

    Time: 39 ms (30.72%), Space: 19.4 MB (40.90%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.cpp
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.cpp
@@ -1,61 +1,33 @@
 class TicTacToe {
 private:
-    vector<vector<int>> board;
+    vector<int> rows, cols;
+    int diagonal, antidiagonal;
     int n;
-    
-    bool checkVertical(int col, int player) {
-        for(int row = 0; row < n; row++) {
-            if(board[row][col] != player)
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool checkHorizontal(int row, int player) {
-        for(int col = 0; col < n; col++) {
-            if(board[row][col] != player)
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool checkDiagonal(int player) {
-        for(int i = 0; i < n; i++) {
-            if(board[i][i] != player)
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool checkAntidiagonal(int player) {
-        for(int row = 0; row < n; row++) {
-            if(board[row][n - row - 1] != player)
-                return false;
-        }
-        
-        return true;
-    }
     
 public:
     TicTacToe(int n) {
-        board = vector<vector<int>>(n, vector<int>(n, 0));
+        rows = vector<int>(n, 0);
+        cols = vector<int>(n, 0);
+        diagonal = 0;
+        antidiagonal = 0;
         this->n = n;
     }
     
     int move(int row, int col, int player) {
-        board[row][col] = player;
+        int add = player == 1 ? 1 : -1;
         
-        if(checkVertical(col, player) || checkHorizontal(row, player))
-            return player;
+        rows[row] += add;
+        cols[col] += add;
         
-        if(row == col && checkDiagonal(player))
-            return player;
-        if(col == n - row - 1 && checkAntidiagonal(player))
-            return player;
+        if(row == col)
+            diagonal += add;
+        if(col == n - row - 1)
+            antidiagonal += add;
         
+        int val = add * n;
+        if(rows[row] == val || cols[col] == val || diagonal == val || antidiagonal == val)
+            return player;
+            
         return 0;
     }
 };
