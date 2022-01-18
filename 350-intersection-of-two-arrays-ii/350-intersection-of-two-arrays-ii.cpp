5e61c374471c783db969556365faf1ea6bace584
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-18 11:36:30 +0900

    Time: 4 ms (88.56%), Space: 10.4 MB (51.24%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-        nums1.erase(set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), nums1.begin()), nums1.end());
-        return nums1;
+        if(nums1.size() > nums2.size())
+            return intersect(nums2, nums1);
+        
+        unordered_map<int, int> m;
+        for(auto n : nums1)
+            m[n]++;
+        
+        int k = 0;
+        for(auto n : nums2) {
+            auto it = m.find(n);
+            if(it != m.end() && --it->second >= 0)
+                nums1[k++] = n;
+        }
+        
+        return vector<int>(nums1.begin(), nums1.begin() + k);
     }
 };
