9e5ddb0c4df096cd3f7f7235b3eb42a9293edda1
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-18 15:37:28 +0900

    Time: 4 ms (15.85%), Space: 5.7 MB (99.11%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        return fmod(sqrt(num), 1) == 0;
+        int left = 1, right = num / 2;
+        
+        while(left < right) {
+            long long mid = left + (right - left) / 2;
+            if(mid * mid < num)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        
+        return (long long)left * left == num;
     }
 };
