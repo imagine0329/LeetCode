717871ea4b36b2a17d904589d3755d9537af9c26
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-20 07:47:30 +0900

    Time: 53 ms (35.67%), Space: 32.6 MB (42.64%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -17,9 +17,29 @@ public:
         if(root2 == nullptr)
             return root1;
         
-        root1->val += root2->val;
-        root1->left = mergeTrees(root1->left, root2->left);
-        root1->right = mergeTrees(root1->right, root2->right);
+        queue<pair<TreeNode*, TreeNode*>> q;
+        q.push({root1, root2});
+        
+        while(!q.empty()) {
+            auto t1  = q.front().first;
+            auto t2 = q.front().second;
+            q.pop();
+            
+            if(!t1 || !t2)
+                continue;
+            
+            t1->val += t2->val;
+            
+            if(!t1->left)
+                t1->left = t2->left;
+            else
+                q.push({t1->left, t2->left});
+            
+            if(!t1->right)
+                t1->right = t2->right;
+            else
+                q.push({t1->right, t2->right});
+        }
         
         return root1;
     }
