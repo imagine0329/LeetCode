2b31ccd864b62805ec80b0613e4063898ffe4e8a
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-19 11:48:19 +0900

    Time: 483 ms (12.86%), Space: 25.3 MB (16.61%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        int count = 0;
         unordered_map<int, int> m;
-        
-        for(int i = 0; i < nums1.size(); i++) {
-            for(int j = 0; j < nums2.size(); j++) {
-                m[nums1[i] + nums2[j]]++;
-            }
+        vector<vector<int>> lists({nums1, nums2, nums3, nums4});
+        addToHash(lists, m, 0, 0);
+        return countComplements(lists, m, lists.size()/2, 0);
+    }
+    
+    void addToHash(vector<vector<int>>& lists, unordered_map<int, int>& m, int i, int sum) {
+        if(i == lists.size() / 2)
+            ++m[sum];
+        else {
+            for(auto a : lists[i])
+                addToHash(lists, m, i+1, sum + a);
+        }
+    }
+    
+    int countComplements(vector<vector<int>>& lists, unordered_map<int, int>& m, int i, int complement) {
+        if(i == lists.size()) {
+            auto it = m.find(complement);
+            return it == m.end() ? 0 : it->second;
         }
         
-        for(int k = 0; k < nums3.size(); k++) {
-            for(int l = 0; l < nums4.size(); l++) {
-                int val = -nums3[k] - nums4[l];
-                if(m.find(val) != m.end())
-                    count += m[val];
-            }
-        }
+        int cnt = 0;
+        for(auto a : lists[i])
+            cnt += countComplements(lists, m, i + 1, complement - a);
         
-        return count;
+        return cnt;
     }
 };
