998cdee0bf9249f75e02b4483d7cb9045e3a589d
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-19 16:55:51 +0900

    Time: 148 ms (50.17%), Space: 67.8 MB (36.98%) - LeetHub

--- 582-kill-process/582-kill-process.cpp
+++ 582-kill-process/582-kill-process.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     vector<int> killProcess(vector<int>& pid, vector<int>& ppid, int kill) {
         unordered_map<int, vector<int>> m;
+        vector<int> ans;
+        queue<int> q;
+        
         for(int i = 0; i < ppid.size(); i++)
             m[ppid[i]].push_back(pid[i]);
         
-        vector<int> ans;
-        vector<bool> visited(pid.size(), false);
-        dfs(m, ans, visited, kill);
+        q.push(kill);
+        while(!q.empty()) {
+            int n = q.front();
+            q.pop();
+            ans.push_back(n);
+            for(auto k : m[n])
+                q.push(k);
+        }
         
         return ans;
-    }
-    
-    void dfs(unordered_map<int, vector<int>>& m, vector<int>& ans, vector<bool>& visited, int kill) {
-        
-        visited[kill] = true;
-        ans.push_back(kill);
-        for(int i = 0; i < m[kill].size(); i++) {
-            if(visited[m[kill][i]] == false) 
-                dfs(m, ans, visited, m[kill][i]);
-        }
     }
 };
