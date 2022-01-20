06e03af6f030976509aa58e186ca4f6a726d15c0
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-21 07:42:31 +0900

    Time: 7 ms (84.99%), Space: 14.1 MB (39.62%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int oldColor = image[sr][sc];
+        if(oldColor == newColor)
+            return image;
         
-        if(image[sr][sc] != newColor)
-            dfs(image, sr, sc, image[sr][sc], newColor);
+        vector<int> offset = {-1, 0, 1, 0, -1};
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        
+        while(!q.empty()) {
+            int n = q.size();
+            while(n--) {
+                sr = q.front().first;
+                sc = q.front().second;
+                q.pop();
+                
+                image[sr][sc] = newColor;
+                
+                for(int i = 0; i < 4; i++) {
+                    int r = sr + offset[i], c = sc + offset[i+1];
+                    if(r < 0 || r >= image.size() || c < 0 || c >= image[0].size())
+                        continue;
+                    if(image[r][c] == oldColor)
+                        q.push({r, c});
+                }
+            }
+        }
+        
         
         return image;
-    }
-    
-    void dfs(vector<vector<int>>& image, int sr, int sc, int oldColor, int newColor) {
-        vector<int> offset = {-1, 0, 1, 0, -1};
-        
-        image[sr][sc] = newColor;
-        for(int i = 0; i < 4; i++) {
-            int r = sr + offset[i], c = sc + offset[i+1];
-            if(r < 0 || r >= image.size() || c < 0 || c >= image[0].size())
-                continue;
-            if(image[r][c] == oldColor)
-                dfs(image, r, c, oldColor, newColor);
-        }
     }
 };
