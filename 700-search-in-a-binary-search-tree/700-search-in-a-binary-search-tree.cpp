df692412ddcd4e819289c5ccff631d4c64eb740f
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-20 15:54:33 +0900

    Time: 79 ms (11.80%), Space: 34.8 MB (22.54%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -12,18 +12,14 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        return dfs(root, val);
-    }
-    
-    TreeNode* dfs(TreeNode* root, int val) {
         if(root == nullptr)
             return nullptr;
         
         if(root->val == val)
             return root;
         
-        auto left = dfs(root->left, val);
-        auto right = dfs(root->right, val);
+        auto left = searchBST(root->left, val);
+        auto right = searchBST(root->right, val);
         
         return left ? left : right;
     }
