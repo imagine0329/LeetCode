20a46da010929c8b960a372a083d3636a02cd69f
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-22 08:12:40 +0900

    Time: 133 ms (30.24%), Space: 46.4 MB (23.91%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,22 +1,23 @@
-// quick sort
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
+        mergeSort(nums);
         return nums;
     }
     
 private:
-    void mergeSort(vector<int>& nums, int left, int right) {
-        if(left >= right) return;
-        int mid = left + (right - left) / 2;
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid + 1, right);
-        merge(nums, left, right);
+    void mergeSort(vector<int>& nums) {
+        int n = nums.size();
+        for(int size = 1; size < n; size *= 2) {
+            for(int left = 0; left < n - size; left += 2 * size) {
+                int mid = left + size - 1;
+                int right = min(left + (2 * size) - 1, n - 1);
+                merge(nums, left, mid, right);
+            }
+        }
     }
     
-    void merge(vector<int>& nums, int left, int right) {
-        int mid = left + (right - left) / 2;
+    void merge(vector<int>& nums, int left, int mid, int right) {
         vector<int> temp(right - left + 1);
         int i = left, j = mid + 1, k = 0;
         while(i <= mid || j <= right) {
