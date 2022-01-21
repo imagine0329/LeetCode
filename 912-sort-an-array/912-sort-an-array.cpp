19bc416e9c0d3513a6dfdbeef51e23d3b1dd65f2
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-22 07:49:31 +0900

    Time: 136 ms (29.73%), Space: 72.1 MB (18.82%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -2,32 +2,30 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        quickSort(nums, 0, nums.size() - 1);
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
     }
     
 private:
-    void quickSort(vector<int>& nums, int left, int right) {
-        if(left >= right)
-            return;
-        int mid = partition(nums, left, right);
-        quickSort(nums, left, mid);
-        quickSort(nums, mid + 1, right);
+    void mergeSort(vector<int>& nums, int left, int right) {
+        if(left >= right) return;
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, right);
     }
     
-    int partition(vector<int>& nums, int left, int right) {
+    void merge(vector<int>& nums, int left, int right) {
         int mid = left + (right - left) / 2;
-        swap(nums[left], nums[mid]);
-        int pivot = nums[left];
-        
-        while(left < right) {
-            while(left < right && nums[right] >= pivot) right--;
-            nums[left] = nums[right];
-            while(left < right && nums[left] <= pivot) left++;
-            nums[right] = nums[left];
+        vector<int> temp(right - left + 1);
+        int i = left, j = mid + 1, k = 0;
+        while(i <= mid || j <= right) {
+            if(i > mid || (j <= right && nums[i] > nums[j]))
+                temp[k++] = nums[j++];
+            else
+                temp[k++] = nums[i++];
         }
         
-        nums[left] = pivot;
-        return left;
+        copy(temp.begin(), temp.end(), nums.begin() + left);
     }
 };
