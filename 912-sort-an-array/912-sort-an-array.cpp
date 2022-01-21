aeb89cacc6e43da7337aebbf8eda53f526338a81
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-21 21:14:37 +0900

    Time: 112 ms (35.65%), Space: 28.5 MB (53.77%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,16 +1,33 @@
+// quick sort
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        priority_queue<int, vector<int>, greater<int>> q;
+        quickSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+    
+private:
+    void quickSort(vector<int>& nums, int left, int right) {
+        if(left >= right)
+            return;
+        int mid = partition(nums, left, right);
+        quickSort(nums, left, mid);
+        quickSort(nums, mid + 1, right);
+    }
+    
+    int partition(vector<int>& nums, int left, int right) {
+        int mid = left + (right - left) / 2;
+        swap(nums[left], nums[mid]);
+        int pivot = nums[left];
         
-        for(auto n : nums)
-            q.push(n);
-        
-        for(auto& n : nums) {
-            n = q.top();
-            q.pop();
+        while(left < right) {
+            while(left < right && nums[right] >= pivot) right--;
+            nums[left] = nums[right];
+            while(left < right && nums[left] <= pivot) left++;
+            nums[right] = nums[left];
         }
         
-        return nums;
+        nums[left] = pivot;
+        return left;
     }
 };
