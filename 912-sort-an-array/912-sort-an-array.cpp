5e9882c81a9064cb4883157e23f53be76f04b145
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-21 15:30:07 +0900

    Time: 158 ms (24.99%), Space: 30.4 MB (32.16%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        priority_queue<int, vector<int>, greater<int>> q;
+        
+        for(auto n : nums)
+            q.push(n);
+        
+        for(auto& n : nums) {
+            n = q.top();
+            q.pop();
+        }
+        
         return nums;
     }
 };
