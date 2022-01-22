ca1dc355d61abd87f05f1366a2d3bff584c85dba
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-22 13:59:18 +0900

    Time: 547 ms (11.90%), Space: 57.1 MB (59.32%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, int>> q;
+        sort(points.begin(), points.end(), [&](vector<int>& a, vector<int>& b) {
+            return squareDistance(a) < squareDistance(b);
+        });
         
-        for(int i = 0; i < points.size(); i++) {
-            int x = points[i][0], y = points[i][1];
-            int area = (x * x) + (y * y);
-            q.push({area, i});
-            if(q.size() > k)
-                q.pop();
-        }
-        
-        vector<vector<int>> ans;
-        for(int i = 0; i < k; i++) {
-            ans.push_back(points[q.top().second]);
-            q.pop();
-        }
-        
-        return ans;
+        return vector<vector<int>>(points.begin(), points.begin() + k);
+    }
+    
+private:
+    int squareDistance(vector<int>& point) {
+        return point[0] * point[0] + point[1] * point[1];
     }
 };
