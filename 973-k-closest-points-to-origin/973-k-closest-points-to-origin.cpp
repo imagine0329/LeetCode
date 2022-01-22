3ebb020aad07f6071de617d5668fb20c367c6e44
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-22 15:04:30 +0900

    Time: 242 ms (42.08%), Space: 56.9 MB (60.19%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,14 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        sort(points.begin(), points.end(), [&](vector<int>& a, vector<int>& b) {
-            return squareDistance(a) < squareDistance(b);
-        });
+        return quickSelect(points, k);
+    }
+    
+private:
+    vector<vector<int>> quickSelect(vector<vector<int>>& points, int k) {
+        int left = 0, right = points.size() - 1;
+        int pivot = points.size();
+        while(pivot != k) {
+            pivot = partition(points, left, right);
+            if(pivot < k)
+                left = pivot;
+            else
+                right = pivot - 1;
+        }
         
         return vector<vector<int>>(points.begin(), points.begin() + k);
     }
     
-private:
+    int partition(vector<vector<int>>& points, int left, int right) {
+        vector<int>& pivot = choosePivot(points, left, right);
+        int pivotDist = squareDistance(pivot);
+        while(left < right) {
+            if(squareDistance(points[left]) >= pivotDist) {
+                swap(points[left], points[right]);
+                right--;
+            }
+            else
+                left++;
+        }
+        
+        if(squareDistance(points[left]) < pivotDist)
+            left++;
+        
+        return left;
+    }
+    
+    vector<int>& choosePivot(vector<vector<int>>& points, int left, int right) {
+        return points[left + (right - left) / 2];
+    }
+    
     int squareDistance(vector<int>& point) {
         return point[0] * point[0] + point[1] * point[1];
     }
