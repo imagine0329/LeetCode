6b75be35a047ecc17d3edd2c80c6311e2f14d5e1
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-23 09:24:40 +0900

    Time: 26 ms (23.73%), Space: 10.9 MB (34.93%) - LeetHub

--- 1086-high-five/1086-high-five.cpp
+++ 1086-high-five/1086-high-five.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> highFive(vector<vector<int>>& items) {
-        sort(items.begin(), items.end(), [](vector<int>& a, vector<int>& b) {
-            if(a[0] != b[0])
-                return a[0] < b[0];
-            return a[1] > b[1];
-        });
+        map<int, priority_queue<int, vector<int>, greater<int>>> m;
+        
+        for(auto item : items) {
+            int id = item[0];
+            int score = item[1];
+            
+            m[id].push(score);
+            if(m[id].size() > 5)
+                m[id].pop();
+        }
         
         vector<vector<int>> ans;
-        int i = 0;
-        int n = items.size();
-        while(i < n) {
-            int id = items[i][0];
+        for(auto n : m) {
             int sum = 0;
-            for(int k = i; k < i + 5; k++)
-                sum += items[k][1];
-            
-            ans.push_back({id, sum / 5});
-            
-            while(i < n && items[i][0] == id)
-                i++;
+            for(int i = 0; i < 5; i++) {
+                sum += n.second.top();
+                n.second.pop();
+            }
+            ans.push_back({n.first, sum / 5});
         }
         
         return ans;
