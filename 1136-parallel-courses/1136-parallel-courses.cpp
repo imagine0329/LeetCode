0842297c5a4ad85df5a314ef798029a9b0ccef44
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-24 06:37:37 +0900

    Time: 56 ms (91.15%), Space: 26.5 MB (85.84%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.cpp
+++ 1136-parallel-courses/1136-parallel-courses.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     int minimumSemesters(int n, vector<vector<int>>& relations) {
-        unordered_map<int, vector<int>> courses;
-        vector<int> visited(n + 1, 0);
+        vector<vector<int>> courses(n + 1);
+        vector<int> indegree(n + 1, 0);
+        queue<int> q;
         
-        for(auto& relation : relations)
+        for(auto& relation : relations) {
             courses[relation[0]].push_back(relation[1]);
-        
-        int count = 1;
-        for(int v = 1; v <= n; v++) {
-            int length = dfs(courses, visited, v);
-            if(length == -1)
-                return -1;
-            count = max(count, length);
+            ++indegree[relation[1]];
         }
         
-        return count;
-    }
-    
-    int dfs(unordered_map<int, vector<int>>& courses, vector<int>& visited, int v) {
-        if(visited[v] != 0)
-            return visited[v];
-        else
-            visited[v] = -1;
-        
-        int count = 1;
-        for(int i = 0; i < courses[v].size(); i++) {
-            int length = dfs(courses, visited, courses[v][i]);
-            if(length == -1)
-                return -1;
-            count = max(count, length + 1);
+        for(int i = 1; i < indegree.size(); i++) {
+            if(indegree[i] == 0)
+                q.push(i);
         }
         
-        return visited[v] = count;
+        int count = 0, visited = 0;
+        while(!q.empty()) {
+            count++;
+            int n = q.size();
+            while(n--) {
+                int v = q.front();
+                q.pop();
+                
+                visited++;
+                for(auto c : courses[v]) {
+                    if(--indegree[c] == 0)
+                        q.push(c);
+                }
+            }
+        }
+        
+        return visited == n ? count : -1;
     }
 };
