4853d8c89466e708094c13cbc9269f8b3ecd04c5
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-24 17:05:37 +0900

    Time: 423 ms (44.22%), Space: 121.4 MB (86.67%) - LeetHub

---
+++ 1429-first-unique-number/1429-first-unique-number.cpp
@@ -0,0 +1,37 @@
+class FirstUnique {
+private:
+    unordered_map<int, bool> isUnique;
+    queue<int> q;
+    
+public:
+    FirstUnique(vector<int>& nums) {
+        for(auto n : nums)
+            add(n);
+    }
+    
+    int showFirstUnique() {
+        while(!q.empty() && !isUnique[q.front()])
+            q.pop();
+        
+        if(!q.empty())
+            return q.front();
+        
+        return -1;
+    }
+    
+    void add(int value) {
+        if(isUnique.find(value) == isUnique.end()) {
+            isUnique[value] = true;
+            q.push(value);
+        }
+        else
+            isUnique[value] = false;
+    }
+};
+
+/**
+ * Your FirstUnique object will be instantiated and called as such:
+ * FirstUnique* obj = new FirstUnique(nums);
+ * int param_1 = obj->showFirstUnique();
+ * obj->add(value);
+ */
