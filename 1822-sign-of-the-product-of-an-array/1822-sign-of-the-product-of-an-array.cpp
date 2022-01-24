9c8dede5588959a6c0597b8ed30e8518621fde81
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-25 08:25:17 +0900

    Time: 7 ms (61.48%), Space: 10.1 MB (85.31%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        bool negative = false;
+        for(auto n : nums) {
+            if(n == 0)
+                return 0;
+            negative = n < 0 ? !negative : negative;
+        }
+        
+        return negative ? -1 : 1;
+    }
+};
