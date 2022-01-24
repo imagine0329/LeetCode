92927358aa52d8632aed376abf02a6df8798a2e5
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-24 13:33:57 +0900

    Time: 208 ms (48.19%), Space: 45.6 MB (19.90%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.cpp
+++ 1229-meeting-scheduler/1229-meeting-scheduler.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
-        sort(slots1.begin(), slots1.end());
-        sort(slots2.begin(), slots2.end());
-        int i1 = 0, i2 = 0;
-        while(i1 < slots1.size() && i2 < slots2.size()) {
-            int start = max(slots1[i1][0], slots2[i2][0]);
-            int end = min(slots1[i1][1], slots2[i2][1]);
-            if(end - start >= duration)
-                return {start, start + duration};
-            if(slots1[i1][1] < slots2[i2][1]) i1++;
-            else i2++;
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> q;
+        for(auto s : slots1)
+            if(s[1] - s[0] >= duration) q.push(s);
+        
+        for(auto s : slots2)
+            if(s[1] - s[0] >= duration) q.push(s);
+        
+        while(q.size() > 1) {
+            auto s1 = q.top();
+            q.pop();
+            auto s2 = q.top();
+            if(s1[1] >= s2[0] + duration)
+                return {s2[0], s2[0] + duration};
         }
         
         return {};
