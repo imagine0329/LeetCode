614ea3e71b01778655feb91311dab2fd69dae0f9
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-27 14:03:02 +0900

    Time: 294 ms (19.72%), Space: 9.6 MB (70.93%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.cpp
+++ 277-find-the-celebrity/277-find-the-celebrity.cpp
@@ -4,33 +4,19 @@
 class Solution {
 public:
     int findCelebrity(int n) {
-        queue<int> q;
+        int candidate = 0;
+        for(int i = 0; i < n; i++) {
+            if(knows(candidate, i))
+                candidate = i;
+        }
         
         for(int i = 0; i < n; i++) {
-            bool res = false;
-            for(int j = 0; j < n; j++) {
-                if(i != j)
-                    res |= knows(i, j);
-                if(res)
-                    break;
+            if(candidate != i) {
+                if(!knows(i, candidate) || knows(candidate, i))
+                    return -1;
             }
-            if(!res) q.push(i);
         }
         
-        if(q.size() == 0) return -1;
-        
-        while(!q.empty()) {
-            auto i = q.front(); q.pop();
-            bool res = true;
-            for(int j = 0; j < n; j++) {
-                if(i != j)
-                    res &= knows(j, i);
-                if(!res)
-                    break;
-            }
-            if(res) return i;
-        }
-        
-        return -1;
+        return candidate;
     }
 };
