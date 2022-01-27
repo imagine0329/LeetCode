26f048810523cdbc301ae4aa2edd1e82d67d9bac
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-27 14:16:02 +0900

    Time: 116 ms (100.00%), Space: 10.1 MB (5.96%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.cpp
+++ 277-find-the-celebrity/277-find-the-celebrity.cpp
@@ -4,16 +4,29 @@
 class Solution {
 public:
     int findCelebrity(int n) {
+        map<pair<int, int>, bool> m;
         int candidate = 0;
+        
         for(int i = 0; i < n; i++) {
-            if(knows(candidate, i))
-                candidate = i;
+            bool res = knows(candidate, i);
+            m[{candidate, i}] = res;
+            if(res) candidate = i;
         }
         
         for(int i = 0; i < n; i++) {
             if(candidate != i) {
-                if(!knows(i, candidate) || knows(candidate, i))
-                    return -1;
+                bool res = false;
+                if(m.find({candidate, i}) != m.end())
+                    res |= m[{candidate, i}];
+                else
+                    res |= knows(candidate, i);
+                
+                if(m.find({i, candidate}) != m.end())
+                    res |= !m[{i, candidate}];
+                else
+                    res |= !knows(i, candidate);
+                
+                if(res) return -1;
             }
         }
         
