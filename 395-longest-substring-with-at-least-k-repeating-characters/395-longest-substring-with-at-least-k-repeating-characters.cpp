9d6e8df23ef32bd508738047914b94aa58a45679
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-28 09:18:18 +0900

    Time: 3 ms (79.84%), Space: 7 MB (39.27%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     int longestSubstring(string s, int k) {
         int n = s.length();
-        int longest = 0;
-        vector<int> count(26, 0);
+        if(n == 0 || k > n) return 0;
         
-        for(int start = 0; start < n; start++) {
-            memset(count.data(), 0, sizeof(int)*count.size());
-            for(int end = start; end < n; end++) {
-                count[s[end] - 'a']++;
-                if(isValid(k, count))
-                    longest = max(longest, end - start + 1);
-            }
-        }
+        unordered_map<char, int> m;
+        for(auto c : s)
+            ++m[c];
         
-        return longest;
+        int mid = 0;
+        while(mid < n && m[s[mid]] >= k) mid++;
+        if(mid == n)
+            return n;
+        int left = longestSubstring(s.substr(0, mid), k);
+        while(mid < n && m[s[mid]] < k) mid++;
+        int right = longestSubstring(s.substr(mid), k);
+        return max(left, right);
     }
     
-    bool isValid(int k, vector<int>& count) {
-        for(auto c : count) {
-            if(c != 0 && c < k)
-                return false;
-        }
-        
-        return true;
-    }
 };
