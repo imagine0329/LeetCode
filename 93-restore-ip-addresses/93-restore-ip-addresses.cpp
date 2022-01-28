5e7be8216c61a0b603504eebe117adfced818780
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-29 08:41:19 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (88.38%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/93-restore-ip-addresses.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
     vector<string> restoreIpAddresses(string s) {
-        vector<string> ans;
-        string ip;
-        backtracking(s, 0, ans, ip, 0);
-        
-        return ans;
-    }
-    
-    void backtracking(string s, int start, vector<string>& ans, string& ip, int parts) {
         int n = s.length();
-        if(parts == 4 && start == n) {
-            ip.pop_back();
-            ans.push_back(ip);
-            return;
-        }
+        vector<string> ans;
         
-        if(parts >= 4)
-            return;
-        
-        for(int i = start; i < min(n, start + 3); i++) {
-            if(valid(s, start, i)) {
-                //ip.append(s.substr(start, i - start + 1));
-                ip.push_back(s[i]);
-                ip.push_back('.');
-                backtracking(s, i + 1, ans, ip, parts + 1);
-                ip.pop_back();
+        for(int a = 1; a <= 3; a++) {
+            for(int b = 1; b <= 3; b++) {
+                for(int c = 1; c <= 3; c++) {
+                    int d = n - a - b - c;
+                    if(d > 0 && d <= 3 && a+b+c+d == n) {
+                        int A = stoi(s.substr(0, a));
+                        int B = stoi(s.substr(a, b));
+                        int C = stoi(s.substr(a+b, c));
+                        int D = stoi(s.substr(a+b+c, d));
+                        if((a > 1 && s[0] == '0') || (b > 1 && s[a] == '0') ||
+                           (c > 1 && s[a+b] == '0') || (d > 1 && s[a+b+c] == '0'))
+                           continue;
+                        if( ((a == 1 || s[0] != '0') && (b == 1 || s[a] != '0') &&
+                            (c == 1 || s[a+b] != '0') && (d == 1 || s[a+b+c] != '0')) &&
+                            (A <= 255 && B <= 255 && C <= 255 && D <= 255)) {
+                            string ip = to_string(A) + '.' + to_string(B) + '.' +
+                                to_string(C) + '.' + to_string(D);
+                            ans.push_back(ip);
+                        }
+                    }
+                }
             }
         }
         
-        int j = ip.length() - 1;
-        while(j >= 0 && ip[j] != '.'){
-            ip.pop_back();
-            j--;
-        }
-    }
-    
-    bool valid(string s, int start, int end) {
-        string temp = s.substr(start, end - start + 1);
-        int ip = stoi(temp);
-        
-        if(s[start] == '0' && start != end) return false;
-        if(ip > 255) return false;
-        
-        return true;
+        return ans;
     }
 };
