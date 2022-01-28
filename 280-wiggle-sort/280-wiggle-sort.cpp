263d66fd2dca8929e39543771acdae73cbc13552
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-28 10:28:09 +0900

    Time: 21 ms (17.06%), Space: 13.5 MB (65.88%) - LeetHub

--- 280-wiggle-sort/280-wiggle-sort.cpp
+++ 280-wiggle-sort/280-wiggle-sort.cpp
@@ -2,17 +2,7 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int left = 0, right = nums.size() - 1;
-        int idx = 0;
-        
-        vector<int> v(nums.size());
-        while(left <= right) {
-            v[idx++] = nums[left];
-            if(left != right)
-                v[idx++] = nums[right];
-            left++; right--;
-        }
-        
-        nums = v;
+        for(int i = 1; i < nums.size() - 1; i += 2)
+            swap(nums[i], nums[i + 1]);
     }
 };
