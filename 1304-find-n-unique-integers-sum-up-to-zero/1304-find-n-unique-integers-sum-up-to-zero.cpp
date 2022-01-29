ed901dc7020aafd1643fd451733a95ae1962f6c2
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-29 13:02:18 +0900

    Time: 0 ms (100.00%), Space: 6.6 MB (79.85%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> ans(n);
-        int k = n / 2;
         
-        for(int i = 0; i < n / 2; i++) {
-            ans[i] = k;
-            ans[n - i - 1] = k * -1;
-            k--;
+        for(int i = 1; i < n; i++) {
+            ans[i] = i;
+            ans[0] -= i;
         }
-        
         return ans;
     }
 };
