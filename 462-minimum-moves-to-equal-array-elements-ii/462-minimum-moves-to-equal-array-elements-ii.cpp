170a7fd892fa7c052adaed2bcffeb2b6774e2626
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-29 14:54:11 +0900

    Time: 1077 ms (5.10%), Space: 10.8 MB (62.54%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,12 +1,34 @@
 class Solution {
+private:
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[right];
+        int i = left;
+        for(int j = left; j <= right; j++) {
+            if(nums[j] < pivot)
+                swap(nums[j], nums[i++]);
+        }
+        
+        swap(nums[i], nums[right]);
+        return i;
+    }
+    
+    int quickSelect(vector<int>& nums, int left, int right, int k) {
+        if(left == right)
+            return nums[left];
+        
+        int pivot = partition(nums, left, right);
+        if(k == pivot) return nums[k];
+        else if(k < pivot) return quickSelect(nums, left, pivot - 1, k);
+        else return quickSelect(nums, pivot + 1, right, k);
+    }
+    
 public:
     int minMoves2(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int left = 0, right = nums.size() - 1;
         int moves = 0;
+        int median = quickSelect(nums, 0, nums.size() - 1, nums.size() / 2);
         
-        while(left < right)
-            moves += nums[right--] - nums[left++];
+        for(auto n : nums)
+            moves += labs(median - n);
         
         return moves;
     }
