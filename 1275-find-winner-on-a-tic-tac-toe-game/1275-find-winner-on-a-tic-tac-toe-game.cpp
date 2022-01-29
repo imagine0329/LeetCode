bab2bd0ffb44f4825efb6a0ef0f8fdc60324de88
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-29 09:55:36 +0900

    Time: 6 ms (16.14%), Space: 8.4 MB (56.26%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -1,68 +1,24 @@
-class Solution {
+class Solution {   
 public:
     string tictactoe(vector<vector<int>>& moves) {
-        bool player = true;
-        char p;
-        vector<vector<char>> board(3, vector<char>(3));
+        int player = 1;
+        vector<int> rows(3, 0);
+        vector<int> cols(3, 0);
+        int diagonal = 0, antiDiagonal = 0;
+
         for(auto move : moves) {
             int r = move[0], c = move[1];
-            bool res = false;
-            p = player ? 'X' : 'O';
-            board[r][c] = p;
-            res = checkRow(board, r, player) || checkCol(board, c, player);
-            if(!res && r == c)
-                res |= checkDiagonal(board, player);
-            if(!res && r == 2 - c)
-                res |= checkAntiDiagonal(board, player);
-                
-            if(res) return player ? "A" : "B";
-            player = !player;
+            rows[r] += player;
+            cols[c] += player;
+            if(r == c) diagonal += player;
+            if(r == 2 - c) antiDiagonal += player;
+            
+            int res = player * 3;
+            if(rows[r] == res || cols[c] == res || diagonal == res || antiDiagonal == res)
+                return player == 1 ? "A" : "B";
+            player *= -1;
         }
         
-        return moves.size() == 9  ? "Draw" : "Pending";
-    }
-    
-    bool checkRow(vector<vector<char>>& board, int row, bool player) {
-        char c = player ? 'X' : 'O';
-        for(int col = 0; col < 3; col++) {
-            if(board[row][col] != c)
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool checkCol(vector<vector<char>>& board, int col, bool player) {
-        char c = player ? 'X' : 'O';
-        for(int row = 0; row < 3; row++) {
-            if(board[row][col] != c)
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool checkDiagonal(vector<vector<char>>& board, bool player) {
-        char c = player ? 'X' : 'O';
-        for(int i = 0; i < 3; i++) {
-            if(board[i][i] != c)
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool checkAntiDiagonal(vector<vector<char>>& board, bool player) {
-        char c = player ? 'X' : 'O';
-        for(int i = 0; i < 3; i++) {
-            if(board[2 - i][i] != c)
-                return false;
-        }
-        
-        return true;
+        return moves.size() == 9 ? "Draw" : "Pending";
     }
 };
-
-
-
-
