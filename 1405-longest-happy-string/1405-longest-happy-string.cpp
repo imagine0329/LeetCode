80e286e3dfdaee219bb8fb84d7525a339b5403d1
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-01-31 14:28:39 +0900

    Time: 0 ms (100.00%), Space: 7 MB (11.41%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.cpp
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        priority_queue<pair<int, char>> q;
+        return diverse(a, b, c, 'a', 'b', 'c');
+    }
+    
+    string diverse(int a, int b, int c, char A, char B, char C) {
+        if(a < b) return diverse(b, a, c, B, A, C);
+        if(b < c) return diverse(a, c, b, A, C, B);
+        if(b == 0)
+            return string(min(2, a), A);
         
-        if(a) q.push({a, 'a'});
-        if(b) q.push({b, 'b'});
-        if(c) q.push({c, 'c'});
-        
-        string ans;
-        while(q.size() > 1) {
-            auto curr = q.top(); q.pop();
-            auto next = q.top(); q.pop();
-            
-            ans += curr.second;
-            if(--curr.first >= 1) {
-                ans += curr.second;
-                --curr.first;
-            }
-            ans += next.second;
-            if(--next.first >= 1 && next.first >= curr.first) {
-                ans += next.second;
-                --next.first;
-            }
-            
-            if(curr.first > 0) q.push(curr);
-            if(next.first > 0) q.push(next);
-        }
-        
-        if(!q.empty()) {
-            ans += q.top().second;
-            if(q.top().first >= 2)
-                ans += q.top().second;
-        }
-        
-        return ans;
+        int use_a = min(2, a), use_b = b >= a - use_a ? min(2, b) : 1;
+        return string(use_a, A) + string(use_b, B) + diverse(a - use_a, b - use_b, c, A, B, C);
     }
 };
