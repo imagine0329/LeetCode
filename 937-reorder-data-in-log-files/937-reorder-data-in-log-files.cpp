755dd7ab75991c1f762434f298b64e15caf916d4
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-01 08:11:42 +0900

    Time: 26 ms (18.39%), Space: 14.9 MB (13.29%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -0,0 +1,22 @@
+bool comparator(const string a, const string b) {
+    int i = a.find(' ') + 1, j = b.find(' ') + 1;
+
+    if(!isdigit(a[i]) && !isdigit(b[j])) {
+        if(a.substr(i) == b.substr(j)) 
+            return a.substr(0, i-1) < b.substr(0, j-1);
+        else 
+            return a.substr(i) < b.substr(j);
+    }
+    else {
+        if(!isdigit(a[i])) return true;
+        else return false;
+    }
+}
+
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        stable_sort(logs.begin(), logs.end(), comparator);
+        return logs;
+    }
+};
