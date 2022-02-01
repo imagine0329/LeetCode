47b3a54242b822151ba604146be8e6fd19443cfb
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-01 13:48:34 +0900

    Time: 29 ms (5.37%), Space: 18.4 MB (5.87%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<int, unordered_set<int>> graph;
+        preorder(root, nullptr, graph);
+        vector<int> ans;
+        unordered_set<int> visited;
+        queue<int> q;
+        q.push(target->val);
+        int dist = 0;
+        while(dist <= k && !q.empty()) {
+            int n = q.size();
+            while(n--) {
+                int u = q.front(); q.pop();
+                visited.insert(u);
+                if(dist == k)
+                    ans.push_back(u);
+                
+                for(auto v : graph[u]) {
+                    if(visited.find(v) == visited.end())
+                        q.push(v);
+                }
+            }
+            dist++;
+        }
+        
+        return ans;
+    }
+    
+    void preorder(TreeNode* root, TreeNode* parent, unordered_map<int, unordered_set<int>>& graph) {
+        if(!root) return;
+        if(parent) {
+            graph[root->val].insert(parent->val);
+            graph[parent->val].insert(root->val);
+        }
+        
+        preorder(root->left, root, graph);
+        preorder(root->right, root, graph);
+    }
+};
