c273b45202506780da72ac2e2178dff05be8c401
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-01 11:20:09 +0900

    Time: 32 ms (97.98%), Space: 23.2 MB (99.17%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,53 +1,18 @@
-class Trie {
-private:
-    vector<string> words;
-    Trie* next[26] = {nullptr};
-    bool isEnd = false;
-    
-public:
-    Trie() {
-        
-    }
-    
-    void insert(string s) {
-        Trie* node = this;
-        for(auto c : s) {
-            if(node->next[c - 'a'] == nullptr)
-                node->next[c - 'a'] = new Trie();
-            node = node->next[c - 'a'];
-            node->words.push_back(s);
-        }
-        node->isEnd = true;
-    }
-    
-    vector<string> search(string s) {
-        Trie* node = this;
-        for(auto c : s) {
-            if(node->next[c - 'a'] == nullptr)
-                return {};
-            node = node->next[c - 'a'];
-        }
-        
-        return node->words;
-    }
-};
-
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        int n = products.size();
         vector<vector<string>> ans(searchWord.length());
-        
         sort(products.begin(), products.end());
-        Trie* trie = new Trie();
-        for(int i = 0; i < products.size(); i++)
-            trie->insert(products[i]);
         
-        for(int i = 1; i <= searchWord.length(); i++) {
-            vector<string> v;
-            v = trie->search(searchWord.substr(0, i));
-            int n = v.size();
-            for(int j = 0; j < min(3, n); j++)
-                ans[i - 1].push_back(v[j]);
+        string str = "";
+        for(int i = 0; i < searchWord.length(); i++) {
+            str += searchWord[i];
+            int start = lower_bound(products.begin(), products.end(), str) - products.begin();
+            for(int j = start; j < min(start + 3, n); j++) {
+                if(products[j].find(str) == string::npos) break;
+                ans[i].push_back(products[j]);
+            }
         }
         
         return ans;
