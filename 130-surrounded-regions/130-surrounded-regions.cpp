5e34efac82fc6333fda6fb4673d88061489489e5
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-02 11:14:15 +0900

    Time: 16 ms (54.95%), Space: 10.8 MB (20.36%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -2,19 +2,23 @@ class Solution {
 public:
     void solve(vector<vector<char>>& board) {
         int m = board.size(), n = board[0].size();
+        queue<pair<int, int>> q;
+        
         for(int i = 0; i < m; i++) {
             if(board[i][0] == 'O')
-                dfs(board, i, 0);
+                q.push({i, 0});
             if(board[i][n-1] == 'O')
-                dfs(board, i, n-1);
+                q.push({i, n-1});
         }
         
         for(int i = 0; i < n; i++) {
             if(board[0][i] == 'O')
-                dfs(board, 0, i);
+                q.push({0, i});
             if(board[m-1][i] == 'O')
-                dfs(board, m-1, i);
+                q.push({m-1, i});
         }
+        
+        bfs(board, q);
         
         for(int row = 0; row < m; row++) {
             for(int col = 0; col < n; col++) {
@@ -26,15 +30,23 @@ public:
         }
     }
     
-    void dfs(vector<vector<char>>& board, int row, int col) {
+    void bfs(vector<vector<char>>& board, queue<pair<int, int>>& q) {
         int m = board.size(), n = board[0].size();
-        board[row][col] = '#';
         
-        vector<int> offset = {-1, 0, 1, 0, -1};
-        for(int i = 0; i < 4; i++) {
-            int r = row + offset[i], c = col + offset[i + 1];
-            if(r >= 0 && r < m && c >= 0 && c < n && board[r][c] == 'O')
-                dfs(board, r, c);
+        while(!q.empty()) {
+            int sz = q.size();
+            while(sz--) {
+                int r = q.front().first, c = q.front().second;
+                q.pop();
+                board[r][c] = '#';
+                
+                vector<int> offset = {-1, 0, 1, 0, -1};
+                for(int i = 0; i < 4; i++) {
+                    int row = r + offset[i], col = c + offset[i + 1];
+                    if(row >= 0 && row < m && col >= 0 && col < n && board[row][col] == 'O')
+                        q.push({row, col});
+                }
+            }
         }
     }
 };
