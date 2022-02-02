0c9e9af41abadbd1377afe178887381e278a4d69
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-02 11:59:58 +0900

    Time: 112 ms (21.98%), Space: 89.9 MB (5.04%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -5,14 +5,12 @@ public:
         int total_tank = 0, tank = 0;
         
         for(int i = 0; i < gas.size(); i++) {
+            tank += gas[i] - cost[i];
+            total_tank += gas[i] - cost[i];
             if(tank + gas[i] < cost[i]) {
                 ans = (i + 1) % gas.size();
                 tank = 0;
             }
-            else
-                tank += gas[i] - cost[i];
-            
-            total_tank += gas[i] - cost[i];
         }
         
         return total_tank >= 0 ? ans : -1;
