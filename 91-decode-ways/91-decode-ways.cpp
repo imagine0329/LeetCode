771dd48fe538fa33dd59dac271a28010a59730d4
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-02 09:41:20 +0900

    Time: 7 ms (16.55%), Space: 6.2 MB (70.22%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        vector<int> dp(s.length() + 1, 0);
-        dp[0] = 1;
-        dp[1] = s[0] == '0' ? 0 : 1;
+        int two_back = 1;
+        int one_back = s[0] == '0' ? 0 : 1;
         
-        for(int i = 2; i <= s.length(); i++) {
-            if(s[i - 1] != '0')
-                dp[i] = dp[i - 1];
+        for(int i = 1; i < s.length(); i++) {
+            int curr = 0;
+            if(s[i] != '0')
+                curr = one_back;
             
-            int two = stoi(s.substr(i - 2, 2));
+            int two = stoi(s.substr(i - 1, 2));
             if(two >= 10 && two <= 26)
-                dp[i] += dp[i - 2];
+                curr += two_back;
+            
+            two_back = one_back;
+            one_back = curr;
         }
         
-        return dp[s.length()];
+        return one_back;
     }
 };
