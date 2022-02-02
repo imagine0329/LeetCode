00b62b4725e8d3e69bce4e90ec8d96355ec5405c
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-02 15:12:34 +0900

    Time: 36 ms (19.96%), Space: 13.5 MB (32.10%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,31 +1,23 @@
 class Solution {
-private:
-    vector<int> memo;
-    
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        memo = vector<int>(s.length(), -1);
-        return helper(s, dict, 0);
+        vector<int> memo(s.length(), -1);
+        return helper(s, dict, memo, 0) == 1 ? true : false;
     }
     
-    bool helper(string& s, unordered_set<string>& dict, int start)
-    {
-        if(start == s.length())
-            return true;
+    bool helper(string& s, unordered_set<string>& dict, vector<int>& memo, int start) {
+        if(start == s.length()) return 1;
+        if(memo[start] != -1) return memo[start];
         
-        if(memo[start] != -1)
-            return memo[start];
-        
-        for(int end=start+1; end<=s.length(); end++)
-        {
-            if(dict.find(s.substr(start, end-start)) != dict.end())
-            {
-                if(helper(s, dict, end))
-                    return memo[start] = true;
+        for(int i = start; i < s.length(); i++) {
+            string str = s.substr(start, i - start + 1);
+            if(dict.find(str) != dict.end()) {
+                if(helper(s, dict, memo, i + 1))
+                    return memo[start] = 1;
             }
         }
         
-        return memo[start] = false;
+        return memo[start] = 0;
     }
 };
