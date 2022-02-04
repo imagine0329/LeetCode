cbf87564a4c1567ee964e3583a84c0bf1e74784d
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-04 22:08:55 +0900

    Time: 55 ms (13.71%), Space: 15.6 MB (14.74%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
-        vector<int> indegree(numCourses, 0);
-        queue<int> q;
+        vector<int> visited(numCourses, -1);
+        unordered_map<int, vector<int>> courses;
         
-        for(auto p : prerequisites) {
-            graph[p[1]].push_back(p[0]);
-            indegree[p[0]]++;
+        for(auto p : prerequisites)
+            courses[p.back()].push_back(p.front());
+        
+        for(auto course : courses) {
+            if(visited[course.first] == -1 && !dfs(courses, visited, course.first))
+                return false;
         }
         
-        for(int i=0; i<numCourses; i++) {
-            if(indegree[i] == 0)
-                q.push(i);
+        return true;
+    }
+    
+    bool dfs(unordered_map<int, vector<int>>& courses, vector<int>& visited, int v) {
+        visited[v] = 0;
+        for(auto u : courses[v]) {
+            if(visited[u] == 0) return false;
+            if(visited[u] == -1 && !dfs(courses, visited, u))
+                return false;
         }
-        
-        int count = 0;
-        while(!q.empty()) {
-            int u = q.front();
-            q.pop();
-            count++;
-            
-            for(auto v : graph[u]) {
-                if(--indegree[v] == 0)
-                    q.push(v);
-            }
-        }
-        
-        return count == numCourses;
+        visited[v] = 1;
+        return true;
     }
 };
