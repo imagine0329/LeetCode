852825284429116a99eafe864d0bd28dbe104d5b
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-05 13:04:19 +0900

    Time: 180 ms (55.58%), Space: 9.1 MB (43.60%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        int bound = sqrt(n);
+        vector<int> memo(n + 1, INT_MAX);
+        return recur(n, bound, memo);
+    }
+    
+    int recur(int n, int start, vector<int>& memo) {
+        if(n < 0) return INT_MAX;
+        if(memo[n] != INT_MAX) return memo[n];
+        if(n == 0)
+            return memo[n] = 0;
+        
+        int minimum = INT_MAX;
+        for(int i = start; i >= 1; i--)
+            minimum = min(minimum, recur(n - (i * i), start, memo));
+        
+        if(minimum != INT_MAX)
+            memo[n] = minimum + 1;
+        
+        return memo[n];
+    }
+};
