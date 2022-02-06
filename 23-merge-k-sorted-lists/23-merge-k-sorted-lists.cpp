9ded35e7164b674dc783ae23e780e3b10bd0f198
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-06 14:42:26 +0900

    Time: 30 ms (61.10%), Space: 13.2 MB (74.87%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,24 +8,31 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+struct cmp {
+    bool operator()(const ListNode* a, const ListNode* b) {
+        return a->val > b->val;
+    }
+};
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.size() == 0) return nullptr;
         
-        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> q;
+        priority_queue<ListNode*, vector<ListNode*>, cmp> q;
         for(auto list : lists) {
-            while(list) {
-                q.push({list->val, list});
-                list = list->next;
-            }
+            if(list) q.push(list);
         }
+        
+        if(q.empty()) return nullptr;
         
         ListNode* dummy = new ListNode();
         ListNode* node = dummy;
         while(!q.empty()) {
-            node->next = q.top().second; q.pop();
+            ListNode* top = q.top(); q.pop();
+            node->next = top;
             node = node->next;
+            if(top->next) q.push(top->next);
         }
         
         return dummy->next;
