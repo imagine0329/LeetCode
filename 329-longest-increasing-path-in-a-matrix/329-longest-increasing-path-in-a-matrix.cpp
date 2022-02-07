2c703aef869c41a4cab9f402b41ba718d5743e91
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-07 10:55:44 +0900

    Time: 95 ms (33.45%), Space: 17.6 MB (31.75%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        offset = {-1, 0, 1, 0, -1};
-        vector<vector<int>> memo(matrix.size(), vector<int>(matrix[0].size(), 0));
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> offset = {-1, 0, 1, 0, -1};
+        vector<vector<int>> outdegree(m, vector<int>(n, 0));
+        queue<pair<int, int>> q;
+        
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                for(int i = 0; i < 4; i++) {
+                    int r = row + offset[i], c = col + offset[i + 1];
+                    if(r >= 0 && r < m && c >= 0 && c < n && matrix[row][col] < matrix[r][c])
+                        outdegree[row][col]++;
+                }
+            }
+        }
+        
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                if(outdegree[row][col] == 0)
+                    q.push({row, col});
+            }
+        }
         
         int longest = 0;
-        for(int row = 0; row < matrix.size(); row++) {
-            for(int col = 0; col < matrix[0].size(); col++)
-                longest = max(longest, dfs(matrix, memo, row, col));
+        while(!q.empty()) {
+            longest++;
+            int sz = q.size();
+            while(sz--) {
+                int row = q.front().first, col = q.front().second;
+                q.pop();
+                for(int i = 0; i < 4; i++) {
+                    int r = row + offset[i], c = col + offset[i + 1];
+                    if(r >= 0 && r < m && c >= 0 && c < n && matrix[row][col] > matrix[r][c]) {
+                        if(--outdegree[r][c] == 0)
+                            q.push({r, c});
+                    }
+                }
+            }
         }
         
         return longest;
     }
-    
-    int dfs(vector<vector<int>>& matrix, vector<vector<int>>& memo, int row, int col) {
-        if(memo[row][col] != 0) return memo[row][col];
-        
-        for(int i = 0; i < 4; i++) {
-            int r = row + offset[i], c = col + offset[i + 1];
-            if(r >= 0 && r < matrix.size() && c >= 0 && c < matrix[0].size() && matrix[row][col] < matrix[r][c])
-                memo[row][col] = max(memo[row][col], dfs(matrix, memo, r, c));
-        }
-        
-        return ++memo[row][col];
-    }
-    
-private:
-    vector<int> offset;
 };
