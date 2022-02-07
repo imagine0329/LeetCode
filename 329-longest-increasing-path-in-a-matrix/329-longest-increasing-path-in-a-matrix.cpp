42632b52732a0c99bc5615dea26450d0e5aae352
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-07 10:20:28 +0900

    Time: 99 ms (31.47%), Space: 15.9 MB (73.40%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         offset = {-1, 0, 1, 0, -1};
-        vector<vector<int>> memo(matrix.size(), vector<int>(matrix[0].size(), 0));
+        vector<vector<int>> memo(matrix.size(), vector<int>(matrix[0].size(), 1));
         
         int longest = 0;
         for(int row = 0; row < matrix.size(); row++) {
@@ -14,15 +14,15 @@ public:
     }
     
     int dfs(vector<vector<int>>& matrix, vector<vector<int>>& memo, int row, int col) {
-        if(memo[row][col] != 0) return memo[row][col];
+        if(memo[row][col] != 1) return memo[row][col];
         
         for(int i = 0; i < 4; i++) {
             int r = row + offset[i], c = col + offset[i + 1];
             if(r >= 0 && r < matrix.size() && c >= 0 && c < matrix[0].size() && matrix[row][col] < matrix[r][c])
-                memo[row][col] = max(memo[row][col], dfs(matrix, memo, r, c));
+                memo[row][col] = max(memo[row][col], dfs(matrix, memo, r, c) + 1);
         }
         
-        return ++memo[row][col];
+        return memo[row][col];
     }
     
 private:
