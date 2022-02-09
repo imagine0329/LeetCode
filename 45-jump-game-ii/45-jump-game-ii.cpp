d51ef285c62e8459d4513b2ec74cc02ce9d1d875
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-09 15:31:39 +0900

    Time: 492 ms (21.15%), Space: 17.6 MB (9.73%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(), INT_MAX);
+        vector<int> dp(nums.size(), 10001);
+        
+        
+        
         return helper(nums, 0, dp);
     }
     
     int helper(vector<int>& nums, int start, vector<int>& dp) {
         if(start >= nums.size() - 1) return 0;
-        if(dp[start] != INT_MAX) return dp[start];
+        if(dp[start] < 10001) return dp[start];
         
         for(int i = nums[start]; i > 0; i--)
-            dp[start] = min(dp[start], helper(nums, start + i, dp));
+            dp[start] = min(dp[start], helper(nums, start + i, dp) + 1);
         
-        return dp[start] == INT_MAX ? dp[start] : ++dp[start];
+        return dp[start];
     }
 };
