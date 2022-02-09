36d302556bbef86967842480448a2e4e62d77fda
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-09 21:19:05 +0900

    Time: 0 ms (100.00%), Space: 9.6 MB (52.01%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int row = 0; row < matrix.size(); row++) {
+            for(int col = 0; col < matrix[0].size(); col++) {
+                if(matrix[row][col] == target)
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+};
