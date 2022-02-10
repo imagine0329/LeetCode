5092c6c12c46a985a559351d8bea13387c88dd02
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-10 09:25:07 +0900

    Time: 19 ms (22.94%), Space: 10.1 MB (42.87%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), INT_MAX));
-        return backtracking(grid, dp, 0, 0);
-    }
-    
-    int backtracking(vector<vector<int>>& grid, vector<vector<int>>& dp, int row, int col) {
         int m = grid.size(), n = grid[0].size();
-        if(dp[row][col] != INT_MAX)
-            return dp[row][col];
-        if(row == m - 1 && col == n - 1)
-            return dp[row][col] = grid[row][col];
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+        dp[m - 1][n] = 0;
         
-        if(row + 1 < m)
-            dp[row][col] = backtracking(grid, dp, row + 1, col);
-        if(col + 1 < n)
-            dp[row][col] = min(dp[row][col], backtracking(grid, dp, row, col + 1));
+        for(int r = m - 1; r >= 0; r--) {
+            for(int c = n - 1; c >= 0; c--)
+                dp[r][c] = min(dp[r][c], grid[r][c] + min(dp[r + 1][c], dp[r][c + 1]));
+        }
         
-        dp[row][col] += grid[row][col];
-        return dp[row][col];
+        return dp[0][0];
     }
 };
