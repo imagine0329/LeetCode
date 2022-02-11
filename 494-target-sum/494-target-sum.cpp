392c6720d5209b7e7954765e8941f62116f15c5e
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-11 16:27:11 +0900

    Time: 72 ms (44.70%), Space: 29.9 MB (25.88%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return backtracking(nums, target, 0, 0);
+        int total = 0;
+        for(auto n : nums)
+            total += n;
+        
+        vector<vector<int>> dp(2 * total + 1, vector<int>(nums.size(), -1));
+        return backtracking(nums, target, total, 0, 0, dp);
     }
     
-    int backtracking(vector<int>& nums, int target, int i, int sum) {
+    int backtracking(vector<int>& nums, int target, int total, int i, int sum, vector<vector<int>>& dp) {
         if(i == nums.size()) {
             if(sum == target) return 1;
             else return 0;
         }
+        
+        if(dp[sum + total][i] != -1) return dp[sum + total][i];
 
-        return backtracking(nums, target, i + 1, sum + nums[i])
-                + backtracking(nums, target, i + 1, sum - nums[i]);
+        int count = backtracking(nums, target, total, i + 1, sum + nums[i], dp)
+                + backtracking(nums, target, total, i + 1, sum - nums[i], dp);
+        
+        dp[sum + total][i] = count;
+        return count;
     }
 };
