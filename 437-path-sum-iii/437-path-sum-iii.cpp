a8818fc5120effb31c7d4e8734abf31563aa9872
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-11 14:48:40 +0900

    Time: 28 ms (49.71%), Space: 20.2 MB (27.30%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -12,27 +12,21 @@
 class Solution {
 public:
     int pathSum(TreeNode* root, int targetSum) {
-        deque<int> dq;
-        return traverse(root, targetSum, dq, 0);
+        unordered_map<int, int> prefix;
+        return traverse(root, targetSum, prefix, 0);
     }
     
-    int traverse(TreeNode* root, int target, deque<int>& dq, int sum) {
+    int traverse(TreeNode* root, int target, unordered_map<int, int>& prefix, int sum) {
         if(!root) return 0;
         
         int count = 0;
         sum += root->val;
         if(sum == target) count++;
+        count += prefix[sum - target];
         
-        int tempSum = sum;
-        deque<int>::iterator it = dq.begin();
-        while(it != dq.end()) {
-            tempSum -= *it++;
-            if(tempSum == target) count++;
-        }
-        
-        dq.push_back(root->val);
-        count += traverse(root->left, target, dq, sum) + traverse(root->right, target, dq, sum);
-        dq.pop_back();
+        prefix[sum]++;
+        count += traverse(root->left, target, prefix, sum) + traverse(root->right, target, prefix, sum);
+        prefix[sum]--;
         
         return count;
     }
