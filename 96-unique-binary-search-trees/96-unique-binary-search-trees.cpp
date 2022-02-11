22aa455f81f1a97c56792064b8f099d8fed4fa83
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-11 09:26:20 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (65.96%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,14 +1,10 @@
 class Solution {    
 public:
     int numTrees(int n) {
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1; dp[1] = 1;
+        long long c = 1;
+        for(int i = 0; i < n; i++)
+            c = c * 2 * (2 * i + 1) / (i + 2);
         
-        for(int k = 2; k <= n; k++) {
-            for(int i = 1; i <= k; i++)
-                dp[k] += dp[i - 1] * dp[k - i];
-        }
-        
-        return dp[n];
+        return (int)c;
     }
 };
