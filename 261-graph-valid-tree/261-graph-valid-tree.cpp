c65fe687751bb9ae0732b9981d7593e078f897d1
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-13 11:28:23 +0900

    Time: 24 ms (60.07%), Space: 12.3 MB (54.94%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.cpp
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -1,16 +1,14 @@
 class UnionFind {
 private:
-    vector<int> parent;
-    vector<int> rank;
+    vector<int> parent, rank;
     int count;
     
 public:
-    UnionFind(int n){
+    UnionFind(int n) {
+        count = n;
         parent = vector<int>(n);
         rank = vector<int>(n);
-        count = n;
-        
-        for(int i=0; i<n; i++) {
+        for(int i = 0; i < n; i++) {
             parent[i] = i;
             rank[i] = 0;
         }
@@ -19,26 +17,25 @@ public:
     int find(int n) {
         if(parent[n] != n)
             return parent[n] = find(parent[n]);
-        
         return n;
     }
     
     bool Union(int x, int y) {
-        int xp = find(x);
-        int yp = find(y);
+        x = find(x);
+        y = find(y);
         
-        if(xp == yp)
+        if(x == y)
             return false;
         
-        if(rank[xp] >= rank[yp]) {
-            parent[yp] = xp;
-            rank[xp]++;
+        if(rank[x] >= rank[y]) {
+            parent[y] = x;
+            rank[x]++;
         }
         else {
-            parent[xp] = yp;
-            rank[yp]++;
+            parent[x] = y;
+            rank[y]++;
         }
-            
+        
         count--;
         return true;
     }
@@ -52,6 +49,7 @@ class Solution {
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
         UnionFind uf(n);
+        
         for(auto e : edges) {
             if(uf.Union(e[0], e[1]) == false)
                 return false;
@@ -60,3 +58,5 @@ public:
         return uf.getCount() == 1;
     }
 };
+
+
