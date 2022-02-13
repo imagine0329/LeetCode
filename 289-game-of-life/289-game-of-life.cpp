4cda075b8297a13d19b330890593d9e07537f6a0
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-13 13:57:25 +0900

    Time: 0 ms (100.00%), Space: 7 MB (59.14%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -5,15 +5,8 @@ public:
         
         for(int row = 0; row < m; row++) {
             for(int col = 0; col < n; col++) {
-                int lives = 0;
-                
-                for(int i = max(row-1, 0); i < min(row+2, m); i++) {
-                    for(int j = max(col-1, 0); j < min(col+2, n); j++) {
-                        lives += board[i][j] & 1;
-                    }
-                }
-                
-                if(lives == 3 || lives - board[row][col] == 3)
+                int neighbors = countNeighbors(board, row, col);
+                if(neighbors == 3 || neighbors - (board[row][col] & 1) == 3)
                     board[row][col] |= 2;
             }
         }
@@ -22,5 +15,15 @@ public:
             for(int col = 0; col < n; col++)
                 board[row][col] >>= 1;
         }
+    }
+    
+    int countNeighbors(vector<vector<int>>& board, int r, int c) {
+        int m = board.size(), n = board[0].size();
+        int count = 0;
+        for(int row = max(0, r - 1); row < min(m, r + 2); row++) {
+            for(int col = max(0, c - 1); col < min(n, c + 2); col++)
+                count += (board[row][col] & 1);
+        }
+        return count;
     }
 };
