71e7f5e075c9f2156bb404d7321f941d35ba7662
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-14 09:48:35 +0900

    Time: 28 ms (54.76%), Space: 15.7 MB (9.43%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int countComponents(int n, vector<vector<int>>& edges) {
-        vector<int> parent(n), rank(n, 0);
-        iota(parent.begin(), parent.end(), 0);
+        unordered_set<int> seen;
+        vector<vector<int>> graph(n);
         
         for(auto e : edges) {
-            int x = find(e[0], parent), y = find(e[1], parent);
-            if(x == y) continue;
-            
-            if(rank[x] >= rank[y]) {
-                parent[y] = x;
-                rank[x]++;
-            }
-            else {
-                parent[x] = y;
-                rank[y]++;
-            }
-            
-            n--;
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
         }
         
-        return n;
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(seen.find(i) == seen.end()) {
+                count++;
+                dfs(graph, seen, i);
+            }
+        }
+        
+        return count;
     }
     
-    int find(int x, vector<int>& parent) {
-        if(parent[x] != x)
-            return parent[x] = find(parent[x], parent);
-        return x;
+    void dfs(vector<vector<int>>& graph, unordered_set<int>& seen, int v) {
+        seen.insert(v);
+        for(auto u : graph[v]) {
+            if(seen.find(u) == seen.end())
+                dfs(graph, seen, u);
+        }
     }
 };
