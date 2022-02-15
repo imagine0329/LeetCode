cb9907ac595f62e7db9455d02a0ce08536bff7a5
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-15 09:39:15 +0900

    Time: 43 ms (58.24%), Space: 15.3 MB (11.55%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int m = matrix.size(), n = matrix[0].size();
-        int left = matrix[0][0], right = matrix[m-1][n-1];
-        int ans = -1;
-        while(left <= right) {
-            int mid = (left + right) >> 1;
-            if(countLessOrEqual(matrix, mid) >= k) {
-                ans = mid;
-                right = mid - 1;
-            }
-            else
-                left = mid + 1;
+        vector<int> v;
+        for(auto r : matrix) {
+            for(auto c : r)
+                v.push_back(c);
         }
         
-        return ans;
-    }
-    
-    int countLessOrEqual(vector<vector<int>>& matrix, int x) {
-        int cnt = 0, col = matrix[0].size() - 1;
-        for(int row = 0; row < matrix.size(); row++) {
-            while(col >= 0 && matrix[row][col] > x) col--;
-            cnt += (col + 1);
-        }
-        return cnt;
+        sort(v.begin(), v.end());
+        return v[k - 1];
     }
 };
