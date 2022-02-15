fc52a508f82da60158726c2794a573d063ea2bbc
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-15 13:54:44 +0900

    Time: 6 ms (61.70%), Space: 6.5 MB (64.58%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
     int longestSubstring(string s, int k) {
-        return longestSubstring(s, k, 0, s.length() - 1);
-    }
-    
-    int longestSubstring(string s, int k, int start, int end) {
-        if(start > end)
-            return 0;
+        int maxUnique = getUniqueLetters(s);
+        int longest = 0;
         
-        unordered_map<char, vector<int>> m;
-        for(int i = start; i <= end; i++)
-            m[s[i]].push_back(i);
-        
-        int mid = findInvalid(m, k);
-        if(mid == -1)
-            return end - start + 1;
-        
-        return max(longestSubstring(s, k, start, mid - 1), longestSubstring(s, k, mid + 1, end));
-    }
-    
-    int findInvalid(unordered_map<char, vector<int>>& m, int k) {
-        for(auto n : m) {
-            if(n.second.size() < k)
-                return n.second.front();
+        for(int i = 0; i <= maxUnique; i++) {
+            vector<int> count(26, 0);
+            int start = 0, end = 0, unique = 0, unique_k = 0;
+            while(end < s.length()) {
+                if(unique <= i) {
+                    int idx = s[end] - 'a';
+                    if(count[idx] == 0)
+                        unique++;
+                    if(++count[idx] == k)
+                        unique_k++;
+                    end++;
+                }
+                else {
+                    int idx = s[start] - 'a';
+                    if(count[idx] == k)
+                        unique_k--;
+                    if(--count[idx] == 0)
+                        unique--;
+                    start++;
+                }
+                
+                if(unique == i && unique_k == unique)
+                    longest = max(longest, end - start);
+            }
         }
         
-        return -1;
+        return longest;
+    }
+
+    int getUniqueLetters(string s) {
+        unordered_set<char> unique;
+        for(auto c : s)
+            unique.insert(c);
+        
+        return unique.size();
     }
 };
