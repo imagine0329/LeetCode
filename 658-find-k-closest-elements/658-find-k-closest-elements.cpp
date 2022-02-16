1812111966061eaa21037899e18c509c20542302
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-16 11:03:45 +0900

    Time: 101 ms (26.59%), Space: 31 MB (87.98%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,28 +1,14 @@
-struct Compare{
-    bool operator() (const pair<int, int>& a, const pair<int, int>& b) {
-        if(a.first == b.first)
-            return a.second < b.second;
-        return a.first < b.first;
-    }
-};
-
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> q;
+        auto compare = [&](const int& a, const int& b) {
+            if(abs(a - x) == abs(b - x))
+                return a < b;
+            return abs(a - x) < abs(b - x);
+        };
         
-        for(auto n : arr) {
-            q.push({abs(n - x), n});
-            if(q.size() > k)
-                q.pop();
-        }
-        
-        vector<int> ans;
-        while(!q.empty()) {
-            ans.push_back(q.top().second);
-            q.pop();
-        }
-        
+        sort(arr.begin(), arr.end(), compare);
+        vector<int> ans(arr.begin(), arr.begin() + k);
         sort(ans.begin(), ans.end());
         return ans;
     }
