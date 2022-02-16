7a162cab1b27ab43a8c671d1fb67f888b1d8db0a
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-16 09:39:13 +0900

    Time: 20 ms (88.56%), Space: 11.4 MB (69.79%) - LeetHub

--- 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.cpp
+++ 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.cpp
@@ -12,28 +12,14 @@ public:
 class Solution {
 public:
     Node* inorderSuccessor(Node* node) {
-        if(!node) return nullptr;
-        
         if(node->right) {
-            Node* leftmost = node->right;
-            while(leftmost->left)
-                leftmost = leftmost->left;
-            return leftmost;
+            node = node->right;
+            while(node->left) node = node->left;
+            return node;
         }
-        else if(node->parent) {
-            if(node->parent->left == node)
-                return node->parent;
-            else {
-                Node* temp = node->parent;
-                while(temp->parent && temp != temp->parent->left)
-                    temp = temp->parent;
-                if(temp->parent && temp == temp->parent->left)
-                    return temp->parent;
-                else
-                    return nullptr;
-            }
-        }
-        else
-            return nullptr;
+        
+        while(node->parent && node == node->parent->right)
+            node = node->parent;
+        return node->parent;
     }
 };
