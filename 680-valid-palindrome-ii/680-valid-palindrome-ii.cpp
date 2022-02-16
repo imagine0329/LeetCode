6f2f47a777707592e28e8d0cf767a25ed7f4573a
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-16 15:22:21 +0900

    Time: 61 ms (71.48%), Space: 19.5 MB (85.81%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,28 +1,24 @@
 class Solution {
-private:
-    bool isPalindrome(string s, int i, int j) {
-        while(i < j) {
-            if(s[i] != s[j])
-                return false;
-            
-            i++;
-            j--;
-        }
-        
-        return true;
-    }
-
 public:
     bool validPalindrome(string s) {
         int left = 0, right = s.length() - 1;
-        
         while(left < right) {
             if(s[left] == s[right]) {
                 left++;
                 right--;
             }
             else
-                return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1);
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+        }
+        
+        return true;
+    }
+    
+    bool isPalindrome(string& s, int left, int right) {
+        while(left < right) {
+            if(s[left] != s[right])
+                return false;
+            left++; right--;
         }
         
         return true;
