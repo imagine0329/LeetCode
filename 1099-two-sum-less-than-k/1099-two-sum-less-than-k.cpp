6162a718e5d6b417c10e6f837b930a2539d2494d
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-17 16:30:40 +0900

    Time: 9 ms (24.88%), Space: 8.9 MB (94.96%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
@@ -5,11 +5,25 @@ public:
         
         sort(nums.begin(), nums.end());
         for(int i = 0; i < nums.size() && nums[i] < k; i++) {
-            int j = lower_bound(nums.begin() + i + 1, nums.end(), k - nums[i]) - nums.begin() - 1;
-            if(j != i)
+            int j = binarySearch(nums, k - nums[i], i + 1) - 1;
+            if(j > i)
                 maximum = max(maximum, nums[i] + nums[j]);
         }
         
         return maximum;
+    }
+    
+    int binarySearch(vector<int>& nums, int k, int start) {
+        int left = start, right = nums.size();
+        
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] < k)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        
+        return left;
     }
 };
