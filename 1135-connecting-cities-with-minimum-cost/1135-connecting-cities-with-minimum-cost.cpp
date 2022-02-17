eb38f81af4c62bc8ba30c6f0dd61e994afcd120b
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-17 17:24:16 +0900

    Time: 282 ms (57.43%), Space: 47.1 MB (45.68%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.cpp
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.cpp
@@ -1,82 +1,41 @@
-class UnionFind {
-private:
-    vector<int> parent;
-    vector<int> rank;
-    int count;
-    
-public:
-    UnionFind(int n) {
-        parent = vector<int>(n + 1);
-        rank = vector<int>(n + 1, 0);
-        count = n;
-        
-        for(int i = 1; i <= n; i++)
-            parent[i] = i;
-    }
-    
-    int find(int x) {
-        if(x != parent[x])
-            return parent[x] = find(parent[x]);
-        
-        return parent[x];
-    }
-    
-    bool Union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        
-        if(x == y)
-            return false;
-        
-        if(rank[x] > rank[y]) {
-            parent[y] = x;
-            rank[x] += rank[y];
-        }
-        else {
-            parent[x] = y;
-            rank[y] += rank[x];
-        }
-        
-        count--;
-        return true;
-    }
-    
-    int getCount() {
-        return count;
+struct Compare {
+    bool operator()(const vector<int>& a, const vector<int>& b) {
+        return a[2] < b[2];
     }
 };
 
 class Solution {
 public:
     int minimumCost(int n, vector<vector<int>>& connections) {
-        sort(connections.begin(), connections.end(), [](vector<int>& a, vector<int>& b) {
-            return a[2] < b[2];
-        });
+        vector<int> parent(n + 1), rank(n + 1, 0);
+        iota(parent.begin(), parent.end(), 0);
         
-        UnionFind* uf = new UnionFind(n);
+        sort(connections.begin(), connections.end(), Compare());
         int cost = 0;
-        for(int i = 0; i < connections.size(); i++) {
-            int x = connections[i][0], y = connections[i][1];
-            if(uf->Union(x, y))
-                cost += connections[i][2];
+        for(auto c : connections) {
+            int x = find(parent, c[0]), y = find(parent, c[1]);
+            if(x == y)
+                continue;
+            
+            cost += c[2];
+            if(rank[x] >= rank[y]) {
+                parent[y] = x;
+                rank[x]++;
+            }
+            else {
+                parent[x] = y;
+                rank[y]++;
+            }
+            
+            n--;
         }
         
-        if(uf->getCount() == 1)
-            return cost;
-        else
-            return -1;
+        return n == 1 ? cost : -1;
+    }
+    
+    int find(vector<int>& parent, int x) {
+        if(parent[x] != x)
+            return parent[x] = find(parent, parent[x]);
+        return x;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
