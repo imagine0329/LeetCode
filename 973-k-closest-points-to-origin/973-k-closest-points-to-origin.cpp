3c4e71e802abee1f2c27798fe029fd60db56e351
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-17 11:13:32 +0900

    Time: 323 ms (33.55%), Space: 64.1 MB (42.22%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,47 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        return quickSelect(points, k);
-    }
-    
-private:
-    vector<vector<int>> quickSelect(vector<vector<int>>& points, int k) {
-        int left = 0, right = points.size() - 1;
-        int pivot = points.size();
-        while(pivot != k) {
-            pivot = partition(points, left, right);
-            if(pivot < k)
-                left = pivot;
-            else
-                right = pivot - 1;
+        vector<pair<int, int>> dist;
+        for(int i = 0; i < points.size(); i++)
+            dist.push_back({points[i][0] * points[i][0] + points[i][1] * points[i][1], i});
+        
+        sort(dist.begin(), dist.end());
+        
+        vector<vector<int>> ans;
+        for(int i = 0; i < k; i++) {
+            ans.push_back(points[dist[i].second]);
         }
         
-        return vector<vector<int>>(points.begin(), points.begin() + k);
-    }
-    
-    int partition(vector<vector<int>>& points, int left, int right) {
-        vector<int>& pivot = choosePivot(points, left, right);
-        int pivotDist = squareDistance(pivot);
-        while(left < right) {
-            if(squareDistance(points[left]) >= pivotDist) {
-                swap(points[left], points[right]);
-                right--;
-            }
-            else
-                left++;
-        }
-        
-        if(squareDistance(points[left]) < pivotDist)
-            left++;
-        
-        return left;
-    }
-    
-    vector<int>& choosePivot(vector<vector<int>>& points, int left, int right) {
-        return points[left + (right - left) / 2];
-    }
-    
-    int squareDistance(vector<int>& point) {
-        return point[0] * point[0] + point[1] * point[1];
+        return ans;
     }
 };
