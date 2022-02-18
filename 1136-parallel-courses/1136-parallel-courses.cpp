9a2fca2b502eb707f5aec1673d5efd4e0c41b455
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-18 11:51:21 +0900

    Time: 76 ms (62.89%), Space: 26.5 MB (71.05%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.cpp
+++ 1136-parallel-courses/1136-parallel-courses.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     int minimumSemesters(int n, vector<vector<int>>& relations) {
         vector<vector<int>> graph(n + 1);
-        vector<int> indegree(n + 1, 0);
+        vector<int> visited(n + 1, 0);
         
-        for(auto r : relations) {
+        for(auto& r : relations)
             graph[r[0]].push_back(r[1]);
-            indegree[r[1]]++;
+        
+        int longest = 1;
+        for(int i = 1; i <= n; i++) {
+            int path = dfs(graph, visited, i);
+            if(path == -1) return -1;
+            longest = max(longest, path);
         }
         
-        queue<int> q;
-        for(int i = 1; i < n + 1; i++) {
-            if(indegree[i] == 0)
-                q.push(i);
+        return longest;
+    }
+    
+    int dfs(vector<vector<int>>& graph, vector<int>& visited, int i) {
+        if(visited[i] != 0) return visited[i];
+        visited[i] = -1;
+        int longest = 1;
+        for(auto v : graph[i]) {
+            int path = dfs(graph, visited, v);
+            if(path == -1) return -1;
+            longest = max(longest, path + 1);
         }
-        
-        if(q.size() == 0)
-            return -1;
-        
-        int step = 0, course = 0;
-        while(!q.empty()) {
-            int sz = q.size();
-            course += sz;
-            while(sz--) {
-                int v = q.front(); q.pop();
-                for(auto u : graph[v]) {
-                    if(--indegree[u] == 0)
-                        q.push(u);
-                }
-            }
-            step++;
-        }
-        
-        return course == n ? step : -1;
+        visited[i] = longest;
+        return longest;
     }
 };
