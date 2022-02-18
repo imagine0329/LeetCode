fb6ac5158cbe6b94eacc9e7496f8aa4b7845fda3
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-18 13:41:10 +0900

    Time: 8 ms (29.48%), Space: 10.5 MB (10.82%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if(str1.size() < str2.size())
+        if(str1 + str2 != str2 + str1) return "";
+        if(str1.length() < str2.length())
             return gcdOfStrings(str2, str1);
-        if(str2.size() == 0) return str1;
-        if(str1.substr(0, str2.size()) != str2) return "";
-        return gcdOfStrings(str1.substr(str2.size()), str2);
+        if(str2.length() == 0)
+            return str1;
+        else
+            return gcdOfStrings(str2, str1.substr(str2.length()));
     }
 };
