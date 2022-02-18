56346d7e6b1447316f94d02b9e81db5f639e9896
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-19 08:53:12 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (9.39%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,15 +2,31 @@ class Solution {
 public:
     int fib(int n) {
         if(n <= 1) return n;
+        vector<vector<int>> matrix = {{1, 1}, {1, 0}};
+        matrixPower(matrix, n - 1);
+        return matrix[0][0];
+    }
+    
+    void matrixPower(vector<vector<int>>& matrix, int n) {
+        if(n <= 1) return;
+        matrixPower(matrix, n / 2);
+        multiply(matrix, matrix);
         
-        int curr = 0, prev1 = 0, prev2 = 1;
-        
-        for(int i = 2; i <= n; i++) {
-            curr = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = curr;
+        if(n & 1) {
+            vector<vector<int>> def = {{1, 1}, {1, 0}};
+            multiply(matrix, def);
         }
+    }
+    
+    void multiply(vector<vector<int>>& a, vector<vector<int>>& b) {
+        int x = a[0][0] * b[0][0] + a[0][1] * b[1][0];
+        int y = a[0][0] * b[0][1] + a[0][1] * b[1][1];
+        int z = a[1][0] * b[0][0] + a[1][1] * b[1][0];
+        int w = a[1][0] * b[0][1] + a[1][1] * b[1][1];
         
-        return curr;
+        a[0][0] = x;
+        a[0][1] = y;
+        a[1][0] = z;
+        a[1][1] = w;
     }
 };
