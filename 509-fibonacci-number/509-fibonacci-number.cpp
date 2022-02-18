622b1389f9d0b17bb93c6e3e6b20842652a921bd
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-18 16:57:41 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (16.66%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
+        vector<int> memo(n + 1, -1);
+        return fibonacci(n, memo);
+    }
+    
+    int fibonacci(int n, vector<int>& memo) {
         if(n == 0) return 0;
         if(n < 3) return 1;
-        return fib(n - 1) + fib(n - 2);
+        if(memo[n] != -1) return memo[n];
+        return memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
     }
 };
