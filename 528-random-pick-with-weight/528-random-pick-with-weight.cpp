5d381fa2408562e6f35b5907cc9dfbcb0719858b
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-19 13:27:56 +0900

    Time: 102 ms (67.42%), Space: 40.3 MB (86.69%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.cpp
+++ 528-random-pick-with-weight/528-random-pick-with-weight.cpp
@@ -11,12 +11,20 @@ public:
     int pickIndex() {
         int test = prefix.back();
         float target = ((float) rand() / RAND_MAX) * prefix.back();
-        for(int i = 0; i < prefix.size(); i++) {
-            if(target < prefix[i])
-                return i;
+        return binarySearch(target);
+    }
+    
+    int binarySearch(float target) {
+        int left = 0, right = prefix.size() - 1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(prefix[mid] < target)
+                left = mid + 1;
+            else
+                right = mid;
         }
         
-        return prefix.size() - 1;
+        return left;
     }
 };
 
