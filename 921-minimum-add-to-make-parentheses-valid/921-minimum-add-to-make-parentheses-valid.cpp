7d4202ea4841bb41a9e1f47638b2a9dfbeab2e20
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-19 15:14:38 +0900

    Time: 5 ms (15.76%), Space: 6.2 MB (79.87%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open = 0, close = 0;
+        for(auto c : s) {
+            if(c == '(') open++;
+            else {
+                if(open == 0) close++;
+                else open--;
+            }
+        }
+        
+        return open + close;
+    }
+};
