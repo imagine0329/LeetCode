f4e12de9af5dc4a3bd3b61e6fc416cdf3dc67c8b
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-21 16:51:49 +0900

    Time: 29 ms (65.97%), Space: 12.7 MB (58.33%) - LeetHub

--- 308-range-sum-query-2d-mutable/308-range-sum-query-2d-mutable.cpp
+++ 308-range-sum-query-2d-mutable/308-range-sum-query-2d-mutable.cpp
@@ -1,105 +1,40 @@
-struct Cell {
-    Cell(int r, int c) : r(r), c(c) {}
-    Cell(Cell& p) : r(p.r), c(p.c) {}
-    Cell(){}
-    
-    bool operator==(const Cell& a) {
-        return a.r == r && a.c == c;
-    }
-    
-    int r, c;
-};
-
-class SegmentTree {
-public:
-    Cell start, end;
-    vector<SegmentTree*> part;
-    int sum;
-    
-    SegmentTree(Cell& start, Cell& end) : start(start), end(end){
-        this->part = vector<SegmentTree*>(4, nullptr);
-        this->sum = 0;
-    }
-};
-
 class NumMatrix {
 private:
-    SegmentTree* root;
+    vector<vector<int>> matrix;
+    vector<vector<int>> tree;
     
 public:
     NumMatrix(vector<vector<int>>& matrix) {
-        root = buildTree(matrix, Cell(0, 0), Cell(matrix.size() - 1, matrix[0].size() - 1));
+        int m = matrix.size(), n = matrix[0].size();
+        this->matrix = vector<vector<int>>(m, vector<int>(n));
+        this->tree = vector<vector<int>>(m + 1, vector<int>(n + 1));
+        
+        for(int r = 0; r < m; r++) {
+            for(int c = 0; c < n; c++)
+                update(r, c, matrix[r][c]);
+        }
     }
     
     void update(int row, int col, int val) {
-        updateTree(root, Cell(row, col), val);
+        int delta = val - matrix[row][col];
+        matrix[row][col] = val;
+        for(int r = row + 1; r <= matrix.size(); r += r & (-r)) {
+            for(int c = col + 1; c <= matrix[0].size(); c += c & (-c))
+                tree[r][c] += delta;
+        }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return query(root, Cell(row1, col1), Cell(row2, col2));
+        return sum(row2 + 1, col2 + 1) + sum(row1, col1) - sum(row1, col2 + 1) - sum(row2 + 1, col1);
     }
     
-    SegmentTree* buildTree(vector<vector<int>>& matrix, Cell start, Cell end) {
-        if(start.r > end.r || start.c > end.c) return nullptr;
-    
-        SegmentTree* node = new SegmentTree(start, end);
-        if(start == end) {
-            node->sum = matrix[start.r][start.c];
-            return node;
+    int sum(int row, int col) {
+        int sum = 0;
+        for(int r = row; r > 0; r -= r & (-r)) {
+            for(int c = col; c > 0; c -= c & (-c))
+                sum += tree[r][c];
         }
-        
-        int r_mid = (start.r + end.r) / 2;
-        int c_mid = (start.c + end.c) / 2;
-        
-        node->part[0] = buildTree(matrix, start, Cell(r_mid, c_mid));
-        node->part[1] = buildTree(matrix, Cell(start.r, c_mid + 1), Cell(r_mid, end.c));
-        node->part[2] = buildTree(matrix, Cell(r_mid + 1, start.c), Cell(end.r, c_mid));
-        node->part[3] = buildTree(matrix, Cell(r_mid + 1, c_mid + 1), end);
-        
-        node->sum += node->part[0] ? node->part[0]->sum : 0;
-        node->sum += node->part[1] ? node->part[1]->sum : 0;
-        node->sum += node->part[2] ? node->part[2]->sum : 0;
-        node->sum += node->part[3] ? node->part[3]->sum : 0;
-        
-        return node;
-    }
-    
-    int query(SegmentTree* root, Cell start, Cell end) {
-        if(!root) return 0;
-        if(root->end.r < start.r || root->end.c < start.c ||
-            root->start.r > end.r || root->start.c > end.c)
-            return 0;
-        
-        if(root->start.r >= start.r && root->start.c >= start.c &&
-            root->end.r <= end.r && root->end.c <= end.c)
-            return root->sum;
-        
-        return query(root->part[0], start, end) + query(root->part[1], start, end) +
-                query(root->part[2], start, end) + query(root->part[3], start, end);
-    }
-    
-    void updateTree(SegmentTree* root, Cell cell, int val) {
-        if(!root) return;
-        if(root->start.r > cell.r || root->start.c > cell.c ||
-            root->end.r < cell.r || root->end.c < cell.c)
-            return;
-        
-        if(root->start.r == cell.r && root->start.c == cell.c &&
-            root->end.r == cell.r && root->end.c == cell.c) {
-            root->sum = val;
-            return;
-        }
-        
-        updateTree(root->part[0], cell, val);
-        updateTree(root->part[1], cell, val);
-        updateTree(root->part[2], cell, val);
-        updateTree(root->part[3], cell, val);
-        
-        root->sum = 0;
-        root->sum += root->part[0] ? root->part[0]->sum : 0;
-        root->sum += root->part[1] ? root->part[1]->sum : 0;
-        root->sum += root->part[2] ? root->part[2]->sum : 0;
-        root->sum += root->part[3] ? root->part[3]->sum : 0;
+        return sum;
     }
 };
 
