bfb06369cadb70e8cfbba91c2398b821024a878f
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-23 10:25:48 +0900

    Time: 420 ms (80.55%), Space: 10.3 MB (11.08%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int maxCoins(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size() + 2, vector<int>(nums.size() + 2, -1));
-        vector<int> arr;
-        arr.push_back(1);
-        for(auto n : nums)
-            arr.push_back(n);
-        arr.push_back(1);
+        nums.insert(nums.begin(), 1);
+        nums.emplace_back(1);
+        vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), 0));
         
-        return helper(arr, 1, arr.size() - 2, dp);
-    }
-    
-    int helper(vector<int>& nums, int start, int end, vector<vector<int>>& dp) {
-        if(start > end) return 0;
-        if(start == end) {
-            int temp = nums[start];
-            if(start - 1 >= 0) temp *= nums[start - 1];
-            if(start + 1 < nums.size()) temp *= nums[start + 1];
-        }
-        if(dp[start][end] != -1) return dp[start][end];
-        
-        int ans = 0;
-        for(int k = start; k <= end; k++) {
-            int temp = nums[k];
-            if(start - 1 >= 0) temp *= nums[start - 1];
-            if(end + 1 < nums.size()) temp *= nums[end + 1];
-            temp += helper(nums, start, k - 1, dp) + helper(nums, k + 1, end, dp);
-            ans = max(ans, temp);
+        for(int start = nums.size() - 2; start >= 1; start--) {
+            for(int end = start; end <= nums.size() - 2; end++) {
+                for(int i = start; i <= end; i++) {
+                    int gain = nums[start - 1] * nums[i] * nums[end + 1];
+                    int remain = dp[start][i - 1] + dp[i + 1][end];
+                    dp[start][end] = max(remain + gain, dp[start][end]);
+                }
+            }
         }
         
-        return dp[start][end] = ans;
+        return dp[1][nums.size() - 2];
     }
 };
