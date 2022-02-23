85a1f1f8428226fa72cc6f2a398ea4724415892c
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-24 08:15:12 +0900

    Time: 4 ms (82.15%), Space: 9.7 MB (53.74%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.cpp
+++ 269-alien-dictionary/269-alien-dictionary.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
     string alienOrder(vector<string>& words) {
-        unordered_map<char, unordered_set<char>> graph;
+        unordered_map<char, vector<char>> graph;
         unordered_map<char, int> indegree;
         
-        for(auto word : words) {
-            for(auto c : word)
+        for(auto s : words) {
+            for(auto c : s)
                 indegree[c] = 0;
         }
         
         for(int i = 0; i < words.size() - 1; i++) {
-            string word1 = words[i];
-            string word2 = words[i + 1];
-            int len = min(word1.length(), word2.length());
-            for(int j = 0; j < len; j++) {
-                if(word1[j] != word2[j]) {
-                    if(graph[word1[j]].find(word2[j]) == graph[word1[j]].end()) {
-                        graph[word1[j]].insert(word2[j]);
-                        indegree[word2[j]]++;
-                    }
+            int p = 0;
+            while(p < min(words[i].length(), words[i + 1].length())) {
+                if(words[i].length() > words[i + 1].length() && words[i].substr(0, words[i + 1].length()) == words[i + 1])
+                    return "";
+                
+                if(words[i][p] != words[i + 1][p]) {
+                    graph[words[i][p]].push_back(words[i + 1][p]);
+                    indegree[words[i + 1][p]]++;
                     break;
                 }
-                
-                if(j == len - 1 && word1.length() > word2.length())
-                    return "";
+                p++;
             }
         }
         
-        string ans;
         queue<char> q;
         for(auto e : indegree) {
             if(e.second == 0)
                 q.push(e.first);
         }
         
+        string ans;
         while(!q.empty()) {
-            auto c = q.front();
-            q.pop();
+            char c = q.front(); q.pop();
             ans += c;
-            for(auto e : graph[c]) {
-                if(--indegree[e] == 0)
-                    q.push(e);
+            for(auto next : graph[c]) {
+                if(--indegree[next] == 0)
+                    q.push(next);
             }
         }
         
-        return ans.length() == indegree.size() ? ans : "";
+        return ans.length() < indegree.size() ? "" : ans;
     }
 };
