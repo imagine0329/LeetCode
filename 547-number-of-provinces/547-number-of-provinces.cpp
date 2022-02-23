9d84de843cc5cc18c6b96861015aa251bd5dfcfc
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-23 15:28:03 +0900

    Time: 44 ms (31.24%), Space: 14 MB (30.79%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -2,43 +2,24 @@ class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>> graph(n);
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(isConnected[i][j])
-                    graph[i].push_back(j);
-            }
-        }
-        
+        queue<int> q;
         vector<bool> visited(n, false);
         int count = 0;
         for(int v = 0; v < n; v++) {
             if(visited[v] == false) {
                 count++;
-                bfs(graph, visited, v);
-            }
-        }
-        
-        return count;
-    }
-    
-    void bfs(vector<vector<int>>& graph, vector<bool>& visited, int v) {
-        queue<int> q;
-        q.push(v);
-        visited[v] = true;
-        
-        while(!q.empty()) {
-            int n = q.size();
-            while(n--) {
-                v = q.front(); q.pop();
-                for(auto u : graph[v]) {
-                    if(visited[u] == false) {
-                        visited[u] = true;
-                        q.push(u);
+                q.push(v);
+                while(!q.empty()) {
+                    int u = q.front(); q.pop();
+                    visited[u] = true;
+                    for(int i = 0; i < n; i++) {
+                        if(isConnected[u][i] && visited[i] == false)
+                            q.push(i);
                     }
                 }
             }
         }
+        
+        return count;
     }
 };
