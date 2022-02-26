22e86887cd16896daaabab8472d244eb5598c873
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-02-26 14:51:15 +0900

    Time: 27 ms (37.94%), Space: 24 MB (8.70%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -13,11 +13,25 @@ class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         if(!root) return nullptr;
-        if(root->val > high) return trimBST(root->left, low, high);
-        if(root->val < low) return trimBST(root->right, low, high);
+        while(root && (root->val < low || root->val > high)) {
+            if(root->val < low) root = root->right;
+            if(root->val > high) root = root->left;
+        }
         
-        root->left = trimBST(root->left, low, high);
-        root->right = trimBST(root->right, low, high);        
+        TreeNode* node = root;
+        while(node) {
+            while(node->left && node->left->val < low)
+                node->left = node->left->right;
+            node = node->left;
+        }
+        
+        node = root;
+        while(node) {
+            while(node->right && node->right->val > high)
+                node->right = node->right->left;
+            node = node->right;
+        }
+        
         return root;
     }
 };
