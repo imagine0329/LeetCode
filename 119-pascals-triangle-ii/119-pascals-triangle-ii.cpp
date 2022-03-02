0d74ee68ef2da07565bf8d1deb2c6d63a6d45d89
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-02 13:13:49 +0900

    Time: 0 ms (100.00%), Space: 6.6 MB (19.04%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> prev, curr;
-        curr.push_back(1);
-        for(int i = 0; i < rowIndex; i++) {
-            for(int j = 1; j <= i + 1; j++) {
-                if(j == i + 1)
-                    curr.push_back(1);
-                else
-                    curr[j] = prev[j - 1] + prev[j];
-            }
-            prev = curr;
-        }
+        vector<int> ans;
+        vector<vector<int>> memo(rowIndex + 1, vector<int>(rowIndex + 1, 0));
         
-        return curr;
+        for(int i = 0; i <= rowIndex; i++)
+            ans.push_back(getNum(rowIndex, i, memo));
+        
+        return ans;
+    }
+    
+    int getNum(int row, int col, vector<vector<int>>& memo) {
+        if(row == 0 || col == 0 || row == col)
+            return 1;
+        
+        if(memo[row][col] != 0) return memo[row][col];
+        
+        return memo[row][col] = getNum(row - 1, col - 1, memo) + getNum(row - 1, col, memo);
     }
 };
