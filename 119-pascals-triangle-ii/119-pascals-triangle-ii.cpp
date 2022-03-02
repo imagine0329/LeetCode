427cbf666bc39034389e52467531a051d04e28ea
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-02 13:26:04 +0900

    Time: 0 ms (100.00%), Space: 6.4 MB (80.26%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> ans;
-        vector<vector<int>> memo(rowIndex + 1, vector<int>(rowIndex + 1, 0));
+        vector<int> ans = {1};
         
-        for(int i = 0; i <= rowIndex; i++)
-            ans.push_back(getNum(rowIndex, i, memo));
-        
+        for(int k = 1; k <= rowIndex; k++)
+            ans.push_back((int)(ans.back() * (long long)(rowIndex - k + 1) / k));
         return ans;
-    }
-    
-    int getNum(int row, int col, vector<vector<int>>& memo) {
-        if(row == 0 || col == 0 || row == col)
-            return 1;
-        
-        if(memo[row][col] != 0) return memo[row][col];
-        
-        return memo[row][col] = getNum(row - 1, col - 1, memo) + getNum(row - 1, col, memo);
     }
 };
