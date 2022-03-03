29e6c610f8a9d4592844f401aa9365e51e3898b9
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-03 16:22:11 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (27.27%) - LeetHub

--- 651-4-keys-keyboard/651-4-keys-keyboard.cpp
+++ 651-4-keys-keyboard/651-4-keys-keyboard.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int maxA(int n) {
-        int maximum = n;
-        for(int i = 1; i < n - 2; i++) {
-            maximum = max(maximum, maxA(i) * (n - i - 1));
+        vector<int> dp(n + 1, 0);
+        for(int j = 0; j <= n; j++) {
+            dp[j] = j;
+            for(int i = 1; i < j - 2; i++) {
+                dp[j] = max(dp[j], dp[i] * (j - i - 1));
+            }
         }
         
-        return maximum;
+        
+        return dp[n];
         
     }
 };
