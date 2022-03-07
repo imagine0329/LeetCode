05ef5e465315a951ba93f2a13305121b4535d9a1
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-07 09:50:43 +0900

    Time: 173 ms (9.20%), Space: 39.9 MB (23.48%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.cpp
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int numDistinctIslands(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<vector<int>>> unique;
-        vector<vector<int>> island;
+        set<set<pair<int, int>>> unique;
+        
         vector<vector<bool>> visited(m, vector<bool>(n, false));
         
         for(int row = 0; row < m; row++) {
             for(int col = 0; col < n; col++) {
                 if(!visited[row][col] && grid[row][col] == 1) {
-                    dfs(grid, visited, island, row, col);
-                    for(auto& cell : island) {
-                        cell[0] -= row;
-                        cell[1] -= col;
-                    }
-                    if(isUnique(unique, island))
-                        unique.push_back(island);
-                    island.clear();
+                    set<pair<int, int>> island;
+                    dfs(grid, visited, island, row, col, row, col);
+                    unique.insert(island);
                 }
             }
         }
@@ -24,32 +19,17 @@ public:
         return unique.size();
     }
     
-    void dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, vector<vector<int>>& island, int row, int col) {
+    void dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, set<pair<int, int>>& island, 
+             int sr, int sc, int row, int col) {
         int m = grid.size(), n = grid[0].size();
         visited[row][col] = true;
-        island.push_back({row, col});
+        island.insert({row - sr, col - sc});
         
         vector<int> offset = {-1, 0, 1, 0, -1};
         for(int i = 0; i < 4; i++) {
             int r = row + offset[i], c = col + offset[i + 1];
             if(r >= 0 && r < m && c >= 0 && c < n && !visited[r][c] && grid[r][c] == 1)
-                dfs(grid, visited, island, r, c);
+                dfs(grid, visited, island, sr, sc, r, c);
         }
-    }
-    
-    bool isUnique(vector<vector<vector<int>>>& unique, vector<vector<int>>& island) {
-        for(auto& u : unique) {
-            if(u.size() != island.size()) continue;
-            if(isEqual(u, island)) return false;
-        }
-        return true;
-    }
-    
-    bool isEqual(vector<vector<int>>& a, vector<vector<int>>& b) {
-        for(int i = 0; i < a.size(); i++) {
-            if(a[i][0] != b[i][0] || a[i][1] != b[i][1])
-                return false;
-        }
-        return true;
     }
 };
