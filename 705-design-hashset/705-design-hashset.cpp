63fc61d4222fe65f98fb9397e28a04e36861eeaf
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-07 16:33:01 +0900

    Time: 119 ms (70.16%), Space: 46.7 MB (39.32%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,10 +1,11 @@
 class MyHashSet {
 private:
-    int prime = 10007;
+    int capacity, m_size;
+    double load_factor;
     vector<list<int>> table;
     
     int hash(int key) {
-        return key % prime;
+        return key % capacity;
     }
     
     list<int>::iterator search(int key) {
@@ -12,22 +13,49 @@ private:
         return find(table[h].begin(), table[h].end(), key);
     }
     
+    void rehash() {
+        m_size = 0;
+        capacity = max(2, capacity);
+        vector<list<int>> old(move(table));
+        table = vector<list<int>>(capacity);
+        
+        for(auto& chain : old) {
+            for(auto& key : chain)
+                add(key);
+        }
+    }
+    
 public:
     MyHashSet() {
-        table = vector<list<int>>(prime);
+        m_size = 0;
+        capacity = 2;
+        load_factor = 0.75;
+        table = vector<list<int>>(capacity);
     }
     
     void add(int key) {
+        if(contains(key)) return;
+        
+        m_size++;
         int h = hash(key);
-        if(!contains(key))
-            table[h].push_back(key);
+        table[h].push_back(key);
+        if(m_size >= load_factor * capacity) {
+            capacity *= 2;
+            rehash();
+        }
     }
     
     void remove(int key) {
         int h = hash(key);
         auto it = search(key);
-        if(it != table[h].end())
-            table[h].erase(it);
+        if(it == table[h].end()) return;
+        
+        m_size--;
+        table[h].erase(it);
+        if(m_size <= 0.5 * load_factor * capacity) {
+            capacity /= 2;
+            rehash();
+        }
     }
     
     bool contains(int key) {
