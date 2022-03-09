2b2f6b1617b0e4f2b58badfe88ea6a85d49c68a6
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-09 09:54:31 +0900

    Time: 221 ms (13.23%), Space: 43.9 MB (22.24%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> freq(26, 0);
-        
+        unordered_map<char, int> freq;
         for(auto t : tasks)
-            freq[t - 'A']++;
+            freq[t]++;
         
-        sort(freq.begin(), freq.end());
-        int f_max = freq[25];
-        int n_max = 0;
+        priority_queue<int> pq;
+        for(auto f : freq)
+            pq.push(f.second);
         
-        for(auto f : freq) {
-            if(f == f_max) n_max++;
+        int totalTime = 0;
+        int cycle = n + 1;
+        while(!pq.empty()) {
+            int time = 0;
+            vector<int> temp;
+            for(int i = 0; i < cycle && !pq.empty(); i++) {
+                temp.push_back(pq.top());
+                pq.pop();
+                time++;
+            }
+            for(auto t : temp) {
+                if(--t) pq.push(t);
+            }
+            
+            totalTime += !pq.empty() ? cycle : time;
         }
-        
-        return max((int)tasks.size(), (f_max - 1) * (n + 1) + n_max);
+        return totalTime;
     }
 };
