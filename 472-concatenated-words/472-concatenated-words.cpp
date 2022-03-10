0295979406f8c5c1078179bb0b72b5d11e7f6477
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-10 09:44:01 +0900

    Time: 402 ms (46.92%), Space: 225 MB (44.86%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.cpp
+++ 472-concatenated-words/472-concatenated-words.cpp
@@ -1,30 +1,55 @@
+class Trie {
+public:
+    Trie* next[26] = {nullptr};
+    bool isWord = false;
+    
+    Trie() {}
+    
+    void insert(string s) {
+        Trie* node = this;
+        for(auto c : s) {
+            c -= 'a';
+            if(node->next[c] == nullptr)
+                node->next[c] = new Trie();
+            node = node->next[c];
+        }
+        node->isWord = true;
+    }
+    
+    bool search(string s) {
+        Trie* node = this;
+        for(auto c : s) {
+            c -= 'a';
+            if(node->next[c] == nullptr) return false;
+            node = node->next[c];
+        }
+        return node->isWord;
+    }
+};
+
 class Solution {
 public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        unordered_set<string> dict(words.begin(), words.end());
-        unordered_map<string, bool> memo;
+        Trie* trie = new Trie();
         vector<string> ans;
-        for(auto word : words) {
-            if(dfs(word, dict, memo)) ans.push_back(word);
+        
+        for(auto s : words)
+            trie->insert(s);
+        
+        for(auto s : words) {
+            if(dfs(trie, s)) ans.push_back(s);
         }
         return ans;
     }
     
-    bool dfs(string word, unordered_set<string>& dict, unordered_map<string, bool>& memo) {
-        if(memo.find(word) != memo.end()) return memo[word];
-        
-        for(int i = 1; i < word.length(); i++) {
-            string substring = word.substr(0, i);
-            if(dict.find(substring) != dict.end()) {
-                string restSubstring = word.substr(i);
-                if(dict.find(restSubstring) != dict.end() || dfs(restSubstring, dict, memo)) {
-                    memo[word] = true;
-                    return true;
-                }
+    bool dfs(Trie* root, string s) {
+        for(int i = 1; i < s.length(); i++) {
+            string prefix = s.substr(0, i);
+            if(root->search(prefix)) {
+                string suffix = s.substr(i);
+                if(root->search(suffix) || dfs(root, suffix)) return true;
             }
         }
-        
-        memo[word] = false;
         return false;
     }
 };
