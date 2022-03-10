efe0c3976349e203e9938cd3f2e75f9fdbd196a7
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-10 10:59:39 +0900

    Time: 218 ms (14.12%), Space: 57.9 MB (77.13%) - LeetHub

---
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        int pos = 0, neg = 0;
+        int ans = 0;
+        
+        for(auto n : nums) {
+            if(n == 0) {
+                pos = 0; neg = 0;
+            }
+            else if(n > 0) {
+                pos++;
+                neg = neg == 0 ? 0 : neg + 1;
+            }
+            else {
+                int temp = pos;
+                pos = neg == 0 ? 0 : neg + 1;
+                neg = temp + 1;
+            }
+            ans = max(ans, pos);
+        }
+        return ans;
+    }
+};
