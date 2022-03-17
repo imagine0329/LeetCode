a1d8407455fb83f8c66c32eb194227fdd6d85493
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-17 13:30:02 +0900

    Time: 236 ms (47.81%), Space: 34.5 MB (8.07%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string, vector<string>> dict;
-        unordered_set<string> visited;
-        queue<pair<string, int>> q;
+        unordered_map<string, vector<string>> m;
+        unordered_set<string> seen;
         
         for(auto s : wordList) {
-            for(int i=0; i<beginWord.length(); i++) {
-                string word = s;
-                word[i] = '*';
-                dict[word].push_back(s);
+            for(int i = 0; i < s.length(); i++) {
+                string str = s;
+                str[i] = '*';
+                m[str].push_back(s);
             }
         }
         
-        q.push({beginWord, 1});
-        visited.insert(beginWord);
+        queue<string> q;
+        int step = 0;
         
-        while(!q.empty())
-        {
-            auto word = q.front().first;
-            auto level = q.front().second;
-            q.pop();
-            
-            for(int i=0; i<beginWord.length(); i++)
-            {
-                string newWord = word;
-                newWord[i] = '*';
-                
-                for(auto s : dict[newWord])
-                {
-                    if(s == endWord)
-                        return level + 1;
-                    
-                    if(visited.find(s) == visited.end())
-                    {
-                        visited.insert(s);
-                        q.push({s, level+1});
+        q.push(beginWord);
+        seen.insert(beginWord);
+        
+        while(!q.empty()) {
+            step++;
+            int n = q.size();
+            while(n--) {
+                string str = q.front(); q.pop();
+                if(str == endWord) return step;
+                for(auto& c : str) {
+                    char org = c;
+                    c = '*';
+                    if(m.find(str) != m.end()) {
+                        for(auto s : m[str]) {
+                            q.push(s);
+                            seen.insert(s);
+                        }
+                        m.erase(str);
                     }
+                    c = org;
                 }
             }
         }
