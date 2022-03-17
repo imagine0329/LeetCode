22e0dd3f82be88b956f35e8a4ca45bacaabe9f39
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-17 13:55:01 +0900

    Time: 106 ms (9.39%), Space: 18.8 MB (11.29%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -9,22 +9,35 @@ public:
             for(int col = 0; col < n; col++) {
                 if(!visited[row][col] && grid[row][col] == '1') {
                     ans++;
-                    dfs(grid, visited, row, col);
+                    bfs(grid, visited, row, col);
                 }
             }
         }
         return ans;
     }
     
-    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col) {
+    void bfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col) {
         int m = grid.size(), n = grid[0].size();
+        queue<pair<int, int>> q;
+        vector<int> offset = {-1, 0 , 1, 0, -1};
+        
+        q.push({row, col});
         visited[row][col] = true;
         
-        vector<int> offset = {-1, 0 , 1, 0, -1};
-        for(int i = 0; i < 4; i++) {
-            int r = row + offset[i], c = col + offset[i + 1];
-            if(r >= 0 && r < m && c >= 0 && c < n && !visited[r][c] && grid[r][c] == '1')
-                dfs(grid, visited, r, c);
+        while(!q.empty()) {
+            int sz = q.size();
+            while(sz--) {
+                row = q.front().first;
+                col = q.front().second;
+                q.pop();
+                for(int i = 0; i < 4; i++) {
+                    int r = row + offset[i], c = col + offset[i + 1];
+                    if(r >= 0 && r < m && c >= 0 && c < n && !visited[r][c] && grid[r][c] == '1') {
+                        visited[r][c] = true;
+                        q.push({r, c});
+                    }
+                }
+            }
         }
     }
 };
