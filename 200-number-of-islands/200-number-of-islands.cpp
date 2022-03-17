4334cb0e1de5e86431b4b4a5ed2daf3675594f21
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-17 14:20:41 +0900

    Time: 44 ms (63.41%), Space: 13.7 MB (24.67%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -2,42 +2,48 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        vector<int> parent(m * n), rank(m * n, 0);
+        iota(parent.begin(), parent.end(), 0);
         
+        vector<int> offset = {-1, 0, 1, 0, -1};
         int ans = 0;
         for(int row = 0; row < m; row++) {
             for(int col = 0; col < n; col++) {
-                if(!visited[row][col] && grid[row][col] == '1') {
+                if(grid[row][col] == '1') {
                     ans++;
-                    bfs(grid, visited, row, col);
+                    for(int i = 0; i < 4; i++) {
+                        int r = row + offset[i], c = col + offset[i + 1];
+                        if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') {
+                            if(Union(parent, rank, (row * n) + col, (r * n) + c))
+                                ans--;
+                        }
+                    }
                 }
             }
         }
         return ans;
     }
     
-    void bfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col) {
-        int m = grid.size(), n = grid[0].size();
-        queue<pair<int, int>> q;
-        vector<int> offset = {-1, 0 , 1, 0, -1};
+    int find(vector<int>& parent, int x) {
+        if(parent[x] != x)
+            return parent[x] = find(parent, parent[x]);
+        return x;
+    }
+    
+    bool Union(vector<int>& parent, vector<int>& rank, int x, int y) {
+        x = find(parent, x);
+        y = find(parent, y);
         
-        q.push({row, col});
-        visited[row][col] = true;
+        if(x == y) return false;
         
-        while(!q.empty()) {
-            int sz = q.size();
-            while(sz--) {
-                row = q.front().first;
-                col = q.front().second;
-                q.pop();
-                for(int i = 0; i < 4; i++) {
-                    int r = row + offset[i], c = col + offset[i + 1];
-                    if(r >= 0 && r < m && c >= 0 && c < n && !visited[r][c] && grid[r][c] == '1') {
-                        visited[r][c] = true;
-                        q.push({r, c});
-                    }
-                }
-            }
+        if(rank[x] >= rank[y]) {
+            parent[y] = x;
+            rank[x]++;
         }
+        else {
+            parent[x] = y;
+            rank[y]++;
+        }
+        return true;
     }
 };
