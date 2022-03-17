fc265efec71ff4c0b87cc6b52b6df4dab1c51111
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-17 10:24:20 +0900

    Time: 11 ms (30.89%), Space: 16.6 MB (17.55%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -12,14 +12,20 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        return dfs(root->left, root->right);
-    }
-    
-    bool dfs(TreeNode* l, TreeNode* r) {
-        if(!l && !r) return true;
-        if(!l || !r) return false;
-        if(l->val != r->val) return false;
+        stack<pair<TreeNode*, TreeNode*>> s;
+        s.push({root->left, root->right});
         
-        return dfs(l->left, r->right) && dfs(l->right, r->left);
+        while(!s.empty()) {
+            auto l = s.top().first, r = s.top().second;
+            s.pop();
+            
+            if(!l && !r) continue;
+            if(!l || !r) return false;
+            if(l->val != r->val) return false;
+            s.push({l->left, r->right});
+            s.push({l->right, r->left});
+        }
+        
+        return true;
     }
 };
