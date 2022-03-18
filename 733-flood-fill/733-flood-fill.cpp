b8bab7503b8d03fbcf16e1be2429fe96fb704b02
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-18 13:18:45 +0900

    Time: 23 ms (11.74%), Space: 14.3 MB (17.44%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int m = image.size(), n = image[0].size();
         int oldColor = image[sr][sc];
-        if(oldColor == newColor)
-            return image;
+        if(oldColor == newColor) return image;
         
         vector<int> offset = {-1, 0, 1, 0, -1};
         queue<pair<int, int>> q;
+        
         q.push({sr, sc});
+        image[sr][sc] = newColor;
         
         while(!q.empty()) {
-            int n = q.size();
-            while(n--) {
-                sr = q.front().first;
-                sc = q.front().second;
+            int sz = q.size();
+            while(sz--) {
+                int row = q.front().first, col = q.front().second;
                 q.pop();
                 
-                image[sr][sc] = newColor;
-                
                 for(int i = 0; i < 4; i++) {
-                    int r = sr + offset[i], c = sc + offset[i+1];
-                    if(r < 0 || r >= image.size() || c < 0 || c >= image[0].size())
-                        continue;
-                    if(image[r][c] == oldColor)
+                    int r = row + offset[i], c = col + offset[i + 1];
+                    if(r >= 0 && r < m && c >= 0 && c < n && image[r][c] == oldColor) {
+                        image[r][c] = newColor;
                         q.push({r, c});
+                    }
                 }
             }
         }
-        
         
         return image;
     }
