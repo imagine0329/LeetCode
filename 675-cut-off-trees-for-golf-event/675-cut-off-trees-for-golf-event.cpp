6131c195c495c2ff52518d96cec5b9d83bc808b1
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-18 12:15:51 +0900

    Time: 1581 ms (8.79%), Space: 99.7 MB (76.97%) - LeetHub

--- 675-cut-off-trees-for-golf-event/675-cut-off-trees-for-golf-event.cpp
+++ 675-cut-off-trees-for-golf-event/675-cut-off-trees-for-golf-event.cpp
@@ -1,63 +1,62 @@
+typedef pair<int, pair<int, int>> element;
+
 class Solution {
 public:
-    int cutOffTree(vector<vector<int>>& forest) {        
+    int cutOffTree(vector<vector<int>>& forest) {
         int m = forest.size(), n = forest[0].size();
         vector<vector<int>> trees;
         
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(forest[i][j] > 1)
-                    trees.push_back({forest[i][j], i, j});
+        for(int r = 0; r < m; r++) {
+            for(int c = 0; c < n; c++) {
+                if(forest[r][c] > 1)
+                    trees.push_back({forest[r][c], r, c});
             }
         }
         
         sort(trees.begin(), trees.end());
         
+        int row = 0, col = 0;
         int ans = 0;
-        for(int i = 0, cur_row = 0, cur_col = 0; i < trees.size(); i++) {
-            int step = bfs(forest, cur_row, cur_col, trees[i][1], trees[i][2]);
-            if(step == -1)
-                return -1;
+        for(int i = 0; i < trees.size(); i++) {
+            int step = bfs(forest, row, col, trees[i][1], trees[i][2]);
+            if(step == -1) return -1;
             ans += step;
-            cur_row = trees[i][1];
-            cur_col = trees[i][2];
+            row = trees[i][1];
+            col = trees[i][2];
         }
-        
         return ans;
     }
     
-    int bfs(vector<vector<int>>& forest, int row, int col, int target_row, int target_col) {
-        if(row == target_row && col == target_col)
-            return 0;
-        
-        vector<vector<bool>> visited(forest.size(), vector<bool>(forest[0].size(), false));
+    int bfs(vector<vector<int>>& forest, int row, int col, int target_r, int target_c) {
+        if(row == target_r && col == target_c) return 0;
+        int m = forest.size(), n = forest[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         queue<pair<int, int>> q;
-        vector<int> dir = {-1, 0, 1, 0, -1};
+        vector<int> offset = {-1, 0, 1, 0, -1};
+        
+        q.push({row, col});
+        visited[row][col] = true;
         int step = 0;
         
-        visited[row][col] = true;
-        q.push({row, col});
         while(!q.empty()) {
             step++;
-            int n = q.size();
-            while(n--) {
-                int cur_row = q.front().first, cur_col = q.front().second;
+            int sz = q.size();
+            while(sz--) {
+                row = q.front().first; col = q.front().second;
                 q.pop();
+
                 for(int i = 0; i < 4; i++) {
-                    int r = cur_row + dir[i], c = cur_col + dir[i+1];
-                    if(r < 0 || r >= forest.size() || c < 0 || c >= forest[0].size()
-                        || visited[r][c] || forest[r][c] == 0)
-                        continue;
-                    if(r == target_row && c == target_col)
-                        return step;
-                    
-                    visited[r][c] = true;
-                    q.push({r, c});
+                    int r = row + offset[i], c = col + offset[i + 1];
+                    if(r >= 0 && r < m && c >= 0 && c < n && !visited[r][c] && forest[r][c] != 0) {
+                        if(r == target_r && c == target_c) return step;
+                        visited[r][c] = true;
+                        q.push({r, c});
+                    }
                 }
             }
+            
         }
         
         return -1;
     }
-    
 };
