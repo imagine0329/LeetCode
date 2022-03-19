e56d0a5c703af3a5a8a1335ac6edc2e4b2c9ff24
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-19 11:04:33 +0900

    Time: 710 ms, Memory: 64.1 MB - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> q;
+        vector<pair<int, int>> distance;
+        for(int i = 0; i < points.size(); i++)
+            distance.push_back({euclideanDistance(points[i]), i});
         
-        for(int i=0; i<points.size(); i++)
-            q.push({getDistance(points[i]), i});
+        quickSelect(distance, k - 1, 0, distance.size() - 1);
         
         vector<vector<int>> ans;
-        for(int i=0; i<k; i++)
-        {
-            ans.push_back(points[q.top().second]);
-            q.pop();
-        }
+        for(int i = 0; i < k; i++)
+            ans.push_back(points[distance[i].second]);
         
         return ans;
     }
     
-    double getDistance(vector<int> point)
-    {
-        return sqrt(pow(point.front(), 2) + pow(point.back(), 2));
+    int partition(vector<pair<int, int>>& distance, int left, int right) {
+        int pivot = left + (right - left) / 2;
+        swap(distance[pivot], distance[right]);
+        
+        int j = left;
+        while(j < right) {
+            if(distance[j].first < distance[right].first)
+                swap(distance[left++], distance[j]);
+            j++;
+        }
+        
+        swap(distance[left], distance[right]);
+        return left;
+    }
+    
+    void quickSelect(vector<pair<int, int>>& distance, int k, int left, int right) {
+        if(left == right) return;
+        int pivot = partition(distance, left, right);
+        
+        if(pivot == k)
+            return;
+        
+        if(pivot > k)
+            quickSelect(distance, k, left, pivot - 1);
+        else
+            quickSelect(distance, k, pivot + 1, right);
+    }
+    
+    double euclideanDistance(vector<int>& point) {
+        return point[0] * point[0] + point[1] * point[1];
     }
 };
