61d1cb4b9d22feca3bf9e17af5b491112fae5f26
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-19 11:03:25 +0900

    Time: 15 ms (39.60%), Space: 10.2 MB (35.50%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,46 +1,14 @@
 class Solution {
-private:
-    vector<int> nums;
-    
-    int partition(int left, int right, int pivot)
-    {
-        int store = left;
-        swap(nums[pivot], nums[right]);
-        
-        for(int i=left; i<=right; i++)
-        {
-            if(nums[i] < nums[right])
-            {
-                swap(nums[i], nums[store]);
-                store++;
-            }
-        }
-        swap(nums[store], nums[right]);
-        return store;
-    }
-    
-    void quickSelect(int left, int right, int k)
-    {
-        if(left == right)
-            return;
-        
-        int pivot = left + rand() % (right - left + 1);
-        pivot = partition(left, right, pivot);
-        
-        if(pivot == k)
-            return;
-        
-        if(pivot > k)
-            quickSelect(left, pivot-1, k);
-        else
-            quickSelect(pivot+1, right, k);
-    }
-    
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        this->nums = nums;
-        quickSelect(0, nums.size()-1, nums.size()-k);
+        priority_queue<int, vector<int>, greater<int>> pq;
         
-        return this->nums[nums.size()-k];
+        for(auto n : nums) {
+            pq.push(n);
+            if(pq.size() > k)
+                pq.pop();
+        }
+        
+        return pq.top();
     }
 };
