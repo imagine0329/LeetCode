ad16ec67149f5916cd73e786b59a6cfdf3f0e65e
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-19 14:13:52 +0900

    Time: 21 ms (56.84%), Space: 13.6 MB (72.51%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> unique;
         unordered_map<int, int> m;
         
         for(auto n : nums)
             m[n]++;
         
-        for(auto e : m) {
-            pq.push({e.second, e.first});
-            if(pq.size() > k)
-                pq.pop();
-        }
+        for(auto e : m)
+            unique.push_back(e.first);
         
-        vector<int> ans;
-        while(!pq.empty()) {
-            ans.push_back(pq.top().second);
-            pq.pop();
+        int n = unique.size();
+        quickSelect(unique, m, 0, n - 1, n - k);
+        return vector<int>(unique.begin() + n - k, unique.end());
+    }
+    
+    void quickSelect(vector<int>& unique, unordered_map<int, int>& m, int left, int right, int k) {
+        if(left == right) return;
+        
+        int pivot = left + (right - left) / 2;
+        pivot = partition(unique, m, left, right, pivot);
+        
+        if(pivot == k) return;
+        if(pivot < k) quickSelect(unique, m, pivot + 1, right, k);
+        else quickSelect(unique, m, left, pivot - 1, k);
+    }
+    
+    int partition(vector<int>& unique, unordered_map<int, int>& m, int left, int right, int pivot) {
+        int store = left, i = left;
+        swap(unique[pivot], unique[right]);
+        
+        while(i < right) {
+            if(m[unique[i]] < m[unique[right]])
+                swap(unique[store++], unique[i]);
+            
+            i++;
         }
-        return ans;
+        swap(unique[store], unique[right]);
+        return store;
     }
 };
