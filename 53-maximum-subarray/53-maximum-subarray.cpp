006a88a755f670ca2a3b9ad3546e4dd780527873
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-20 16:31:08 +0900

    Time: 164 ms (39.40%), Space: 67.8 MB (11.85%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        return helper(nums, 0, nums.size()-1);
-    }
-    
-    int helper(vector<int>& nums, int left, int right)
-    {
-        if(left > right)
-            return INT_MIN;
+        int maximum = INT_MIN;
         
-        int mid = left + (right - left)/2;
-        int bestLeft = 0, bestRight = 0, sum = 0;
-        
-        for(int i=mid-1; i>=left; i--)
-        {
-            sum += nums[i];
-            bestLeft = max(sum, bestLeft);
+        int sum = 0;
+        for(auto n : nums) {
+            sum += n;
+            if(sum < n)
+                sum = n;
+            maximum = max(maximum, sum);
         }
-        
-        sum = 0;
-        for(int i=mid+1; i<=right; i++)
-        {
-            sum += nums[i];
-            bestRight = max(sum, bestRight);
-        }
-        
-        int combined = bestLeft + bestRight + nums[mid];
-        
-        int leftHalf = helper(nums, left, mid-1);
-        int rightHalf = helper(nums, mid+1, right);
-        
-        return max(combined, max(leftHalf, rightHalf));
+        return maximum;
     }
-    
 };
