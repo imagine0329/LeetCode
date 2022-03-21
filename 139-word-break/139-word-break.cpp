72253ca03c7a07b91ac738a2399562b06fe989d3
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-22 08:33:34 +0900

    Time: 28 ms (31.66%), Space: 13.4 MB (32.64%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        vector<int> memo(s.length(), -1);
-        return helper(dict, s, 0, memo);
-    }
-    
-    bool helper(unordered_set<string>& dict, string s, int start, vector<int>& memo) {
-        int n = s.length();
-        if(start == n) return true;
-        if(memo[start] != -1) return memo[start];
-        for(int end = start; end < n; end++) {
-            string str = s.substr(start, end - start + 1);
-            if(dict.find(str) != dict.end()) {
-                if(helper(dict, s, end + 1, memo)) return memo[start] = 1;
+        queue<int> q;
+        vector<bool> visited(s.length(), false);
+        
+        q.push(0);
+        while(!q.empty()) {
+            int start = q.front(); q.pop();
+            if(visited[start]) continue;
+            for(int end = start + 1; end <= s.length(); end++) {
+                if(dict.find(s.substr(start, end - start)) != dict.end()) {
+                    q.push(end);
+                    if(end == s.length()) return true;
+                }
             }
+            visited[start] = true;
         }
-        return memo[start] = 0;
+        return false;
     }
 };
