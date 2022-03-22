416bcc2c04c27bb60d1a1333f31abd2cc446cf7d
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-03-22 09:06:10 +0900

    Time: 123 ms (48.28%), Space: 16 MB (26.38%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, amount + 1);
-        dp[0] = 0;
+        vector<int> memo(amount + 1);
+        return backtracking(coins, amount, memo);
+    }
+    
+    int backtracking(vector<int>& coins, int remain, vector<int>& memo) {
+        if(remain < 0) return -1;
+        if(remain == 0) return 0;
+        if(memo[remain] != 0) return memo[remain];
         
-        for(int i = 1; i <= amount; i++) {
-            for(auto coin : coins) {
-                if(i >= coin) {
-                    dp[i] = min(dp[i], dp[i - coin] + 1);
-                }
-            }
+        int min = INT_MAX;
+        for(auto coin : coins) {
+            int res = backtracking(coins, remain - coin, memo);
+            if(res >= 0 && min > res)
+                min = res + 1;
         }
         
-        return dp[amount] > amount ? -1 : dp[amount]; 
+        memo[remain] = min == INT_MAX ? -1 : min;
+        return memo[remain];
     }
 };
