5d0e53bb84f467a3f4a3a9b1905b6f1fcaa2cba6
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-02 16:32:11 +0900

    Time: 556 ms (19.71%), Space: 64 MB (62.18%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, vector<int>>> q;
+        vector<pair<int, int>> distance;
+        for(int i = 0; i < points.size(); i++)
+            distance.push_back({points[i][0] * points[i][0] + points[i][1] * points[i][1], i});
         
-        for(auto p : points) {
-            q.push({p[0] * p[0] + p[1] * p[1], p});
-            if(q.size() > k)
-                q.pop();
-        }
+        quickSelect(distance, k - 1, 0, points.size() - 1);
         
         vector<vector<int>> ans;
-        while(!q.empty()) {
-            ans.push_back(q.top().second);
-            q.pop();
-        }
+        for(int i = 0; i < k; i++)
+            ans.push_back(points[distance[i].second]);
         
         return ans;
+    }
+    
+    void quickSelect(vector<pair<int, int>>& distance, int k, int left, int right) {
+        if(left == right) return;
+        
+        int pivot = (left + right) / 2;
+        pivot = partition(distance, left, right, pivot);
+        
+        if(pivot == k) return;
+        if(pivot > k) quickSelect(distance, k, left, pivot - 1);
+        else quickSelect(distance, k, pivot + 1, right);
+    }
+    
+    int partition(vector<pair<int, int>>& distance, int left, int right, int pivot) {
+        swap(distance[right], distance[pivot]);
+        
+        int i = left;
+        while(i < right) {
+            if(distance[i].first < distance[right].first)
+                swap(distance[left++], distance[i]);
+            i++;
+        }
+        swap(distance[left], distance[right]);
+        return left;
     }
 };
