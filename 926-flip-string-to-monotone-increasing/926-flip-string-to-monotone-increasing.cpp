44a4cefba2a0042d7aa5399b0770d5528b657149
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-02 16:52:11 +0900

    Time: 45 ms (36.24%), Space: 11 MB (94.01%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
-        int n = s.length();
-        vector<int> p(n + 1, 0);
+        int one = 0, flip = 0;
         
-        for(int i = 0; i < n; i++)
-            p[i + 1] = p[i] + (s[i] - '0');
-        
-        int count = INT_MAX;
-        for(int i = 0; i <= n; i++)
-            count = min(count, p[i] + (n - i) - (p[n] - p[i]));
-        
-        return count;
+        for(auto c : s) {
+            if(c == '1')
+                one++;
+            else
+                flip++;
+            flip = min(one, flip);
+        }
+        return flip;
     }
 };
