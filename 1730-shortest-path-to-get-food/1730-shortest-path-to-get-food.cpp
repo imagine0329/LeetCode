b836756e989b55e248a31282d0442f4446a5aacc
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-04 16:42:45 +0900

    Time: 74 ms (85.13%), Space: 19.5 MB (47.52%) - LeetHub

--- 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.cpp
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     int getFood(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
         queue<pair<int, int>> q;
-        vector<int> offset = {-1, 0, 1, 0, -1};
+        vector<vector<int>> visited(m, vector<int>(n, false));
         
-        for(int row = 0; row < grid.size(); row++) {
-            for(int col = 0; col < grid[0].size(); col++) {
+        int found = false;
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
                 if(grid[row][col] == '*') {
                     q.push({row, col});
+                    visited[row][col] = true;
+                    found = true;
                     break;
                 }
             }
+            if(found) break;
         }
         
-        int path = 0;
+        int len = 0;
+        vector<int> offset = {-1, 0, 1, 0, -1};
         while(!q.empty()) {
-            ++path;
-            int n = q.size();
-            while(n--) {
-                auto [row, col] = q.front(); q.pop();
+            len++;
+            int sz = q.size();
+            while(sz--) {
+                int row = q.front().first, col = q.front().second;
+                q.pop();
+                
                 for(int i = 0; i < 4; i++) {
                     int r = row + offset[i], c = col + offset[i + 1];
-                    if(r >= 0 && r < grid.size() && c >= 0 && c < grid[0].size() && grid[r][c] != 'X') {
-                        if(grid[r][c] == '#')
-                            return path;
+                    if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] != 'X' && !visited[r][c]) {
+                        if(grid[r][c] == '#') return len;
                         q.push({r, c});
-                        grid[r][c] = 'X';
+                        visited[r][c] = true;
                     }
-                        
                 }
             }
         }
