d2ff7cc390bec50bdb1f66d0c2f5bf7664c8a195
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-04 15:27:13 +0900

    Time: 7 ms (72.42%), Space: 18.3 MB (5.54%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -12,28 +12,31 @@ public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         unordered_map<int, unordered_set<int>> graph;
         preorder(root, nullptr, graph);
+        
+        queue<int> q;
         vector<int> ans;
         unordered_set<int> visited;
-        queue<int> q;
+        
         q.push(target->val);
+        visited.insert(target->val);
         int dist = 0;
         while(dist <= k && !q.empty()) {
             int n = q.size();
             while(n--) {
                 int u = q.front(); q.pop();
-                visited.insert(u);
                 if(dist == k)
                     ans.push_back(u);
                 else {
                     for(auto v : graph[u]) {
-                        if(visited.find(v) == visited.end())
+                        if(visited.find(v) == visited.end()) {
                             q.push(v);
+                            visited.insert(v);
+                        }
                     }
                 }
             }
             dist++;
         }
-        
         return ans;
     }
     
