b4341fb9eb5666252da76cd77ae07c7f309817d5
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-04 18:03:24 +0900

    Time: 30 ms (82.35%), Space: 17.2 MB (35.74%) - LeetHub

--- 370-range-addition/370-range-addition.cpp
+++ 370-range-addition/370-range-addition.cpp
@@ -3,13 +3,14 @@ public:
     vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
         vector<int> ans(length, 0);
         
-        for(auto u : updates) {
-            ans[u[0]] += u[2];
-            if(u[1] < length - 1)
-                ans[u[1] + 1] -= u[2];
+        for(auto v : updates) {
+            ans[v[0]] += v[2];
+            if(v[1] < length - 1)
+                ans[v[1] + 1] -= v[2];
         }
         
-        partial_sum(ans.begin(), ans.end(), ans.begin());
+        for(int i = 1; i < length; i++)
+            ans[i] += ans[i - 1];
         
         return ans;
     }
