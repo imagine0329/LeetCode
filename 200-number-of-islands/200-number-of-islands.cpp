f8b833b9193075dc2fcb7b9d6a0747eb0f2c38af
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-05 18:27:40 +0900

    Time: 51 ms (43.28%), Space: 14.6 MB (23.20%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,33 +1,78 @@
+class UnionFind {
+private:
+    vector<int> parent, rank;
+    int count = 0;
+    
+public:
+    UnionFind(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        rank = vector<int>(m * n, 0);
+        
+        for(int r = 0; r < m; r++) {
+            for(int c = 0; c < n; c++) {
+                if(grid[r][c] == '1') {
+                    parent.push_back(r * n + c);
+                    count++;
+                }
+                else
+                    parent.push_back(-1);
+            }
+        }
+    }
+    
+    int find(int x) {
+        if(parent[x] != x)
+            return parent[x] = find(parent[x]);
+        return x;
+    }
+    
+    bool Union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if(x == y)
+            return false;
+        
+        if(rank[x] >= rank[y]) {
+            parent[y] = x;
+            rank[x]++;
+        }
+        else {
+            parent[x] = y;
+            rank[y]++;
+        }
+        count--;
+        return true;
+    }
+    
+    int getCount() {
+        return count;
+    }
+};
+
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        queue<pair<int, int>> q;
         vector<int> offset = {-1, 0, 1, 0, -1};
         
-        int islands = 0;
-        for(int row = 0; row < m; row++) {
-            for(int col = 0; col < n; col++) {
-                if(!visited[row][col] && grid[row][col] == '1') {
-                    islands++;
-                    visited[row][col] = true;
-                    q.push({row, col});
-                    while(!q.empty()) {
-                        int r = q.front().first, c = q.front().second;
-                        q.pop();
-                        for(int i = 0; i < 4; i++) {
-                            int nr = r + offset[i], nc = c + offset[i + 1];
-                            if(nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc] 
-                               && grid[nr][nc] == '1') {
-                                visited[nr][nc] = true;
-                                q.push({nr, nc});
-                            }
+        UnionFind uf(grid);
+        int ans = 0;
+        for(int r = 0; r < m; r++) {
+            for(int c = 0; c < n; c++) {
+                if(grid[r][c] == '1') {
+                    ans++;
+                    for(int i = 0; i < 4; i++) {
+                        int row = r + offset[i], col = c + offset[i + 1];
+                        if(row >= 0 && row < m && col >= 0 && col < n && grid[row][col] == '1') {
+                            if(uf.Union(r * n + c, row * n + col))
+                                ans--;
                         }
+                            
                     }
                 }
             }
         }
-        return islands;
+        
+        return ans;
     }
 };
