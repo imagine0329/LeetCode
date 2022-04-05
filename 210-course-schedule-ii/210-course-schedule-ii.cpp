83851ba80a61a382700da1039cf29822cd2eebae
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-05 17:23:28 +0900

    Time: 27 ms (62.88%), Space: 14.9 MB (25.21%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> m;
-        vector<int> visited(numCourses, -1);
         vector<int> ans;
+        vector<int> visited(numCourses, -1);
+        vector<vector<int>> graph(numCourses);
         
         for(auto pre : prerequisites)
-            m[pre[1]].push_back(pre[0]);
+            graph[pre[1]].push_back(pre[0]);
         
-        for(int i = 0; i < numCourses; i++) {
-            if(visited[i] == -1) {
-                if(dfs(m, visited, ans, i) == false)
-                    return {};
-            }
+        for(int i = 0 ; i < numCourses; i++) {
+            if(visited[i] != 1 && !dfs(graph, i, visited, ans))
+                return {};
         }
+        
         reverse(ans.begin(), ans.end());
+        
         return ans;
     }
     
-    bool dfs(unordered_map<int, vector<int>>& m, vector<int>& visited, vector<int>& ans, int v) {
+    bool dfs(vector<vector<int>>& graph, int v, vector<int>& visited, vector<int>& ans) {
+        if(visited[v] == 0) return false;
         visited[v] = 0;
         
-        for(auto u : m[v]) {
-            if(visited[u] == 0)
-                return false;
-            if(visited[u] == -1 && dfs(m, visited, ans, u) == false)
+        for(auto u : graph[v]) {
+            if(visited[u] != 1 && !dfs(graph, u, visited, ans))
                 return false;
         }
         ans.push_back(v);
