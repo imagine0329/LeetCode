2320939feb77e1be1cdee2216ac6584baed7480a
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-05 17:59:46 +0900

    Time: 52 ms (41.29%), Space: 13.4 MB (25.66%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -3,6 +3,8 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
+        queue<pair<int, int>> q;
+        vector<int> offset = {-1, 0, 1, 0, -1};
         
         int islands = 0;
         for(int row = 0; row < m; row++) {
@@ -10,23 +12,22 @@ public:
                 if(!visited[row][col] && grid[row][col] == '1') {
                     islands++;
                     visited[row][col] = true;
-                    dfs(grid, visited, row, col);
+                    q.push({row, col});
+                    while(!q.empty()) {
+                        int r = q.front().first, c = q.front().second;
+                        q.pop();
+                        for(int i = 0; i < 4; i++) {
+                            int nr = r + offset[i], nc = c + offset[i + 1];
+                            if(nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc] 
+                               && grid[nr][nc] == '1') {
+                                visited[nr][nc] = true;
+                                q.push({nr, nc});
+                            }
+                        }
+                    }
                 }
             }
         }
         return islands;
-    }
-    
-    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col) {
-        int m = grid.size(), n = grid[0].size();
-        vector<int> offset = {-1, 0, 1, 0, -1};
-        
-        for(int i = 0; i < 4; i++) {
-            int r = row + offset[i], c = col + offset[i + 1];
-            if(r >= 0 && r < m && c >= 0 && c < n && !visited[r][c] && grid[r][c] == '1') {
-                visited[r][c] = true;
-                dfs(grid, visited, r, c);
-            }
-        }
     }
 };
