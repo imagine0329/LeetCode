7a7e2ca0ed9073a8ff7b08d1354e3cea1c04bb52
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-06 16:55:46 +0900

    Time: 48 ms (59.83%), Space: 19 MB (66.64%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -7,10 +7,8 @@ public:
         ans.push_back(intervals[0]);
         
         for(int i = 1; i < intervals.size(); i++) {
-            if(ans.back()[1] >= intervals[i][0]) {
-                ans.back()[0] = min(ans.back()[0], intervals[i][0]);
+            if(ans.back()[1] >= intervals[i][0]) 
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-            }
             else
                 ans.push_back(intervals[i]);
         }
