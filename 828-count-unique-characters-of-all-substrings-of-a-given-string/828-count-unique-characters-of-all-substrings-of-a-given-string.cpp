850c869128622e61b6b4f37124a63e6698703bf1
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-07 17:44:37 +0900

    Time: 27 ms (96.01%), Space: 11.3 MB (94.05%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int uniqueLetterString(string s) {
-        vector<vector<int>> idx(26, vector<int>(2, -1));
+        int index[26][2];
         int n = s.length(), mod = pow(10, 9) + 7;
+        int ans = 0;
         
-        int res = 0;
+        memset(index, -1, sizeof(int) * 52);
+        
         for(int i = 0; i < n; i++) {
             int c = s[i] - 'A';
-            res = (res + (i - idx[c][1]) * (idx[c][1] - idx[c][0]) % mod) % mod;
-            idx[c][0] = idx[c][1];
-            idx[c][1] = i;
+            ans = ans + (i - index[c][1]) * (index[c][1] - index[c][0]);
+            index[c][0] = index[c][1];
+            index[c][1] = i;
         }
         
-        for(int c = 0; c < 26; c++)
-            res = (res + (n - idx[c][1]) * (idx[c][1] - idx[c][0]) % mod) % mod;
+        for(int i = 0; i < 26; i++)
+            ans = ans + (n - index[i][1]) * (index[i][1] - index[i][0]);
         
-        return res;
+        return ans;
     }
 };
