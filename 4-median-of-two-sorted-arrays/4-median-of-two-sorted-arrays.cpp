90d76f25d452d4a572d81d0c891dde5acbd4cafe
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-08 15:54:41 +0900

    Time: 52 ms (55.69%), Space: 89.3 MB (64.06%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -6,17 +6,18 @@ public:
         
         int lo = 0, hi = n1;
         while(lo <= hi) {
-            int cut1 = (lo + hi) / 2;
-            int cut2 = (n1 + n2) / 2 - cut1;
+            int c1 = (lo + hi) / 2;
+            int c2 = (n1 + n2) / 2 - c1;
             
-            int l1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
-            int l2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
-            int r1 = cut1 == n1 ? INT_MAX : nums1[cut1];
-            int r2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+            int l1 = c1 == 0 ? INT_MIN : nums1[c1 - 1];
+            int l2 = c2 == 0 ? INT_MIN : nums2[c2 - 1];
+            int r1 = c1 == n1 ? INT_MAX : nums1[c1];
+            int r2 = c2 == n2 ? INT_MAX : nums2[c2];
             
-            if(l1 > r2) hi = cut1 - 1;
-            else if(l2 > r1) lo = cut1 + 1;
-            else return (n1 + n2) % 2 ? min(r1, r2) : (max(l1, l2) + min(r1, r2)) * 0.5;
+            if(l1 > r2) hi = c1 - 1;
+            else if(l2 > r1) lo = c1 + 1;
+            else
+                return (n1 + n2) % 2 ? min(r1, r2) : (max(l1, l2) + min(r1, r2)) * 0.5;
         }
         
         return -1;
