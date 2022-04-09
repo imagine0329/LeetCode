02f90f1cb528a12d0d5994c05acf471f96f0b08d
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-09 11:33:44 +0900

    Time: 600 ms (64.86%), Space: 13 MB (52.39%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -1,42 +1,40 @@
 class Trie {
 public:
-    Trie* next[26] = {nullptr};
+    Trie* next[26] = { nullptr };
     string* word = nullptr;
     
-    Trie() {}
-    void insert(string& s) {
+    void insert(string& word) {
         Trie* node = this;
-        for(auto c : s) {
-            c -= 'a';
-            if(!node->next[c])
-                node->next[c] = new Trie();
-            
-            node = node->next[c];
+        for(auto c : word) {
+            if(!node->next[c - 'a'])
+                node->next[c - 'a'] = new Trie();
+            node = node->next[c - 'a'];
         }
-        node->word = &s;
+        node->word = &word;
     }
-    
 };
 
 class Solution {
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        int m = board.size(), n = board[0].size();
         Trie* trie = new Trie();
         
-        for(auto& s : words)
-            trie->insert(s);
+        for(auto& word : words)
+            trie->insert(word);
         
         vector<string> ans;
-        for(int row = 0; row < board.size(); row++) {
-            for(int col = 0; col < board[0].size(); col++)
-                dfs(board, trie, ans, row, col);
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++)
+                dfs(board, trie, row, col, ans);
         }
         
         return ans;
     }
     
-    void dfs(vector<vector<char>>& board, Trie* trie, vector<string>& ans, int row, int col) {
-        if(board[row][col] == '#' || !trie->next[board[row][col] - 'a']) return;
+    void dfs(vector<vector<char>>& board, Trie* trie, int row, int col, vector<string>& ans) {
+        if(board[row][col] == '#' || !trie->next[board[row][col] - 'a'])
+            return;
         
         char org = board[row][col];
         trie = trie->next[org - 'a'];
@@ -45,12 +43,11 @@ public:
             trie->word = nullptr;
         }
         
-        
         board[row][col] = '#';
-        if(row - 1 >= 0) dfs(board, trie, ans, row - 1, col);
-        if(row + 1 < board.size()) dfs(board, trie, ans, row + 1, col);
-        if(col - 1 >= 0) dfs(board, trie, ans, row, col - 1);
-        if(col + 1 < board[0].size()) dfs(board, trie, ans, row, col + 1);
+        if(row - 1 >= 0) dfs(board, trie, row - 1, col, ans);
+        if(row + 1 < board.size()) dfs(board, trie, row + 1, col, ans);
+        if(col - 1 >= 0) dfs(board, trie, row, col - 1, ans);
+        if(col + 1 < board[0].size()) dfs(board, trie, row, col + 1, ans);
         board[row][col] = org;
     }
 };
