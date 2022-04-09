46d26f28e6d74883b0dd3b73b4273a6311d5faf7
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-09 10:07:23 +0900

    Time: 113 ms (82.72%), Space: 32.1 MB (10.75%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -3,6 +3,7 @@ public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_map<string, vector<string>> m;
         unordered_set<string> seen;
+        queue<string> q;
         
         for(auto s : wordList) {
             for(int i = 0; i < s.length(); i++) {
@@ -12,29 +13,27 @@ public:
             }
         }
         
-        queue<string> q;
-        int step = 0;
-        
         q.push(beginWord);
         seen.insert(beginWord);
+        int step = 0;
         
         while(!q.empty()) {
             step++;
             int n = q.size();
             while(n--) {
-                string str = q.front(); q.pop();
-                if(str == endWord) return step;
-                for(auto& c : str) {
-                    char org = c;
-                    c = '*';
-                    if(m.find(str) != m.end()) {
-                        for(auto s : m[str]) {
-                            q.push(s);
-                            seen.insert(s);
+                string word = q.front(); q.pop();
+                if(word == endWord)
+                    return step;
+                
+                for(int i = 0; i < word.length(); i++) {
+                    string s = word;
+                    s[i] = '*';
+                    for(auto e : m[s]) {
+                        if(seen.find(e) == seen.end()) {
+                            q.push(e);
+                            seen.insert(e);
                         }
-                        m.erase(str);
                     }
-                    c = org;
                 }
             }
         }
