27ae2606544a9b92c5a7fb4030cac5da9373d384
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-11 05:56:06 +0900

    Time: 267 ms (66.41%), Space: 38.3 MB (97.27%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.cpp
+++ 472-concatenated-words/472-concatenated-words.cpp
@@ -1,53 +1,24 @@
-class Trie {
-public:
-    Trie* next[26] = {nullptr};
-    bool isWord = false;
-    
-    Trie() {}
-    
-    void insert(string s) {
-        Trie* node = this;
-        for(auto c : s) {
-            c -= 'a';
-            if(node->next[c] == nullptr)
-                node->next[c] = new Trie();
-            node = node->next[c];
-        }
-        node->isWord = true;
-    }
-    
-    bool search(string s) {
-        Trie* node = this;
-        for(auto c : s) {
-            c -= 'a';
-            if(node->next[c] == nullptr) return false;
-            node = node->next[c];
-        }
-        return node->isWord;
-    }
-};
-
 class Solution {
 public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        Trie* trie = new Trie();
+        unordered_set<string> dict(words.begin(), words.end());
         vector<string> ans;
         
-        for(auto s : words)
-            trie->insert(s);
-        
-        for(auto s : words) {
-            if(dfs(trie, s)) ans.push_back(s);
+        for(auto word : words) {
+            if(isConcatenate(dict, word))
+                ans.push_back(word);
         }
+        
         return ans;
     }
     
-    bool dfs(Trie* root, string s) {
-        for(int i = 1; i < s.length(); i++) {
-            string prefix = s.substr(0, i);
-            if(root->search(prefix)) {
-                string suffix = s.substr(i);
-                if(root->search(suffix) || dfs(root, suffix)) return true;
+    bool isConcatenate(unordered_set<string>& dict, string word) {
+        for(int len = 1; len < word.length(); len++) {
+            if(dict.find(word.substr(0, len)) != dict.end()) {
+                if(dict.find(word.substr(len)) != dict.end())
+                    return true;
+                else if(isConcatenate(dict, word.substr(len)))
+                    return true;
             }
         }
         return false;
