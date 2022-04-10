a95414eecc6d2da4eeb41106bc1342749c641010
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-11 06:19:56 +0900

    Time: 372 ms (39.75%), Space: 54.8 MB (56.89%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.cpp
+++ 472-concatenated-words/472-concatenated-words.cpp
@@ -3,24 +3,26 @@ public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         unordered_set<string> dict(words.begin(), words.end());
         vector<string> ans;
-        
+        unordered_map<string, bool> memo;
         for(auto word : words) {
-            if(isConcatenate(dict, word))
+            if(isConcatenate(dict, word, memo))
                 ans.push_back(word);
         }
         
         return ans;
     }
     
-    bool isConcatenate(unordered_set<string>& dict, string word) {
+    bool isConcatenate(unordered_set<string>& dict, string word, unordered_map<string, bool>& memo) {
+        if(memo.find(word) != memo.end())
+            return memo[word];
+        
         for(int len = 1; len < word.length(); len++) {
             if(dict.find(word.substr(0, len)) != dict.end()) {
-                if(dict.find(word.substr(len)) != dict.end())
-                    return true;
-                else if(isConcatenate(dict, word.substr(len)))
-                    return true;
+                string s = word.substr(len);
+                if(dict.find(s) != dict.end() || isConcatenate(dict, s, memo))
+                    return memo[word] = true;
             }
         }
-        return false;
+        return memo[word] = false;
     }
 };
