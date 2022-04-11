71a839ae8cff44d56acfaf6d69f7ea9cb9db666c
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-11 16:39:42 +0900

    Time: 86 ms (36.43%), Space: 20.6 MB (24.60%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,17 +1,22 @@
 struct Compare {
     bool operator()(vector<int>& a, vector<int>& b) {
-        return a[1] > b[1];
+        return a[1] < b[1];
     }
 };
 
 class Solution {
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(), boxTypes.end(), Compare());
+        priority_queue<vector<int>, vector<vector<int>>, Compare> q;
+        
+        for(auto b : boxTypes)
+            q.push(b);
+        
         int count = 0;
-        for(auto box : boxTypes) {
-            int n = min(box[0], truckSize);
-            count += (n * box[1]);
+        while(!q.empty()) {
+            int n = min(q.top()[0], truckSize);
+            count += n * q.top()[1];
+            q.pop();
             truckSize -= n;
             if(truckSize == 0)
                 break;
