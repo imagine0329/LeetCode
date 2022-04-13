a8a2ec4f90467a220947cfb174bdbe8dbed91086
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-13 10:46:37 +0900

    Time: 21 ms (70.58%), Space: 12.3 MB (98.49%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.cpp
+++ 909-snakes-and-ladders/909-snakes-and-ladders.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
         int n = board.size();
+        vector<bool> visited(n * n + 1, false);
         queue<pair<int, int>> q;
-        vector<int> visited(n * n + 1, false);
         
         q.push({1, 0});
         visited[1] = true;
@@ -11,7 +11,8 @@ public:
         while(!q.empty()) {
             int p = q.front().first, step = q.front().second;
             q.pop();
-            if(p == n * n) return step;
+            if(p == n * n) 
+                return step;
             
             for(int i = 1; i <= 6 && p + i <= n * n; i++) {
                 int val = getBoardValue(board, p + i);
@@ -27,11 +28,11 @@ public:
     
     int getBoardValue(vector<vector<int>>& board, int p) {
         int n = board.size();
-        int r = n - ((p - 1) / n) - 1;
         int c = (p - 1) % n;
-        if(((n % 2 == 1) && (r % 2 == 1)) || ((n % 2 == 0) && (r % 2 == 0)))
-            c = n - c - 1;
+        int r = n - ((p - 1) / n) - 1;
         
+        if(((n % 2 == 0) && (r % 2 == 0)) || ((n % 2 == 1) && (r % 2 == 1)))
+            c = n - c - 1;
         return board[r][c];
     }
 };
