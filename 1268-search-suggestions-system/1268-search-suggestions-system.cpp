847a913e7d22434820f94d0be444a1a8579769d8
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-15 07:49:01 +0900

    Time: 169 ms (48.30%), Space: 137.4 MB (20.23%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,11 +1,34 @@
+/*
+1. parameters
+    string array; product names
+        -> list length
+        -> string length
+        -> all unique strings
+        -> only lowercase
+    string word; searched word
+        -> word length
+        -> only lowercase
+2. returns
+    each character typed
+    
+    word = "amazon"
+    -> "a" -> "am" -> "ama" -> "amaz"
+    string array<string array>
+3. product names > 3
+    return 3 lexicographically minimum words
+    
+*/
+
 class Trie {
-public:
+private:
     Trie* next[26] = {nullptr};
+    bool isEnd = false;
     vector<string> words;
     
+public:
     Trie() {}
     
-    void insert(string s) {
+    void insert(string& s) {
         Trie* node = this;
         for(auto c : s) {
             c -= 'a';
@@ -14,6 +37,7 @@ public:
             node = node->next[c];
             node->words.push_back(s);
         }
+        node->isEnd = true;
     }
     
     vector<string> search(string s) {
@@ -34,18 +58,20 @@ public:
         Trie* trie = new Trie();
         sort(products.begin(), products.end());
         
-        for(auto p : products)
-            trie->insert(p);
+        for(auto& s : products)
+            trie->insert(s);
         
-        vector<vector<string>> ans(searchWord.length());
-        
+        vector<vector<string>> ans;
         string str;
-        for(int i = 0; i < searchWord.length(); i++) {
-            str += searchWord[i];
-            vector<string> ret = trie->search(str);
-            int n = ret.size();
-            for(int j = 0; j < min(3, n); j++)
-                ans[i].push_back(ret[j]);
+        for(auto c : searchWord) {
+            str += c;
+            vector<string> words = trie->search(str);
+            vector<string> v;
+            int n = words.size();
+            for(int i = 0; i < min(3, n); i++) {
+                v.push_back(words[i]);
+            }
+            ans.push_back(v);
         }
         
         return ans;
