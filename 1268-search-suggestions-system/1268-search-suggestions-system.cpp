43dd6cbc2e291f6be36037eb210cf235db3b3a1f
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-15 08:21:20 +0900

    Time: 44 ms (92.57%), Space: 23.4 MB (97.16%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -18,73 +18,22 @@
     return 3 lexicographically minimum words
     
 */
-
-class Trie {
-private:
-    Trie* next[26] = {nullptr};
-    bool isEnd = false;
-    
-public:
-    Trie() {}
-    
-    void insert(string& s) {
-        Trie* node = this;
-        for(auto c : s) {
-            c -= 'a';
-            if(node->next[c] == nullptr)
-                node->next[c] = new Trie();
-            node = node->next[c];
-        }
-        node->isEnd = true;
-    }
-    
-    vector<string> search(string s) {
-        Trie* node = this;
-        for(auto c : s) {
-            c -= 'a';
-            if(node->next[c] == nullptr)
-                return {};
-            node = node->next[c];
-        }
-        
-        vector<string> result;
-        dfs(node, s, result);
-        
-        return result;
-    }
-    
-    void dfs(Trie* node, string word, vector<string>& result) {
-        if(result.size() == 3)
-            return;
-        
-        if(node->isEnd)
-            result.push_back(word);
-        
-        for(int i = 0; i < 26; i++) {
-            if(node->next[i]) {
-                word += i + 'a';
-                dfs(node->next[i], word, result);
-                word.pop_back();
-            }
-        }
-    }
-};
-
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        Trie* trie = new Trie();
+        int n = products.size();
         sort(products.begin(), products.end());
         
-        for(auto& s : products)
-            trie->insert(s);
-        
-        vector<vector<string>> ans;
         string str;
-        for(auto c : searchWord) {
-            str += c;
-            vector<string> words = trie->search(str);
-            ans.push_back(words);
+        vector<vector<string>> ans(searchWord.length());
+        for(int i = 0; i < searchWord.length(); i++) {
+            str += searchWord[i];
+            int start = lower_bound(products.begin(), products.end(), str) - products.begin();
+            for(int j = start; j < min(start + 3, n); j++) {
+                if(products[j].find(str) == string::npos) 
+                    break;
+                ans[i].push_back(products[j]);
+            }
         }
         
         return ans;
