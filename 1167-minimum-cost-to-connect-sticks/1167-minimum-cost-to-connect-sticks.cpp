5b8e6d0bb7f527b8404c653c6c244688762ca2b4
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-15 08:42:58 +0900

    Time: 108 ms (51.83%), Space: 24.3 MB (20.99%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
@@ -1,12 +1,26 @@
+/*
+- sticks = int array
+    len of array
+    len of stick
+
+- sticks[i] = len of ith stick
+- connect 2 sticks into one stick
+    cost = s[x] + s[y]
+- connect all sticks until 1 stick remaining
+- return
+    min cost of all connecting
+
+*/
+
 class Solution {
 public:
     int connectSticks(vector<int>& sticks) {
         priority_queue<int, vector<int>, greater<int>> q;
+        
+        for(auto s : sticks)
+            q.push(s);
+        
         int cost = 0;
-        
-        for(auto stick : sticks)
-            q.push(stick);
-        
         while(q.size() > 1) {
             int sum = q.top(); q.pop();
             sum += q.top(); q.pop();
