161713ac2d854b72cd203eb9a6f6aa6f86e9b25a
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-15 13:15:20 +0900

    Time: 7 ms (80.23%), Space: 12.9 MB (94.86%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -2,44 +2,40 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        queue<pair<int, int>> q;
-        int fresh = 0;
+        int time = 2;
+        
+        while(helper(grid, time))
+            time++;
         
         for(int row = 0; row < m; row++) {
             for(int col = 0; col < n; col++) {
-                if(grid[row][col] == 2)
-                    q.push({row, col});
                 if(grid[row][col] == 1)
-                    fresh++;
+                    return -1;
             }
         }
         
-        if(fresh == 0)
-            return 0;
-        
-        int step = -1;
+        return time - 2;
+    }
+    
+    bool helper(vector<vector<int>>& grid, int time) {
+        int m = grid.size(), n = grid[0].size();
         vector<int> offset = {-1, 0, 1, 0, -1};
-        while(!q.empty()) {
-            step++;
-            int sz = q.size();
-            while(sz--) {
-                int row = q.front().first, col = q.front().second;
-                q.pop();
-                for(int i = 0; i < 4; i++) {
-                    int r = row + offset[i], c = col + offset[i + 1];
-                    if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1) {
-                        grid[r][c] = 2;
-                        fresh--;
-                        q.push({r, c});
+        bool continued = false;
+        
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                if(grid[row][col] == time) {
+                    for(int i = 0; i < 4; i++) {
+                        int r = row + offset[i], c = col + offset[i + 1];
+                        if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1) {
+                            grid[r][c] = time + 1;
+                            continued = true;
+                        }
                     }
                 }
             }
-            
         }
         
-        if(fresh != 0)
-            return -1;
-        
-        return step;
+        return continued;
     }
 };
