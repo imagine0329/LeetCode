03eac8379b822ee1e99467127229058932883f0a
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-15 13:22:50 +0900

    Time: 220 ms (84.41%), Space: 62.5 MB (70.47%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<int, int>> distance;
-        for(int i = 0; i < points.size(); i++)
-            distance.push_back({points[i][0] * points[i][0] + points[i][1] * points[i][1], i});
+        priority_queue<pair<int, int>> q;
         
-        quickSelect(distance, k - 1, 0, points.size() - 1);
+        for(int i = 0; i < points.size(); i++) {
+            q.push({points[i][0] * points[i][0] + points[i][1] * points[i][1], i});
+            if(q.size() > k)
+                q.pop();
+        }
         
         vector<vector<int>> ans;
-        for(int i = 0; i < k; i++)
-            ans.push_back(points[distance[i].second]);
+        while(!q.empty()) {
+            int i = q.top().second;
+            q.pop();
+            ans.push_back(points[i]);
+        }
         
         return ans;
-    }
-    
-    void quickSelect(vector<pair<int, int>>& distance, int k, int left, int right) {
-        if(left == right) return;
-        
-        int pivot = (left + right) / 2;
-        pivot = partition(distance, left, right, pivot);
-        
-        if(pivot == k) return;
-        if(pivot > k) quickSelect(distance, k, left, pivot - 1);
-        else quickSelect(distance, k, pivot + 1, right);
-    }
-    
-    int partition(vector<pair<int, int>>& distance, int left, int right, int pivot) {
-        swap(distance[right], distance[pivot]);
-        
-        int i = left;
-        while(i < right) {
-            if(distance[i].first < distance[right].first)
-                swap(distance[left++], distance[i]);
-            i++;
-        }
-        swap(distance[left], distance[right]);
-        return left;
     }
 };
