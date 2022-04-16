9acee3631b1583d3d196e29301334df55d2c6d0e
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-17 04:01:12 +0900

    Time: 34 ms (38.94%), Space: 15.4 MB (16.10%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,32 +1,52 @@
+/*
+return
+    - order of courses I should take to finish all courses
+        if many valid answers -> return any of them
+    - impossible to finish all courses -> return an empty array
+    
+    
+*/
+
+
+//  visited status
+//  -1 = not yet, 0 = visiting, 1 = visited
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> graph;
+        vector<int> visited(numCourses, -1);
         vector<int> ans;
-        vector<vector<int>> graph(numCourses);
-        vector<int> indegree(numCourses);
         
-        for(auto pre : prerequisites) {
+        for(auto pre : prerequisites)
             graph[pre[1]].push_back(pre[0]);
-            indegree[pre[0]]++;
-        }
         
-        queue<int> q;
         for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) 
-                q.push(i);
-        }
-        
-        if(q.empty()) return {};
-        
-        while(!q.empty()) {
-            int v = q.front(); q.pop();
-            ans.push_back(v);
-            for(auto u : graph[v]) {
-                if(--indegree[u] == 0)
-                    q.push(u);
+            if(visited[i] == -1) {
+                if(dfs(graph, visited, i, ans) == false)
+                    return {};
             }
         }
         
-        return ans.size() == numCourses ? ans : vector<int>();
+        reverse(ans.begin(), ans.end());
+            
+        return ans;
+    }
+    
+    bool dfs(unordered_map<int, vector<int>>& graph, vector<int>& visited, int v, vector<int>& ans) {
+        if(visited[v] == 0)
+            return false;
+        
+        visited[v] = 0;
+        
+        for(auto u : graph[v]) {
+            if(visited[u] != 1) {
+                if(dfs(graph, visited, u, ans) == false)
+                    return false;
+            }
+        }
+        
+        ans.push_back(v);
+        visited[v] = 1;
+        return true;
     }
 };
