4ef18e79e7206cca6c65fd8f3b062118d3fca8e3
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-17 08:14:10 +0900

    Time: 2349 ms (5.02%), Space: 238.6 MB (5.25%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        for(int row = 0; row < board.size(); row++) {
-            for(int col = 0; col < board[0].size(); col++) {
-                if(dfs(board, word, 0, row, col))
+        int m = board.size(), n = board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                if(backtracking(board, word, 0, row, col, visited))
                     return true;
             }
         }
-        
         return false;
     }
     
-    bool dfs(vector<vector<char>>& board, string word, int idx, int row, int col) {
-        if(idx == word.length()) return true;
-        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size()) return false;
-        if(board[row][col] != word[idx]) return false;
+    bool backtracking(vector<vector<char>>& board, string word, int idx, int row, int col,
+                     vector<vector<bool>>& visited) {
+        int m = board.size(), n = board[0].size();
+        if(word.length() == idx)
+            return true;
         
-        board[row][col] = '#';
+        if(row < 0 || row >= m || col < 0 || col >= n || visited[row][col])
+            return false;
+        
+        if(word[idx] != board[row][col])
+            return false;
         
         vector<int> offset = {-1, 0, 1, 0, -1};
-        for(int i = 0; i < 4; i++) {
-            if(dfs(board, word, idx + 1, row + offset[i], col + offset[i + 1])) return true;
-        }
+        visited[row][col] = true;
         
-        board[row][col] = word[idx];
+        for(int i = 0; i < 4; i++) {
+            int r = row + offset[i], c = col + offset[i + 1];
+            if(backtracking(board, word, idx + 1, r, c, visited))
+                return true;
+        }
+        visited[row][col] = false;
         return false;
     }
 };
