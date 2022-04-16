d96730bbabc72689716776062ad40e578363a409
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-17 04:43:40 +0900

    Time: 74 ms (16.70%), Space: 17.5 MB (21.23%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,78 +1,33 @@
-class UnionFind {
-private:
-    vector<int> parent, rank;
-    int count = 0;
-    
-public:
-    UnionFind(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        rank = vector<int>(m * n, 0);
-        
-        for(int r = 0; r < m; r++) {
-            for(int c = 0; c < n; c++) {
-                if(grid[r][c] == '1') {
-                    parent.push_back(r * n + c);
-                    count++;
-                }
-                else
-                    parent.push_back(-1);
-            }
-        }
-    }
-    
-    int find(int x) {
-        if(parent[x] != x)
-            return parent[x] = find(parent[x]);
-        return x;
-    }
-    
-    bool Union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if(x == y)
-            return false;
-        
-        if(rank[x] >= rank[y]) {
-            parent[y] = x;
-            rank[x]++;
-        }
-        else {
-            parent[x] = y;
-            rank[y]++;
-        }
-        count--;
-        return true;
-    }
-    
-    int getCount() {
-        return count;
-    }
-};
-
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<int> offset = {-1, 0, 1, 0, -1};
+        int islands = 0;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         
-        UnionFind uf(grid);
-        int ans = 0;
-        for(int r = 0; r < m; r++) {
-            for(int c = 0; c < n; c++) {
-                if(grid[r][c] == '1') {
-                    ans++;
-                    for(int i = 0; i < 4; i++) {
-                        int row = r + offset[i], col = c + offset[i + 1];
-                        if(row >= 0 && row < m && col >= 0 && col < n && grid[row][col] == '1') {
-                            if(uf.Union(r * n + c, row * n + col))
-                                ans--;
-                        }
-                            
-                    }
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                if(grid[row][col] == '1' && !visited[row][col]) {
+                    visited[row][col] = true;
+                    islands++;
+                    dfs(grid, visited, row, col);
                 }
             }
         }
         
-        return ans;
+        return islands;
+    }
+    
+    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col) {
+        int m = grid.size(), n = grid[0].size();
+        vector<int> offset = {-1, 0, 1, 0, -1};
+        
+        for(int i = 0; i < 4; i++) {
+            int r = row + offset[i], c = col + offset[i + 1];
+            if(r >= 0 && r < m && c >= 0 && c < n && !visited[r][c] && grid[r][c] == '1') {
+                visited[r][c] = true;
+                dfs(grid, visited, r, c);
+            }
+        }
     }
 };
