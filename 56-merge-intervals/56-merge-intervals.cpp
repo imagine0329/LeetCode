919086dbf05632d458f7d471f54015031522c5e9
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-17 08:34:07 +0900

    Time: 43 ms (71.53%), Space: 19 MB (85.47%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -4,10 +4,10 @@ public:
         vector<vector<int>> ans;
         
         sort(intervals.begin(), intervals.end());
-        ans.push_back(intervals[0]);
         
+        ans.push_back(intervals[0]);
         for(int i = 1; i < intervals.size(); i++) {
-            if(ans.back()[1] >= intervals[i][0]) 
+            if(ans.back()[1] >= intervals[i][0])
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             else
                 ans.push_back(intervals[i]);
