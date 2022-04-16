6da0f936288cce58d7eec0901a38a8275796898d
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-16 11:35:28 +0900

    Time: 22 ms (63.36%), Space: 11.9 MB (68.22%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        priority_queue<int, vector<int>, greater<int>> allocator;
+        priority_queue<int, vector<int>, greater<int>> q;
         
         sort(intervals.begin(), intervals.end());
-        allocator.push(intervals[0][1]);
+        
+        q.push(intervals[0][1]);
         for(int i = 1; i < intervals.size(); i++) {
-            if(intervals[i][0] >= allocator.top())
-                allocator.pop();
-            allocator.push(intervals[i][1]);
+            if(intervals[i][0] >= q.top()) {
+                q.pop();
+            }
+            q.push(intervals[i][1]);
         }
-        return allocator.size();
+        
+        return q.size();
     }
 };
