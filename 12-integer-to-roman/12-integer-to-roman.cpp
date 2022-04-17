afe89881472f57b24c886f62f8358fd3531eb4b3
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-17 09:04:30 +0900

    Time: 13 ms (42.73%), Space: 8.9 MB (24.58%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int nums[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> sym = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        vector<int> val = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         
+        int i = 0;
         string ans;
-        for(int i = 0; num != 0; i++) {
-            while(nums[i] <= num) {
-                ans += sym[i];
-                num -= nums[i];
+        while(i < sym.size()) {
+            while(num >= val[i]) {
+                int n = num / val[i];
+                for(int j = 0; j < n; j++)
+                    ans += sym[i];
+                num %= val[i];
             }
+            i++;
         }
+        
         return ans;
     }
 };
