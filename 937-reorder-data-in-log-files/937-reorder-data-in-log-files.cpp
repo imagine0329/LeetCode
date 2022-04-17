59f622173704900c7d83e87eb00f0d43e3b2cbd4
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-18 05:03:45 +0900

    Time: 22 ms (29.47%), Space: 15.2 MB (13.23%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -1,23 +1,38 @@
+/*
+    - order: letter -> digit
+    - sort letter logs
+        if contents are the same, then sort lexicographically by their identifiers
+    - maintain digit logs relative ordering
+*/
 
-bool compare(const string a, const string b) {
-    int i = a.find(' ') + 1, j = b.find(' ') + 1;
-
-    if(isalpha(a[i]) && isalpha(b[j])) {
-        if(a.substr(i) == b.substr(j))
-            return a.substr(0, i - 1) < b.substr(0, j - 1);
-        else
-            return a.substr(i) < b.substr(j);
+struct Compare {
+    bool operator()(string a, string b) {
+        int a_i = a.find_first_of(" ");
+        int b_i = b.find_first_of(" ");
+        
+        if(isdigit(a[a_i + 1]) && isdigit(b[b_i + 1]))
+            return false;
+        else if(!isdigit(a[a_i + 1]) && isdigit(b[b_i + 1]))
+            return true;
+        else if(isdigit(a[a_i + 1]) && !isdigit(b[b_i + 1]))
+            return false;
+        else {
+            string a_id = a.substr(0, a_i);
+            string b_id = b.substr(0, b_i);
+            string a_content = a.substr(a_i);
+            string b_content = b.substr(b_i);
+            
+            if(a_content == b_content)
+                return a_id < b_id;
+            return a.substr(a_i) < b.substr(b_i);
+        }
     }
-    else {
-        if(isalpha(a[i])) return true;
-        else return false;
-    }
-}
+};
 
 class Solution {
 public:
     vector<string> reorderLogFiles(vector<string>& logs) {
-        stable_sort(logs.begin(), logs.end(), compare);
+        stable_sort(logs.begin(), logs.end(), Compare());
         return logs;
     }
 };
