e3e9f69984332527c410684495144206c41a9f59
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-19 18:20:45 +0900

    Time: 65 ms (27.94%), Space: 18.8 MB (94.88%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -4,8 +4,8 @@ public:
         vector<vector<int>> ans;
         
         sort(intervals.begin(), intervals.end());
-        
         ans.push_back(intervals[0]);
+        
         for(int i = 1; i < intervals.size(); i++) {
             if(ans.back()[1] >= intervals[i][0])
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
@@ -15,3 +15,13 @@ public:
         return ans;
     }
 };
+/*
+                   i
+    [[1,3],[2,6],[8,10],[15,18]]
+                  s e1
+               e2         
+    [[1,6], [8,10]]
+    
+    if s <= e2
+        e2 = max(e2, e1)
+*/
