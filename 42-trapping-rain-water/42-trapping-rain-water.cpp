89133eb573cedcfddca0afeebc6906fbf096de1d
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-19 17:23:59 +0900

    Time: 18 ms (51.16%), Space: 18.2 MB (34.82%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> left(n), right(n);
-        
         int water = 0;
+        stack<int> s;
         
-        left[0] = height[0];
-        right[n - 1] = height[n - 1];
-        
-        for(int i = 1; i < n; i++)
-            left[i] = max(left[i - 1], height[i]);
-        
-        for(int i = n - 2; i >= 0; i--)
-            right[i] = max(right[i + 1], height[i]);
-        
-        for(int i = 0; i < n; i++)
-            water += min(left[i], right[i]) - height[i];
-        
+        for(int i = 0; i < height.size(); i++) {
+            while(!s.empty() && height[s.top()] < height[i]) {
+                int curr = s.top(); s.pop();
+                if(s.empty())
+                    break;
+                
+                int h = min(height[s.top()], height[i]) - height[curr];
+                int w = i - s.top() - 1;
+                water += h * w;
+            }
+            
+            s.push(i);
+        }
         return water;
     }
 };
+
+/*
+                i
+height = [2,1,0,2,1,0,1,3,2,1,2,1]
+
+stack
+top <---> bottom
+index:  0   1
+value:  2   1
+    
+right height = 2
+current height = 0
+left height = 1
+    
+height = min(right_h, left_h) - curr_h
+width = i - stack.top()
+*/
