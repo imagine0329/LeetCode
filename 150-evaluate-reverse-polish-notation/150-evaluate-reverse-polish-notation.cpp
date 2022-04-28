fcdf8236e378d876eaa2f35778d0f3c522d8743f
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-29 07:54:16 +0900

    Time: 12 ms (67.91%), Space: 11.9 MB (89.18%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,26 +1,27 @@
 class Solution {
-private:
-    bool isNumber(const string& s) {
-        int sign = s.length() > 1 && s[0] == '-' ? 1 : 0;
-        return all_of(s.begin() + sign, s.end(), [](char c) {
-            return isdigit(c);
-        });
-    }
-    
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> s;
         
-        for(auto t : tokens) {
-            if(isNumber(t)) s.push(stoi(t));
-            else {
-                int n2 = s.top(); s.pop();
-                int n1 = s.top(); s.pop();
-                if(t == "+") s.push(n1 + n2);
-                else if(t == "-") s.push(n1 - n2);
-                else if(t == "*") s.push(n1 * n2);
-                else s.push(n1 / n2);
+        for(auto token : tokens) {
+            if(token == "+" || token == "-" || token == "*" || token == "/") {
+                int x = s.top(); s.pop();
+                int y = s.top(); s.pop();
+                int res = 0;
+                
+                if(token == "+")
+                    res = x + y;
+                else if(token == "-")
+                    res = y - x;
+                else if(token == "*")
+                    res = x * y;
+                else if(token == "/")
+                    res = y / x;
+                
+                s.push(res);
             }
+            else
+                s.push(stoi(token));
         }
         
         return s.top();
