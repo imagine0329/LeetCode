462d5d8b1470a7e588cfd5b415d4f182f44db284
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-04-29 09:20:29 +0900

    Time: 6 ms (50.16%), Space: 6.7 MB (20.22%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -4,21 +4,19 @@ public:
         unordered_map<char, int> count;
         int bulls = 0, cows = 0;
         
-        for(auto c : secret)
-            count[c]++;
-        
         for(int i = 0; i < secret.length(); i++) {
-            if(secret[i] == guess[i]) {
+            char s = secret[i], g = guess[i];
+            if(s == g)
                 bulls++;
-                if(count[secret[i]] <= 0)
-                    cows--;
-            }
             else {
-                if(count[guess[i]] > 0)
+                if(count[s] < 0)
                     cows++;
+                if(count[g] > 0)
+                    cows++;
+                
+                count[s]++;
+                count[g]--;
             }
-            
-            count[guess[i]]--;
         }
         
         return to_string(bulls) + "A" + to_string(cows) + "B";
