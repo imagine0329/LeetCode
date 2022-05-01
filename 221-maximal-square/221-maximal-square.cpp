66defa9824b08e3caeba33c6f547ffc57effebff
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-05-02 08:13:01 +0900

    Time: 78 ms (61.29%), Space: 18.2 MB (39.06%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -2,23 +2,35 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        int maxLen = 0, prev = 0;
-        vector<int> dp(n+1, 0);
+        int maxLen = 0;
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
         
-        for(int row = 1; row <= m; row++) {
-            for(int col = 1; col <= n; col++) {
-                int temp = dp[col];
-                if(matrix[row-1][col-1] == '1') {
-                    dp[col] = min(dp[col], min(dp[col-1], prev)) + 1;
-                    maxLen = max(maxLen, dp[col]);
+        for(int row = 1; row < m + 1; row++) {
+            for(int col = 1; col < n + 1; col++) {
+                if(matrix[row - 1][col - 1] == '1') {
+                    dp[row][col] = min(dp[row - 1][col - 1], min(dp[row - 1][col], dp[row][col - 1])) + 1;
+                    maxLen = max(maxLen, dp[row][col]);
                 }
-                else
-                    dp[col] = 0;
-                
-                prev = temp;
             }
         }
-        
         return maxLen * maxLen;
     }
 };
+/*
+
+    0   1   2   3   4
+0 ["1","0","1","0","0"],
+1 ["1","0","1","1","1"],
+2 ["1","1","1","1","1"],
+3 ["1","0","0","1","0"]
+length = 1
+from (r, c) to (r + len, c + len)
+
+    
+    0   1   2   3   4
+0 [ 1 ,"0", 1 ,"0","0"],
+1 [ 1 ,"0", 1 , 1 , 1 ],
+2 [ 1 , 1 , 2 , 2 , 2 ],
+3 [ 1 ,"0", 1 , 2 , 3 ]
+dp(i, j) = min(dp(i-1, j-1), dp(i-1, j), dp(i, j-1)) + 1
+*/
