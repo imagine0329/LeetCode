f1816d9c1917a901b7756bab74a6aee81eaab905
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-05-02 08:35:21 +0900

    Time: 79 ms (59.25%), Space: 16.5 MB (92.56%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -3,21 +3,25 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
         int maxLen = 0;
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        
-        for(int row = 1; row < m + 1; row++) {
-            for(int col = 1; col < n + 1; col++) {
-                if(matrix[row - 1][col - 1] == '1') {
-                    dp[row][col] = min(dp[row - 1][col - 1], min(dp[row - 1][col], dp[row][col - 1])) + 1;
-                    maxLen = max(maxLen, dp[row][col]);
+        vector<int> dp(n + 1, 0);
+        int prev = 0;
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                int temp = dp[col + 1];
+                if(matrix[row][col] == '1') {
+                    dp[col + 1] = min(dp[col + 1], min(dp[col], prev)) + 1;
+                    maxLen = max(maxLen, dp[col + 1]);
                 }
+                else
+                    dp[col + 1] = 0;
+                prev = temp;
             }
         }
         return maxLen * maxLen;
     }
 };
-/*
 
+/*
     0   1   2   3   4
 0 ["1","0","1","0","0"],
 1 ["1","0","1","1","1"],
@@ -33,4 +37,11 @@ from (r, c) to (r + len, c + len)
 2 [ 1 , 1 , 2 , 2 , 2 ],
 3 [ 1 ,"0", 1 , 2 , 3 ]
 dp(i, j) = min(dp(i-1, j-1), dp(i-1, j), dp(i, j-1)) + 1
+
+    0   1   2   3   4
+ dp[0, pre,  0,  0,  0]
+      [x][cur]
+  
+    dp[col] = min(dp[col], dp[col-1], pre) + 1
+    pre = dp[col]
 */
