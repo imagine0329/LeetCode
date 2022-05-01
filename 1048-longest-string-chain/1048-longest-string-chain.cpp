0918e17266135e8a0bb90e2517310981f269737e
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-05-01 10:06:21 +0900

    Time: 92 ms (76.40%), Space: 20.2 MB (44.36%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,23 +1,59 @@
-static bool compare(const string &s1, const string &s2) {
-    return s1.length() < s2.length();
-}
+struct Compare {
+    bool operator()(string& a, string& b) {
+        return a.length() < b.length();
+    }
+};
 
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-        unordered_map<string, int> dp;
-        sort(words.begin(), words.end(), compare);
+        unordered_set<string> dict(words.begin(), words.end());
+        sort(words.begin(), words.end(), Compare());
+        unordered_map<string, int> memo;
         
-        int longest = 0;
-        for(const auto& word : words) {
-            for(int i = 0; i < word.length(); i++) {
-                string newWord = word.substr(0, i) + word.substr(i + 1);
-                dp[word] = max(dp[word], dp.find(newWord) == dp.end() ? 1 : 1 + dp[newWord]);
-            }
-            
-            longest = max(longest, dp[word]);
-        }
-            
+        int longest = 1;
+        for(int i = 0; i < words.size(); i++)
+            longest = max(longest, checkNext(dict, words[i], memo));
+        
         return longest;
     }
+    
+    int checkNext(unordered_set<string>& dict, string word, unordered_map<string, int>& memo) {    
+        if(memo.find(word) != memo.end())
+            return memo[word];
+        
+        int longest = 1;
+        for(int j = 0; j < word.length(); j++) {
+            string str = word.substr(0, j) + word.substr(j + 1);
+            if(dict.find(str) != dict.end())
+                longest = max(longest, checkNext(dict, str, memo) + 1);
+        }
+        
+        return memo[word] = longest;
+    }
 };
+
+/*
+["a","b","ba","bca","bda","bdca"]
+
+"a" -> "ba" -> "bca" -> "bdca"
+"a" -> "ba" -> "bda" -> "bdca"
+
+"a" ["b","ba","bca","bda","bdca"]
+"ba" -> "a" "b"
+
+"ba" ["bca","bda","bdca"]
+"bca" -> "ca" "ba" "bc"
+    
+"bca" ["bda","bdca"]
+"bdca" -> "dca" "bca" "bda" "bdc"
+
+ 0123
+"bdca"
+i = 0 ~ 3
+substr(0,i) + substr(i + 1)
+    i = 0 : "" + "dca"
+    i = 1 : "b" + "ca"
+    i = 2 : "bd" + "a"
+    i = 3 : "bdc" + ""
+*/
