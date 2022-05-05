166b1caf6776d9597e2f4ee29a79a27c25ac92da
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-05-05 10:25:44 +0900

    Time: 178 ms (18.12%), Space: 24.9 MB (21.56%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -3,36 +3,33 @@ public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
         int longest = 0;
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
         vector<vector<int>> memo(m, vector<int>(n, -1));
         
         for(int row = 0; row < m; row++) {
             for(int col = 0; col < n; col++)
-                dfs(matrix, row, col, 1, longest, memo, visited);
+                dfs(matrix, row, col, 1, longest, memo);
         }
         
         return longest;
     }
     
-    int dfs(vector<vector<int>>& matrix, int row, int col, int path, int& longest, vector<vector<int>>& memo, 
-           vector<vector<bool>>& visited) {
+    int dfs(vector<vector<int>>& matrix, int row, int col, int path, int& longest, vector<vector<int>>& memo) {
         
         if(memo[row][col] != -1)
             return memo[row][col];
             
         int m = matrix.size(), n = matrix[0].size();
         vector<int> offset = {-1, 0, 1, 0, -1};
-        int len = 1;
         
-        visited[row][col] = true;
+        int len = 1;
+        memo[row][col] = 0;
         for(int i = 0; i < 4; i++) {
             int r = row + offset[i], c = col + offset[i + 1];
-            if(r >= 0 && r < m && c >= 0 && c < n && matrix[row][col] < matrix[r][c] && !visited[r][c])
-                len = max(len, dfs(matrix, r, c, path + 1, longest, memo, visited) + 1);
+            if(r >= 0 && r < m && c >= 0 && c < n && matrix[row][col] < matrix[r][c] && memo[r][c] != 0)
+                len = max(len, dfs(matrix, r, c, path + 1, longest, memo) + 1);
         }
         
         longest = max(longest, len);
-        visited[row][col] = false;
         return memo[row][col] = len;
     }
 };
