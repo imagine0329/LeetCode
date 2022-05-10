ac308cf125f9759968ec3447acb6d80b62bc5e5c
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-05-11 08:13:11 +0900

    Time: 4 ms (47.39%), Space: 8.4 MB (20.13%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -2,25 +2,31 @@ class Solution {
 public:
     string tictactoe(vector<vector<int>>& moves) {
         int n = 3;
-        int player = 1;
-        vector<int> rows(3), cols(3);
+        vector<int> rows(n, 0), cols(n, 0);
         int diagonal = 0, antiDiagonal = 0;
-        
         int count = 0;
-        for(auto m : moves) {
+        
+        int player = 1;
+        for(auto move : moves) {
             count++;
-            rows[m[0]] += player;
-            cols[m[1]] += player;
-            if(m[0] == m[1]) diagonal += player;
-            if(m[1] == n - m[0] - 1) antiDiagonal += player;
+            int r = move[0], c = move[1];
+            rows[r] += player;
+            cols[c] += player;
+            if(r == c)
+                diagonal += player;
+            if(r == n - c - 1)
+                antiDiagonal += player;
             
-            int win = n * player;
-            if(rows[m[0]] == win || cols[m[1]] == win || diagonal == win || antiDiagonal == win)
+            int win = player * n;
+            if(rows[r] == win || cols[c] == win || diagonal == win || antiDiagonal == win)
                 return player == 1 ? "A" : "B";
             
             player *= -1;
         }
         
-        return count == n * n ? "Draw" : "Pending";
+        if(count == n * n)
+            return "Draw";
+        else
+            return "Pending";
     }
 };
