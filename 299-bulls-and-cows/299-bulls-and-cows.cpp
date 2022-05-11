96c2913c02efa17999013f5955f2a3cc16e42bcf
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-05-12 08:14:49 +0900

    Time: 16 ms (10.25%), Space: 6.6 MB (74.83%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -2,23 +2,49 @@ class Solution {
 public:
     string getHint(string secret, string guess) {
         unordered_map<char, int> count;
-        int bulls = 0, cows = 0;
         
+        for(auto c : secret)
+            count[c]++;
+        
+        int bulls = 0, cows = 0;
         for(int i = 0; i < secret.length(); i++) {
-            char s = secret[i], g = guess[i];
-            if(s == g)
+            if(secret[i] == guess[i]) {
                 bulls++;
+                if(count[secret[i]]-- <= 0)
+                    cows--;
+            }
             else {
-                if(count[s] < 0)
+                if(count[guess[i]] > 0) {
                     cows++;
-                if(count[g] > 0)
-                    cows++;
-                
-                count[s]++;
-                count[g]--;
+                    count[guess[i]]--;
+                }
             }
         }
         
         return to_string(bulls) + "A" + to_string(cows) + "B";
     }
 };
+
+/*
+secret = "1807", guess = "7810"
+bulls = 1, cows = 3
+
+1: 1
+2: 1
+8: 0
+0: -1
+7: 0
+    
+    i        j
+"18207"  "78001"
+
+1 != 7 && 7 exists -> cows++, m[7]--
+8 == 8 -> bulls++, m[8]--
+2 != 0 
+    if(m[0] > 0)
+        cows++, m[0]--
+        
+0 == 0 -> bulls++
+    if(m[0]-- <= 0)
+        cows--
+    */
