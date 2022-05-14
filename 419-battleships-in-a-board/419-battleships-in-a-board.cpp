84965d4ce9a837a4ea95e7f8583aaf48ae70c530
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-05-14 09:05:01 +0900

    Time: 10 ms (57.41%), Space: 9.1 MB (5.36%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -3,43 +3,35 @@ public:
     int countBattleships(vector<vector<char>>& board) {
         int m = board.size(), n = board[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
-        queue<pair<int, int>> q;
-        
-        
         int count = 0;
-        for(int r = 0; r < m; r++) {
-            for(int c = 0; c < n; c++) {
-                if(!visited[r][c] && board[r][c] == 'X') {
+        
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                if(board[row][col] == 'X' && !visited[row][col]) {
                     count++;
-                    bfs(board, visited, r, c);
+                    dfs(board, visited, row, col);
                 }
             }
         }
         return count;
     }
     
-    void bfs(vector<vector<char>>& board, vector<vector<bool>>& visited, int row, int col) {
+    void dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, int row, int col) {
         int m = board.size(), n = board[0].size();
-        queue<pair<int, int>> q;
         vector<int> offset = {-1, 0, 1, 0, -1};
+        visited[row][col] = true;
         
-        q.push({row, col});
-        while(!q.empty()) {
-            int sz = q.size();
-            while(sz--) {
-                row = q.front().first;
-                col = q.front().second;
-                q.pop();
-                visited[row][col] = true;
-                
-                for(int i = 0; i < 4; i++) {
-                    int r = row + offset[i], c = col + offset[i + 1];
-                    if(r >= 0 && r < m && c >= 0 && c < n && !visited[r][c] && board[r][c] == 'X') {
-                        q.push({r, c});
-                        visited[r][c] = true;
-                    }
-                }
-            }
+        for(int i = 0; i < 4; i++) {
+            int r = row + offset[i], c = col + offset[i + 1];
+            if(r >= 0 && r < m && c >= 0 && c < n && board[r][c] == 'X' && !visited[r][c])
+                dfs(board, visited, r, c);
         }
     }
 };
+
+/*
+["X",".","X","X"]
+[".",".",".","X"]
+[".",".",".","X"]
+[".",".",".","."]
+*/
