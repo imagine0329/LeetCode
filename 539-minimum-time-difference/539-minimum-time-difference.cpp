f912ef65e10a387785d67ca8a45f561298824187
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-05-14 10:17:45 +0900

    Time: 18 ms (57.35%), Space: 13.8 MB (15.86%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.cpp
+++ 539-minimum-time-difference/539-minimum-time-difference.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
-        int n = timePoints.size();
-        int minimum = INT_MAX;
-        sort(timePoints.begin(), timePoints.end());
-        int diff;
-        for(int i = 1; i < n; i++) {
-            diff = getDiff(timePoints[i - 1], timePoints[i]);
-            minimum = min(minimum, min(diff, 1440 - diff));
+        vector<int> times;
+        int totalTime = 24 * 60;
+        
+        for(auto point : timePoints) {
+            int hour = stoi(point.substr(0, 2)) * 60;
+            int minute = stoi(point.substr(3, 2));
+            times.push_back(hour + minute);
         }
         
-        diff = getDiff(timePoints[0], timePoints[n - 1]);
-        minimum = min(minimum, min(diff, 1440 - diff));
-        return minimum;
-    }
-    
-    int getDiff(string t1, string t2) {
-        int h1 = stoi(t1.substr(0, 2));
-        int m1 = stoi(t1.substr(3, 2));
-        int h2 = stoi(t2.substr(0, 2));
-        int m2 = stoi(t2.substr(3, 2));
+        sort(times.begin(), times.end());
         
-        return abs((h2 - h1) * 60 + (m2 - m1));
+        int minDiff = INT_MAX;
+        for(int i = 1; i < times.size(); i++) {
+            int diff1 = times[i] - times[i - 1];
+            int diff2 = totalTime - times[i] + times[i - 1];
+            minDiff = min(minDiff, min(diff1, diff2));
+        }
+        
+        minDiff = min(minDiff, totalTime - times[times.size() - 1] + times[0]);
+        return minDiff;
     }
 };
-    
+
+
 /*
-["00:00","23:59","00:00"]
-sort -> ["00:00","00:00", "23:59"]
-HH = 00 ~ 24
-MM = 00 ~ 60
-
- t1                                t2
-0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
-    --------------1st--------------40-------------2nd--------
---20
-
-ans = minimum(1st, 2nd)
-
-1st = t1 to t2 = t2 - t1
-2nd = t2 to t1 = t1 + (1440 - t2)
-
-t1 = 80     t2 = 940*/
+hour    0   1   2   3   4   5   6 ...
+min         20              40
+            a               b
+            |<------------->|       x
+        --->|               |<---   y
+        
+        x = b - a
+        y = totalTime - b + a
+            */
