49f447b4e9c051c4d87facb1d0ea2181766c2d39
imagine0329 <51038203+imagine0329@users.noreply.github.com>
2022-08-08 20:06:59 +0900

    Time: 7 ms (56.36%), Space: 10.7 MB (8.94%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans;
+        return lcp(strs, 0, strs.size() - 1);
+    }
+    
+    string lcp(vector<string>& strs, int left, int right) {
+        if(left == right)
+            return strs[left];
         
-        for(int i=0; i<strs[0].size(); i++) {
-            char c = strs[0][i];
-            for(auto s : strs) {
-                if(i == s.size() || s[i] != c)
-                    return ans;
-            }
-            ans += c;
+        int mid = (left + right) / 2;
+        string str_l = lcp(strs, left, mid);
+        string str_r = lcp(strs, mid + 1, right);
+        
+        string ret;
+        for(int i = 0; i < min(str_l.length(), str_r.length()); i++) {
+            if(str_l[i] != str_r[i])
+                return ret;
+            
+            ret += str_l[i];
         }
         
-        return ans;
+        return ret;
     }
 };
 
 
 /*
 
-     i
-leets
-leetcode
-leetabcds
+0 1 2 3 4 5 6
+l     m     r
+a b c d e f g
 
-ans = leet
+l m   r     l m r
+a b c d     e f g
+
+l r     l r
+a b     c d
+
+l/r     l/r
+a       b
 
 */
